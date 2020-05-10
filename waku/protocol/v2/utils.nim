b2636c4c775343f4a1a3df97bb589600f8eb1990
Oskar Thoren <ot@oskarthoren.com>
2020-05-10 14:53:48 +0800

    Import standard_setup and utils
    Will be used for new PubSub protocol

---
+++ waku/protocol/v2/utils.nim
@@ -0,0 +1,15 @@
+import chronos
+import standard_setup
+export standard_setup
+
+proc generateNodes*(num: Natural, gossip: bool = false): seq[Switch] =
+  for i in 0..<num:
+    result.add(newStandardSwitch(gossip = gossip))
+
+proc subscribeNodes*(nodes: seq[Switch]) {.async.} =
+  var dials: seq[Future[void]]
+  for dialer in nodes:
+    for node in nodes:
+      if dialer.peerInfo.peerId != node.peerInfo.peerId:
+        dials.add(dialer.connect(node.peerInfo))
+        await allFutures(dials)
