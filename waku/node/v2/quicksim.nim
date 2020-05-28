0a9ce6c0b2d7049b703bb71ede2c4f49a46859a0
Oskar Thoren <ot@oskarthoren.com>
2020-05-28 11:52:57 +0800

    Hardcode all six nodes listen topic, flooding

--- waku/node/v2/quicksim.nim
+++ waku/node/v2/quicksim.nim
@@ -15,18 +15,31 @@ const topicAmount = 100
 
 let node1 = newRpcHttpClient()
 let node2 = newRpcHttpClient()
+let node3 = newRpcHttpClient()
+let node4 = newRpcHttpClient()
+let node5 = newRpcHttpClient()
+let node6 = newRpcHttpClient()
 
 waitFor node1.connect("localhost", Port(8547))
 waitFor node2.connect("localhost", Port(8548))
+waitFor node3.connect("localhost", Port(8549))
+waitFor node4.connect("localhost", Port(8550))
+waitFor node5.connect("localhost", Port(8551))
+waitFor node6.connect("localhost", Port(8552))
 
-#let version = waitFor node1.wakuVersion()
-#info "Version is", version
+let version = waitFor node6.wakuVersion()
+info "Version is", version
 
 proc handler(topic: string, data: seq[byte]) {.async, gcsafe.} =
   debug "Hit handler", topic=topic, data=data
 
 # TODO: Implement handler logic
-let res1 = waitFor node2.wakuSubscribe("foobar")
+# All subscribing to foobar topic
+let res2 = waitFor node2.wakuSubscribe("foobar")
+let res3 = waitFor node3.wakuSubscribe("foobar")
+let res4 = waitFor node4.wakuSubscribe("foobar")
+let res5 = waitFor node5.wakuSubscribe("foobar")
+let res6 = waitFor node6.wakuSubscribe("foobar")
 os.sleep(2000)
 
 # info "Posting envelopes on all subscribed topics"
