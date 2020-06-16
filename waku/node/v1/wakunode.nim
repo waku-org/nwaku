6a7375751c9ff4e2557cb905786a91d6c3533166
decanus <7621705+decanus@users.noreply.github.com>
2020-06-16 12:02:02 +0200

    fix

--- waku/node/v1/wakunode.nim
+++ waku/node/v1/wakunode.nim
@@ -119,15 +119,13 @@ proc run(config: WakuNodeConf) =
   proc logPeerAccounting(udata: pointer) {.closure, gcsafe.} =
     {.gcsafe.}:
       
-    for peer in node.peerPool.peers:
-      let 
-        accounting = peer.state(Waku)
-        sent = accounting.sent
-        received = accounting.sent
-        id = peer.remote.id
-
-      info "Peer Metrics", id, sent, received
-      peer.state(Waku).accounting = Accounting(sent: 0, received: 0)
+      for peer in node.peerPool.peers:
+        let 
+          sent = peer.state(Waku).accounting.sent
+          received = peer.state(Waku).accounting.received
+          id = peer.remote.id
+        info "Peer Metrics", id, sent, received
+        peer.state(Waku).accounting = Accounting(sent: 0, received: 0)
 
     addTimer(Moment.fromNow(2.seconds), logPeerAccounting)
   addTimer(Moment.fromNow(2.seconds), logPeerAccounting)
