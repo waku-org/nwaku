b27d9c3858cb893c1e59854f04fe50f15d56f9ed
decanus <7621705+decanus@users.noreply.github.com>
2020-07-13 18:49:38 +0200

    added

---
+++ waku/protocol/v2/waku_historic_messages.nim
@@ -0,0 +1,14 @@
+import libp2p/protocols/protocol
+
+const
+  HistoricMessagesCodec* = "/waku/history/1.0.0"
+
+type
+  HistoricMessages* = ref object of LPProtocol
+
+method init*(p: HistoricMessages) =
+  proc handle(conn: Connection, proto: string) {.async, gcsafe, closure.} =
+    discard
+
+  p.handle = handle
+  p.codec = HistoricMessagesCodec
