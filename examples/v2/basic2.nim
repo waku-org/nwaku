704363dd2d7b4a7dad82924c603ac3717c92e1fa
Oskar Thoren <ot@oskarthoren.com>
2020-07-17 13:06:43 +0800

    Sketch out API usage

--- examples/v2/basic2.nim
+++ examples/v2/basic2.nim
@@ -17,3 +17,12 @@ let conf = WakuNodeConf.load()
 # Running this should give you output like this:
 # INF Listening on tid=5719 full=/ip4/127.0.0.1/tcp/60000/p2p/16Uiu2HAmNiAqr1cwhyntotP9fiSDyBvfKtB4ZiaDsrkipSCoKCB4
 run(conf)
+
+# To do more operations on this node, we can do this in two ways:
+# - We can use the Nim Node API, which is currently not exposed
+# - We can use JSON RPC
+
+# TODO Subscribe and publish to topic via Node API
+# Requires https://github.com/status-im/nim-waku/issues/53
+
+# TODO Show how to do this with JSON RPC
