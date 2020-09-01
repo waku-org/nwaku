67a964be28523bf44c3aaed51cb0e9bdda097e63
Oskar Thorén <ot@oskarthoren.com>
2020-09-01 11:03:17 +0800

    Partial filter (#120)
    * start work on filter protocol
    
    * init functions for protobufs
    
    * comment
    
    * added init
    
    * playing around with subscribers
    
    * Update waku_filter.nim
    
    * added test for protobuf
    
    * fixes
    
    * fix
    
    * Update test_waku_filter.nim
    
    * Update waku_filter.nim
    
    * Add note on status
    
    Co-authored-by: decanus <7621705+decanus@users.noreply.github.com>

---
+++ tests/v2/test_waku_filter.nim
@@ -0,0 +1,30 @@
+
+import unittest, options, tables, sets, sequtils
+import chronos, chronicles
+import utils,
+       libp2p/errors,
+       libp2p/switch,
+       libp2p/protobuf/minprotobuf,
+       libp2p/stream/[bufferstream, connection],
+       libp2p/crypto/crypto,
+       libp2p/protocols/pubsub/floodsub,
+       libp2p/protocols/pubsub/rpc/message,
+       libp2p/multistream,
+       libp2p/transports/transport,
+       libp2p/transports/tcptransport
+import ../../waku/protocol/v2/[waku_protocol2, waku_filter, filter]
+
+import ../test_helpers
+
+procSuite "Waku Filter":
+
+  test "encoding and decoding FilterRPC":
+    let rpc = FilterRPC(filters: @[ContentFilter(topics: @["foo", "bar"])])
+
+    let buf = rpc.encode()
+
+    let decode = FilterRPC.init(buf.buffer)
+
+    check:
+      decode.isErr == false
+      decode.value == rpc
