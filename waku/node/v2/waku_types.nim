a2726578f86466a253470cd5cb12000301fc455a
Oskar Thoren <ot@oskarthoren.com>
2020-09-01 12:31:58 +0800

    Add WakuMessage encoding and decoding
    Also clean up waku_types module and imports

--- waku/node/v2/waku_types.nim
+++ waku/node/v2/waku_types.nim
@@ -1,13 +1,13 @@
+## Core Waku data types are defined here to avoid recursive dependencies.
+##
+## TODO Move more common data types here
+
 import
   chronos,
-  libp2p/multiaddress,
-  libp2p/crypto/crypto,
-  libp2p/peerinfo,
-  standard_setup
+  libp2p/[switch, peerinfo, multiaddress, crypto/crypto],
+  libp2p/protobuf/minprotobuf
 
-# Core Waku data types are defined here to avoid recursive dependencies.
-#
-# TODO Move more common data types here
+# Common data types -----------------------------------------------------------
 
 type
   Topic* = string
@@ -16,11 +16,29 @@ type
   # NOTE based on Eth2Node in NBC eth2_network.nim
   WakuNode* = ref object of RootObj
     switch*: Switch
-    # XXX Unclear if we need this
     peerInfo*: PeerInfo
     libp2pTransportLoops*: seq[Future[void]]
     messages*: seq[(Topic, Message)]
 
-  WakuMessage* =
+  WakuMessage* = object
     payload*: seq[byte]
     contentTopic*: string
+
+
+
+# Encoding and decoding -------------------------------------------------------
+
+proc init*(T: type WakuMessage, buffer: seq[byte]): ProtoResult[T] =
+  var msg = WakuMessage()
+  let pb = initProtoBuffer(buffer)
+
+  discard ? pb.getField(1, msg.payload)
+  discard ? pb.getField(2, msg.contentTopic)
+
+  ok(msg)
+
+proc encode*(message: WakuMessage): ProtoBuffer =
+  result = initProtoBuffer()
+
+  result.write(1, message.payload)
+  result.write(2, message.contentTopic)
