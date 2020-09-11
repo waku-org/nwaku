49e5eb44f5fda760b1f3e8a5dff967450e2cd827
Oskar Thorén <ot@oskarthoren.com>
2020-09-11 15:49:45 +0800

    JSON RPC: Use WakuNode instead of WakuRelay (#154)
    * Using Node publish for JSON RPC
    
    Compiles, untested
    
    * Subscribe and print waku message
    
    * Readable string
    
    * Use actual input

--- waku/node/v2/rpc/rpc_publish.nim
+++ waku/node/v2/rpc/rpc_publish.nim
@@ -1,9 +1,11 @@
 import
   os, strutils, strformat, chronicles, json_rpc/[rpcclient, rpcserver], nimcrypto/sysrand,
+  stew/byteutils,
   libp2p/protobuf/minprotobuf,
   eth/common as eth_common, eth/keys,
   system,
-  options
+  options,
+  ../waku_types
 
 from strutils import rsplit
 template sourceDir: string = currentSourcePath.rsplit(DirSep, 1)[0]
@@ -18,8 +20,9 @@ if paramCount() < 1:
 let rpcPort = Port(parseInt(paramStr(1)))
 
 echo "Please enter your message:"
-let message = readLine(stdin)
-echo "Message is:", message
+let raw_input = readLine(stdin)
+let input = fmt"{raw_input}"
+echo "Input is:", input
 
 var node = newRpcHttpClient()
 waitfor node.connect("localhost", rpcPort)
@@ -27,5 +30,10 @@ waitfor node.connect("localhost", rpcPort)
 # Subscribe ourselves to topic
 #var res = node.wakuSubscribe("waku")
 
-# TODO When RPC uses Node, create WakuMessage and pass instead
-var res2 = waitfor node.wakuPublish("waku", cast[seq[byte]]("hello world"))
+let pubSubTopic = "waku"
+let contentTopic = "foobar"
+var wakuMessage = WakuMessage(payload: input.toBytes(), contentTopic: contentTopic)
+# XXX This should be WakuMessage type, but need to setup JSON-RPC mapping for that to work
+var raw_bytes = wakuMessage.encode().buffer
+var res = waitfor node.wakuPublish2(pubSubTopic, raw_bytes)
+echo "Waku publish response: ", res
