6b5d8ffc83c30b6e196cca239667a488561df459
Oskar Thoren <ot@oskarthoren.com>
2020-10-05 12:23:11 +0800

    Node Info API call.
    RPC node info
    
    fix Makefile error
    
    fix rpc query error
    
    add rpc_node_info to scripts target
    
    hm
    
    node info -> info
    
    consistent query node ref
    
    Add info to node api
    
    update node api docs
    
    update node api doc for consistency and accuracy
    
    minor

--- waku/node/v2/rpc/rpc_info.nim
+++ waku/node/v2/rpc/rpc_info.nim
@@ -12,19 +12,8 @@ template sourceDir: string = currentSourcePath.rsplit(DirSep, 1)[0]
 const sigWakuPath = sourceDir / "wakucallsigs.nim"
 createRpcSigs(RpcHttpClient, sigWakuPath)
 
-if paramCount() < 1:
-  echo "Please provide rpcPort as argument."
-  quit(1)
-
-let rpcPort = Port(parseInt(paramStr(1)))
-
-echo "Please enter your topic:"
-let raw_input = readLine(stdin)
-let input = fmt"{raw_input}"
-echo "Input is:", input
-
 var node = newRpcHttpClient()
-waitfor node.connect("localhost", rpcPort)
+waitfor node.connect("localhost", Port(8545))
 
-var res = waitfor node.wakuQuery(@[input])
-echo "Waku query response: ", res
+var res = waitfor node.wakuInfo()
+echo "Waku info res: ", res
