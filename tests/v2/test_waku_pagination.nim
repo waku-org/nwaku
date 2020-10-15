a48e570594c3bc7610c3b33cf93012723b7623dd
Sanaz <staheri14@ku.edu.tr>
2020-10-14 18:36:18 -0700

    Some clean up

--- tests/v2/test_waku_pagination.nim
+++ tests/v2/test_waku_pagination.nim
@@ -7,19 +7,21 @@ import
 procSuite "pagination":
 
     test "computeIndex empty contentTopic test":
-        let wm = WakuMessage(payload: @[byte 1, 2, 3])
-        let index=wm.computeIndex()
+        let 
+            wm = WakuMessage(payload: @[byte 1, 2, 3])
+            index=wm.computeIndex()
         check:
             # the fields of the index should be non-empty
             index.digest.len != 0
             index.receivedTime!=0 
         
     test "computeIndex identical WakuMessages test":
-        let wm = WakuMessage(payload: @[byte 1, 2, 3], contentTopic: "topic2")
-        let index1=wm.computeIndex()
-
-        let wm2 = WakuMessage(payload: @[byte 1, 2, 3], contentTopic: "topic2")
-        let index2=wm2.computeIndex()
+        let 
+            wm = WakuMessage(payload: @[byte 1, 2, 3], contentTopic: "topic2")
+            index1=wm.computeIndex()
+            wm2 = WakuMessage(payload: @[byte 1, 2, 3], contentTopic: "topic2")
+            index2=wm2.computeIndex()
+            
         check:
             # the digests of two identical WakuMessages must be the same
             index1.digest==index2.digest
