f1a5ca0dd5df4cdc91cbc303eb0801e8da421db8
Sanaz <staheri14@ku.edu.tr>
2020-10-16 12:56:13 -0700

    edits indentations and fixes a bug

--- tests/v2/test_waku_pagination.nim
+++ tests/v2/test_waku_pagination.nim
@@ -1,28 +1,25 @@
 import
-    std/unittest, times,
-    ../../waku/node/v2/waku_types,
-    ../test_helpers,
-    nimcrypto/sha2
+    std/unittest,
+    ../../waku/node/v2/waku_types
 
-procSuite "pagination":
+suite "pagination":
+  test "computeIndex: empty contentTopic test":
+      let
+          wm = WakuMessage(payload: @[byte 1, 2, 3])
+          index = wm.computeIndex()
+      check:
+          # the fields of the index should be non-empty
+          len(index.digest.data) != 0
+          index.receivedTime != 0
 
-    test "computeIndex: empty contentTopic test":
-        let
-            wm = WakuMessage(payload: @[byte 1, 2, 3])
-            index = wm.computeIndex()
-        check:
-            # the fields of the index should be non-empty
-            index.digest.len != 0
-            index.receivedTime != 0
+  test "computeIndex: identical WakuMessages test":
+      let
+          wm = WakuMessage(payload: @[byte 1, 2, 3], contentTopic: "topic2")
+          index1 = wm.computeIndex()
+          wm2 = WakuMessage(payload: @[byte 1, 2, 3], contentTopic: "topic2")
+          index2 = wm2.computeIndex()
 
-    test "computeIndex: identical WakuMessages test":
-        let
-            wm = WakuMessage(payload: @[byte 1, 2, 3], contentTopic: "topic2")
-            index1 = wm.computeIndex()
-            wm2 = WakuMessage(payload: @[byte 1, 2, 3], contentTopic: "topic2")
-            index2 = wm2.computeIndex()
-
-        check:
-            # the digests of two identical WakuMessages must be the same
-            index1.digest == index2.digest
+      check:
+          # the digests of two identical WakuMessages must be the same
+          index1.digest == index2.digest
 
