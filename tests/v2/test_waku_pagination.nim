d6c48da071206c909b90d5b4b26414c205b8c374
Sanaz Taheri Boshrooyeh <35961250+staheri14@users.noreply.github.com>
2020-10-19 19:20:44 -0700

    Pagination: Pagination feature/compute index (#225)
    * adds the index object
    
    * adds the indexedWakuMessage
    
    * adds the PagingInfo
    
    * Adds PagingInfo to HistoryResponse and HistoryQuery
    
    * adds the computeIndex procedure
    
    * Update waku/node/v2/waku_types.nim
    
    Fixes spacing issues
    
    Co-authored-by: Oskar Thorén <ot@oskarthoren.com>
    
    * Changes timestamp to receivedTime and checks for the  empty contentTopic
    
    * adds a test file for pagination with test scenarios for computeIndex
    
    * changes receivedTimestamp to the unix timestamp
    
    * updates a test case
    
    * replaces std/sha1 with  nimcrypto/sha2
    
    * changes the tests titles
    
    * minor comments
    
    * Some clean up
    
    * fixes some formatting issue
    
    * edits a test-case
    
    * adds comments
    
    * changes the digest type to MDigest[256] and modifies the computeIndex
    
    * fixes formatting issue
    
    * edits indentations and fixes a bug
    
    * minor edits
    
    * changes suite to procSuite and adds a new text case
    
    * fixes all the indentations
    
    * cleanup of the imports
    
    Co-authored-by: Oskar Thorén <ot@oskarthoren.com>

---
+++ tests/v2/test_waku_pagination.nim
@@ -0,0 +1,27 @@
+import
+  std/unittest,
+  ../../waku/node/v2/waku_types,
+  ../test_helpers
+
+procSuite "pagination":
+  test "computeIndex: empty contentTopic test":
+    let
+      wm = WakuMessage(payload: @[byte 1, 2, 3])
+      index = wm.computeIndex()
+    check:
+      # the fields of the index should be non-empty
+      len(index.digest.data) != 0
+      len(index.digest.data) == 32 # sha2 output length in bytes
+      index.receivedTime != 0 # the timestamp should be a non-zero value
+
+  test "computeIndex: identical WakuMessages test":
+    let
+      wm = WakuMessage(payload: @[byte 1, 2, 3], contentTopic: "topic2")
+      index1 = wm.computeIndex()
+      wm2 = WakuMessage(payload: @[byte 1, 2, 3], contentTopic: "topic2")
+      index2 = wm2.computeIndex()
+
+    check:
+      # the digests of two identical WakuMessages must be the same
+      index1.digest == index2.digest
+
