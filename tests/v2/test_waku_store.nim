8c6ad6a5b00d386ae52d10651df12a55f70674a0
Sanaz <staheri14@ku.edu.tr>
2020-10-27 11:21:45 -0700

    adds test for the encode and init methods of PagingInfo

--- tests/v2/test_waku_store.nim
+++ tests/v2/test_waku_store.nim
@@ -70,4 +70,20 @@ procSuite "Waku Store":
     check:
       decoded_index.value.receivedTime == index.receivedTime
       decoded_index.value.digest.data == index.digest.data
+    
+    let 
+      index=computeIndex(WakuMessage(payload: @[byte 1], contentTopic:"topic 1")
+      pagingInfo: PagingInfo(pageSize:3, cursor:index,direction: true)
+      pb= pagingInfo.encode()
+    
+    echo "protobuf ", pb.buffer
+    let decodedPagingInfo= PagingInfo.init(pb.buffer)
+    echo decodedPagingInfo
+    echo pagingInfo
+    
+    check:
+      decoded_index.value.pageSize == pagingInfo.pageSize
+      decoded_index.value.cursor == pagingInfo.cursor
+      decoded_index.value.direction == pagingInfo.direction
+
 
