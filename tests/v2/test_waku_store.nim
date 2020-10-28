e66dd678865fd22ba2cb42c02a9927100ef90fe2
Sanaz Taheri Boshrooyeh <35961250+staheri14@users.noreply.github.com>
2020-10-28 09:11:50 -0700

    Update tests/v2/test_waku_store.nim
    Co-authored-by: Dean Eigenmann <7621705+decanus@users.noreply.github.com>

--- tests/v2/test_waku_store.nim
+++ tests/v2/test_waku_store.nim
@@ -70,7 +70,7 @@ procSuite "Waku Store":
 
   test "PagingDirection Protobuf encod/init test": 
     let 
-      pagingDirection=PagingDirection.BACKWARD
+      pagingDirection = PagingDirection.BACKWARD
       pb = pagingDirection.encode()
       decodedPagingDirection = PagingDirection.init(pb.buffer)
     
@@ -88,4 +88,3 @@ procSuite "Waku Store":
       decodedPagingInfo.value.pageSize == pagingInfo.pageSize
       decodedPagingInfo.value.cursor == pagingInfo.cursor
       decodedPagingInfo.value.direction == pagingInfo.direction
-
