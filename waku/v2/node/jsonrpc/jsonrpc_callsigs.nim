135eaae9fb614c6de597ed445f5299691053fccd
Hanno Cornelius <68783915+jm-clius@users.noreply.github.com>
2020-11-24 05:44:37 +0200

    Waku v2 JSON-RPC REST API: Store protocol proof of concept (#263)
    * Waku V2 history query POC
    
    * Fix folder structure
    
    * Improve test clarity
    
    * Improve imports, returns and some naming
    
    * Changed naming conventions. Refactor & improve.
    
    Co-authored-by: Oskar Thorén <ot@oskarthoren.com>

---
+++ waku/v2/node/jsonrpc/jsonrpc_callsigs.nim
@@ -0,0 +1,1 @@
+proc get_waku_v2_store_v1_messages(topics: seq[ContentTopic], pagingOptions: Option[StorePagingOptions]): StoreResponse
---
+++ waku/v2/node/jsonrpc/jsonrpc_utils.nim
@@ -0,0 +1,23 @@
+import
+  std/options,
+  ../../waku_types,
+  ../wakunode2,
+  ./jsonrpc_types
+
+## Conversion tools
+## Since the Waku v2 JSON-RPC API has its own defined types,
+## we need to convert between these and the types for the Nim API
+
+proc toPagingInfo*(pagingOptions: StorePagingOptions): PagingInfo =
+  PagingInfo(pageSize: pagingOptions.pageSize,
+             cursor: if pagingOptions.cursor.isSome: pagingOptions.cursor.get else: Index(),
+             direction: if pagingOptions.forward: PagingDirection.FORWARD else: PagingDirection.BACKWARD)
+
+proc toPagingOptions*(pagingInfo: PagingInfo): StorePagingOptions =
+  StorePagingOptions(pageSize: pagingInfo.pageSize,
+                     cursor: some(pagingInfo.cursor),
+                     forward: if pagingInfo.direction == PagingDirection.FORWARD: true else: false)
+
+proc toStoreResponse*(historyResponse: HistoryResponse): StoreResponse =
+  StoreResponse(messages: historyResponse.messages,
+                pagingOptions: if historyResponse.pagingInfo != PagingInfo(): some(historyResponse.pagingInfo.toPagingOptions()) else: none(StorePagingOptions))
