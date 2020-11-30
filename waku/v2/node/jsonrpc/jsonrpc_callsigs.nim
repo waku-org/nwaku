51b35c59c6e059ee61efd1e326703e0bc980f935
Hanno Cornelius <68783915+jm-clius@users.noreply.github.com>
2020-11-30 11:44:57 +0200

    Added some basic debug and relay json-rpc calls (#309)

--- waku/v2/node/jsonrpc/jsonrpc_callsigs.nim
+++ waku/v2/node/jsonrpc/jsonrpc_callsigs.nim
@@ -4,9 +4,15 @@ proc get_waku_v2_debug_v1_info(): WakuInfo
 
 # Relay API
 
+proc post_waku_v2_relay_v1_message(topic: string, message: WakuRelayMessage): bool
 proc post_waku_v2_relay_v1_subscriptions(topics: seq[string]): bool
 proc delete_waku_v2_relay_v1_subscriptions(topics: seq[string]): bool
 
 # Store API
 
 proc get_waku_v2_store_v1_messages(topics: seq[ContentTopic], pagingOptions: Option[StorePagingOptions]): StoreResponse
+
+# Filter API
+
+proc post_waku_v2_filter_v1_subscription(contentFilters: seq[ContentFilter], topic: Option[string]): bool
+proc delete_waku_v2_filter_v1_subscription(contentFilters: seq[ContentFilter], topic: Option[string]): bool
