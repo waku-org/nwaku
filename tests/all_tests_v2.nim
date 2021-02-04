91d996a518e17b002983e22c77e67fe331f5ba85
jm-clius <jm-clius@users.noreply.github.com>
2021-02-04 10:53:58 +0000

    deploy: 85ffaea518af3205803b185fe1bee35be6e76953

--- tests/all_tests_v2.nim
+++ tests/all_tests_v2.nim
@@ -10,5 +10,6 @@ import
   ./v2/test_waku_swap,
   ./v2/test_message_store,
   ./v2/test_jsonrpc_waku,
+  ./v2/test_peer_manager,
   ./v2/test_web3 # will remove it when rln-relay tests get added
   # TODO ./v2/test_waku_rln_relay
