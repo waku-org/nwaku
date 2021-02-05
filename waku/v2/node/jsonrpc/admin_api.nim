13fc9d6f710b4822eeb53187acd804a083d5772c
Hanno Cornelius <68783915+jm-clius@users.noreply.github.com>
2021-02-05 12:49:11 +0200

    Feat/peer manager improvements (#367)
    * Add managed peers to Admin API result
    
    * Deal with dial failures

--- waku/v2/node/jsonrpc/admin_api.nim
+++ waku/v2/node/jsonrpc/admin_api.nim
@@ -1,20 +1,25 @@
 {.push raises: [Exception, Defect].}
 
 import
-  std/[options,sequtils],
+  std/[options, sequtils, sets],
   json_rpc/rpcserver,
   libp2p/[peerinfo, switch],
   ../../protocol/waku_store/[waku_store_types, waku_store],
   ../../protocol/waku_swap/[waku_swap_types, waku_swap],
   ../../protocol/waku_filter/[waku_filter_types, waku_filter],
   ../wakunode2,
+  ../peer_manager,
   ./jsonrpc_types
 
 export jsonrpc_types
 
+proc constructMultiaddrStr*(wireaddr: MultiAddress, peerId: PeerId): string =
+  # Constructs a multiaddress with both wire address and p2p identity
+  $wireaddr & "/p2p/" & $peerId
+
 proc constructMultiaddrStr*(peerInfo: PeerInfo): string =
-  # Constructs a multiaddress with both location address and p2p identity
-  $peerInfo.addrs[0] & "/p2p/" & $peerInfo.peerId
+  # Constructs a multiaddress with both location (wire) address and p2p identity
+  constructMultiaddrStr(peerInfo.addrs[0], peerInfo.peerId)
 
 proc installAdminApiHandlers*(node: WakuNode, rpcsrv: RpcServer) =
 
@@ -29,6 +34,18 @@ proc installAdminApiHandlers*(node: WakuNode, rpcsrv: RpcServer) =
 
     var wPeers: seq[WakuPeer] = @[]
 
+    ## Managed peers
+
+    if not node.wakuRelay.isNil:
+      # Map all managed peers to WakuPeers and add to return list
+      wPeers.insert(node.peerManager.peers.mapIt(WakuPeer(multiaddr: constructMultiaddrStr(toSeq(it.addrs.items)[0], it.peerId),
+                                                          protocol: toSeq(it.protos.items)[0],
+                                                          connected: node.peerManager.connectedness(it.peerId))),
+                    wPeers.len) # Append to the end of the sequence
+
+    ## Unmanaged peers
+    ## @TODO add these peers to peer manager
+    
     if not node.wakuSwap.isNil:
       # Map WakuSwap peers to WakuPeers and add to return list
       wPeers.insert(node.wakuSwap.peers.mapIt(WakuPeer(multiaddr: constructMultiaddrStr(it.peerInfo),
