0e87bdaa61d98e05985c8bb54be8b0f9df3fd8c3
Oskar Thoren <ot@oskarthoren.com>
2021-02-08 12:36:14 +0800

    Get balance string from running node

--- tests/v2/test_waku_swap_contracts.nim
+++ tests/v2/test_waku_swap_contracts.nim
@@ -1,15 +1,24 @@
  # Tests of Swap contracts via external module
  #
 import
-  std/[unittest, options, tables, sets, osproc, strutils],
+  std/[unittest, options, tables, sets, osproc, strutils, strformat],
   ../test_helpers, ./utils
 
 procSuite "Basic balance test":
-  test "Get pwd":
-    let (output, errC) = osproc.execCmdEx("pwd")
+  test "Get pwd of swap module":
+    let (output, errC) = osproc.execCmdEx("(cd ../swap-contracts-module && pwd)")
     echo output
 
     check:
-      contains(output, "nim-waku")
+      contains(output, "swap-contracts-module")
 
-#let cmdString = "npx hardhat balance --account 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+  test "Get balance from running node":
+    # NOTE: This corresponds to the first default account in Hardhat
+    let taskString = "npx hardhat balance --account 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+    let cmdString = "cd ../swap-contracts-module; " & &"{taskString}"
+    echo cmdString
+    let (output, errC) = osproc.execCmdEx(cmdString)
+    echo output
+
+    check:
+      contains(output, "ETH")
