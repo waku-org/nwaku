b6584ddc3543315da18baddf4c3752520b015fca
Oskar Thoren <ot@oskarthoren.com>
2021-02-22 12:57:17 +0800

    fix redeem part too

--- tests/v2/test_waku_swap_contracts.nim
+++ tests/v2/test_waku_swap_contracts.nim
@@ -37,7 +37,7 @@ procSuite "Basic balance test":
       contains(aliceAddress, "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
 
   test "Sign Cheque":
-    let signature = waku_swap_contracts.signCheque(aliceSwapAddress)
+    signature = waku_swap_contracts.signCheque(aliceSwapAddress)
 
     check:
       contains(signature, "0x")
@@ -49,25 +49,12 @@ procSuite "Basic balance test":
       json["bobBalance"].getInt() == 10000
 
   test "Redeem cheque and check balance":
-    let taskString = "npx hardhat --network localhost redeemCheque --swapaddress '" & &"{aliceSwapAddress}" & "' --signature '" & &"{signature}" & "'"
-    let cmdString = "cd ../swap-contracts-module; " & &"{taskString}"
-    echo cmdString
-    let (output, errC) = osproc.execCmdEx(cmdString)
-
-    # XXX Assume succeeds
-    echo output
-    let json = parseJson(output)
+    let json = waku_swap_contracts.redeemCheque(aliceSwapAddress, signature)
     var resp = json["resp"].getStr()
     echo json
 
     echo "Get balances"
-    let taskString2 = "npx hardhat --network localhost getBalances --erc20address '" & &"{erc20address}" & "'"
-    let cmdString2 = "cd ../swap-contracts-module; " & &"{taskString2}"
-    echo cmdString2
-    let (output2, errC2) = osproc.execCmdEx(cmdString2)
-
-    # XXX Assume succeeds
-    let json2 = parseJson(output2)
+    let json2 = getERC20Balances(erc20address)
     echo json2
 
     # Balance for Bob has now increased
