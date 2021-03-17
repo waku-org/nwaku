b103b7a222c91ac66671284cf991203e0906b74c
Sanaz <staheri14@ku.edu.tr>
2021-03-17 13:07:59 -0700

    entirely replaces the prior rln header, the var variables are changed to ptr

--- waku/v2/protocol/waku_rln_relay/rln.nim
+++ waku/v2/protocol/waku_rln_relay/rln.nim
@@ -2,9 +2,6 @@
 
 import os
 
-# librln.dylib is the rln library taken from https://github.com/kilic/rln (originally implemented in rust with an exposed C API)  
-# contains the key generation and other relevant functions
-
 
 const libPath = "vendor/rln/target/debug/"
 when defined(Windows):
@@ -14,58 +11,44 @@ elif defined(Linux):
 elif defined(MacOsX):
   const libName* = libPath / "librln.dylib"
 
-# Data types -----------------------------
-
-# pub struct Buffer {
-#     pub ptr: *const u8,
-#     pub len: usize,
-# }
-
-type
-  Buffer* = object
-    `ptr`*: pointer
-    len*: csize_t
-  RLNBn256* = object
-
-# Procedures ------------------------------
  # all the following procedures are Nim wrappers for the functions defined in libName
 {.push dynlib: libName.}
 
-# pub extern "C" fn new_circuit_from_params(
-#     merkle_depth: usize,
-#     index: usize,
-#     parameters_buffer: *const Buffer,
-#     ctx: *mut *mut RLN<Bn256>,
-# ) -> bool
-proc newCircuitFromParams*(merkle_depth: csize_t, parameters_buffer: ptr Buffer, ctx: var  ptr RLNBn256): bool{.importc: "new_circuit_from_params".}
+type RLN*[E] {.incompleteStruct.} = object
+type Bn256* = pointer
 
-# pub extern "C" fn key_gen(ctx: *const RLN<Bn256>, keypair_buffer: *mut Buffer) -> bool
-proc keyGen*(ctx: ptr RLNBn256, keypair_buffer: var Buffer): bool {.importc: "key_gen".}
+## Buffer struct is taken from
+# https://github.com/celo-org/celo-threshold-bls-rs/blob/master/crates/threshold-bls-ffi/src/ffi.rs
+type Buffer* = object
+  `ptr`*: ptr uint8
+  len*: uint
 
+type Auth* = object
+  secret_buffer*: ptr Buffer
+  index*: uint
 
-# pub extern "C" fn hash(
-#     ctx: *const RLN<Bn256>,
-#     inputs_buffer: *const Buffer,
-#     input_len: *const usize,
-#     output_buffer: *mut Buffer,
-# ) -> bool 
-proc hash*(ctx: ptr RLNBn256, inputs_buffer:ptr Buffer, input_len: ptr csize_t, output_buffer: ptr Buffer ) {.importc: "hash".} #TODO not tested yet
+proc hash*(ctx: ptr RLN[Bn256],
+           inputs_buffer: ptr Buffer,
+           input_len: uint,
+           output_buffer: ptr Buffer): bool {.importc: "hash".}
 
-# pub extern "C" fn verify(
-#     ctx: *const RLN<Bn256>,
-#     proof_buffer: *const Buffer,
-#     public_inputs_buffer: *const Buffer,
-#     result_ptr: *mut u32,
-# ) -> bool
+proc key_gen*(ctx: ptr RLN[Bn256], keypair_buffer: ptr Buffer): bool {.importc: "key_gen".}
 
+proc new_circuit_from_params*(merkle_depth: uint,
+                              parameters_buffer: ptr Buffer,
+                              ctx: ptr (ptr RLN[Bn256])): bool {.importc: "new_circuit_from_params".}
 
+proc update_next_member*(ctx: ptr RLN[Bn256],
+                         input_buffer: ptr Buffer): bool {.importc: "update_next_member".}
 
-# pub extern "C" fn generate_proof(
-#     ctx: *const RLN<Bn256>,
-#     input_buffer: *const Buffer,
-#     output_buffer: *mut Buffer,
-# ) -> bool
+proc delete_member*(ctx: ptr RLN[Bn256], index: uint): bool {.importc: "delete_member".}
 
+proc generate_proof*(ctx: ptr RLN[Bn256],
+                     input_buffer: ptr Buffer,
+                     auth: ptr Auth,
+                     output_buffer: ptr Buffer): bool {.importc: "generate_proof".}
+
+proc verify*(ctx: ptr RLN[Bn256],
+             proof_buffer: ptr Buffer,
+             result_ptr: ptr uint32): bool {.importc: "verify".}
 {.pop.}
-
-
