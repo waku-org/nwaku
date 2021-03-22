d31f92aa9e5425a6f0e0de0ee24ff08bf8dc3a31
Sanaz <staheri14@ku.edu.tr>
2021-03-22 16:06:39 -0700

    clean up

--- waku/v2/protocol/waku_rln_relay/rln.nim
+++ waku/v2/protocol/waku_rln_relay/rln.nim
@@ -23,33 +23,18 @@ type Buffer* = object
   `ptr`*: ptr uint8
   len*: uint
 
-type Auth* = object
-  secret_buffer*: ptr Buffer
-  index*: uint
-
-proc hash*(ctx: ptr RLN[Bn256],
-           inputs_buffer: ptr Buffer,
-           input_len: uint,
-           output_buffer: ptr Buffer): bool {.importc: "hash".}
-
 proc key_gen*(ctx: ptr RLN[Bn256], keypair_buffer: ptr Buffer): bool {.importc: "key_gen".}
 
 proc new_circuit_from_params*(merkle_depth: uint,
                               parameters_buffer: ptr Buffer,
                               ctx: ptr (ptr RLN[Bn256])): bool {.importc: "new_circuit_from_params".}
-
+#------------------------------Merkle Tree operations -----------------------------------------
 proc update_next_member*(ctx: ptr RLN[Bn256],
                          input_buffer: ptr Buffer): bool {.importc: "update_next_member".}
 
 proc delete_member*(ctx: ptr RLN[Bn256], index: uint): bool {.importc: "delete_member".}
 
-proc generate_proof*(ctx: ptr RLN[Bn256],
-                     input_buffer: ptr Buffer,
-                     auth: ptr Auth,
-                     output_buffer: ptr Buffer): bool {.importc: "generate_proof".}
 proc get_root*(ctx: ptr RLN[Bn256], output_buffer: ptr Buffer): bool {.importc: "get_root".}
+#----------------------------------------------------------------------------------------------
 
-proc verify*(ctx: ptr RLN[Bn256],
-             proof_buffer: ptr Buffer,
-             result_ptr: ptr uint32): bool {.importc: "verify".}
 {.pop.}
