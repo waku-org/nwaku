7a30505a7e8f66a6b0162be1a371e6c8b61edb92
Sanaz <staheri14@ku.edu.tr>
2021-04-06 14:56:18 -0700

    updates the fields numbers of proof and timestamp

--- waku/v2/protocol/waku_message.nim
+++ waku/v2/protocol/waku_message.nim
@@ -30,8 +30,9 @@ proc init*(T: type WakuMessage, buffer: seq[byte]): ProtoResult[T] =
   discard ? pb.getField(1, msg.payload)
   discard ? pb.getField(2, msg.contentTopic)
   discard ? pb.getField(3, msg.version)
-  discard ? pb.getField(4, msg.proof)
-  discard ? pb.getField(5, msg.timestamp)
+  discard ? pb.getField(4, msg.timestamp)
+  discard ? pb.getField(21, msg.proof)
+
 
   ok(msg)
 
@@ -41,5 +42,5 @@ proc encode*(message: WakuMessage): ProtoBuffer =
   result.write(1, message.payload)
   result.write(2, message.contentTopic)
   result.write(3, message.version)
-  result.write(4, message.proof)
-  result.write(5, message.timestamp)
+  result.write(4, message.timestamp)
+  result.write(21, message.proof)
