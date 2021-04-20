4f431fe69e798b5f8a9dd6f6566861642d8229eb
Sanaz <staheri14@ku.edu.tr>
2021-04-20 09:58:43 -0700

    fix a broken comment line

--- waku/v2/node/storage/message/message_store.nim
+++ waku/v2/node/storage/message/message_store.nim
@@ -4,8 +4,8 @@ import
   ../../../utils/pagination
 
 ## This module defines a message store interface. Implementations of
-## MessageStore are used by the `WakuStore` protocol to store and re-
-## trieve historical messages
+## MessageStore are used by the `WakuStore` protocol to store and 
+## retrieve historical messages
 
 type
   DataProc* = proc(timestamp: uint64, msg: WakuMessage, pubsubTopic: string) {.closure.}
