fcbbe070e6fb94a1a612ad678f286e8b1701b106
Oskar Thoren <ot@oskarthoren.com>
2021-04-22 21:17:23 +0800

    Lightpush types

--- waku/v2/protocol/waku_lightpush/waku_lightpush_types.nim
+++ waku/v2/protocol/waku_lightpush/waku_lightpush_types.nim
@@ -1,1 +1,27 @@
-# Types
+import
+  std/[tables],
+  bearssl,
+  libp2p/peerinfo,
+  libp2p/protocols/protocol,
+  ../../node/peer_manager/peer_manager,
+  ../waku_message
+
+export waku_message
+
+type
+  PushRequest* = object
+    pubsubTopic*: string
+    message*: WakuMessage
+
+  PushResponse* = object
+    isSuccess*: bool
+    info*: string
+
+  PushRPC* = object
+    requestId*: string
+    request*: PushRequest
+    push*: PushResponse
+
+  WakuLightPush* = ref object of LPProtocol
+    rng*: ref BrHmacDrbgContext
+    peerManager*: PeerManager
