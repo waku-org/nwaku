7e9dac67c8927c953687ee6391c63286377bb9c9
Sanaz Taheri Boshrooyeh <35961250+staheri14@users.noreply.github.com>
2021-04-28 04:14:03 -0700

    Integrates pubsub topic filter in the store json rpc api  (#514)
    * replaces topics with seq of ContentFilters
    
    * update topics to contentFilter
    
    * updates the contentFilter  structure
    
     one content  topic per content filter instead of a sequence of topics
    
    * updates store json rpc api
    
    * renames ContentFilter to HistoryContentFilter
    
    * unit test for a query with several content filters
    
    * makes shortcut for store api
    
    * updates chat2
    
    * clean up
    
    * renames topic to contentTopic
    
    * adds pubsub topic to the history query
    
    updates message store interface to return the pubsub topic
    updates waku message store implementation
    updates database schema to hold pubsub topi per waku message
    
    * clarifies the use of content topic in store api
    
    * clarifies the use of contentTopic in the init method of HistoryContentFilter
    
    * simplifies the test and add comments
    
    * lowers the field number of pubsub topic in historyQuery  protobuf
    
    * captures an empty contentFilter case
    
    * test pubsub topic filter for the entire history and no message match
    
    * demoves duplicates
    
    * adds TODO
    
    * fix a broken comment line
    
    * updates waku store codec
    
    * swaps the order of pubsub topic and content topic in protobuf
    
    * Update waku/v2/protocol/waku_store/waku_store_types.nim
    
    Co-authored-by: Oskar Thorén <ot@oskarthoren.com>
    
    * updates the pubsub topic to the default value
    
    * bumps protocol id
    
    * moves the comment close to IndexedWakuMessage
    
    * adds checks to the store put method
    
    * makes table title a constant variable and retitles the table to Message
    
    * updates the changelog
    
    * minor update
    
    * minor
    
    * adds pubsubtopic to the store json rpc api
    
    Co-authored-by: Oskar Thorén <ot@oskarthoren.com>

--- waku/v2/node/scripts/rpc_query.nim
+++ waku/v2/node/scripts/rpc_query.nim
@@ -24,13 +24,18 @@ if paramCount() < 1:
 
 let rpcPort = Port(parseInt(paramStr(1)))
 
-echo "Please enter your topic:"
+echo "Please enter your pubsub topic:"
+let raw_pubsub = readLine(stdin)
+let pubsubTopic = fmt"{raw_pubsub}"
+echo "PubSubTopic is:", pubsubTopic
+
+echo "Please enter your content topic:"
 let raw_input = readLine(stdin)
 let input = fmt"{raw_input}"
-echo "Input is:", input
+echo "Content topic is:", input
 
 var node = newRpcHttpClient()
 waitfor node.connect("localhost", rpcPort)
 
-var res = waitfor node.get_waku_v2_store_v1_messages(@[HistoryContentFilter(contentTopic: ContentTopic(input))], none(StorePagingOptions))
+var res = waitfor node.get_waku_v2_store_v1_messages(pubsubTopic, @[HistoryContentFilter(contentTopic: ContentTopic(input))], none(StorePagingOptions))
 echo "Waku query response: ", res
