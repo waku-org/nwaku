891a2a47f955fc8044ba9be0862180517541dcc0
Hanno Cornelius <68783915+jm-clius@users.noreply.github.com>
2021-05-06 15:43:43 +0200

    Add chat2-matterbridge (#532)
    * Add chat2-matterbridge
    
    * Update waku/common/README.md
    
    Co-authored-by: Oskar Thorén <ot@oskarthoren.com>
    
    Co-authored-by: Oskar Thorén <ot@oskarthoren.com>

---
+++ examples/v2/matterbridge/config_chat2bridge.nim
@@ -0,0 +1,149 @@
+import
+  confutils, confutils/defs, confutils/std/net, chronicles, chronos,
+  libp2p/crypto/[crypto, secp],
+  eth/keys
+
+type
+  Chat2MatterbridgeConf* = object
+    logLevel* {.
+      desc: "Sets the log level"
+      defaultValue: LogLevel.INFO
+      name: "log-level" .}: LogLevel
+
+    listenAddress* {.
+      defaultValue: defaultListenAddress(config)
+      desc: "Listening address for the LibP2P traffic"
+      name: "listen-address"}: ValidIpAddress
+
+    libp2pTcpPort* {.
+      desc: "Libp2p TCP listening port (for Waku v2)"
+      defaultValue: 9000
+      name: "libp2p-tcp-port" .}: uint16
+
+    udpPort* {.
+      desc: "UDP listening port"
+      defaultValue: 9000
+      name: "udp-port" .}: uint16
+
+    portsShift* {.
+      desc: "Add a shift to all default port numbers"
+      defaultValue: 0
+      name: "ports-shift" .}: uint16
+
+    nat* {.
+      desc: "Specify method to use for determining public address. " &
+            "Must be one of: any, none, upnp, pmp, extip:<IP>"
+      defaultValue: "any" .}: string
+
+    rpc* {.
+      desc: "Enable Waku RPC server"
+      defaultValue: false
+      name: "rpc" .}: bool
+
+    rpcAddress* {.
+      desc: "Listening address of the RPC server",
+      defaultValue: ValidIpAddress.init("127.0.0.1")
+      name: "rpc-address" }: ValidIpAddress
+
+    rpcPort* {.
+      desc: "Listening port of the RPC server"
+      defaultValue: 8545
+      name: "rpc-port" .}: uint16
+
+    metricsServer* {.
+      desc: "Enable the metrics server"
+      defaultValue: false
+      name: "metrics-server" .}: bool
+
+    metricsServerAddress* {.
+      desc: "Listening address of the metrics server"
+      defaultValue: ValidIpAddress.init("127.0.0.1")
+      name: "metrics-server-address" }: ValidIpAddress
+
+    metricsServerPort* {.
+      desc: "Listening HTTP port of the metrics server"
+      defaultValue: 8008
+      name: "metrics-server-port" .}: uint16
+
+    ### Waku v2 options
+    staticnodesv2* {.
+      desc: "Multiaddr of peer to directly connect with. Argument may be repeated"
+      name: "staticnodev2" }: seq[string]
+
+    nodekeyv2* {.
+      desc: "P2P node private key as hex"
+      defaultValue: crypto.PrivateKey.random(Secp256k1, newRng()[]).tryGet()
+      name: "nodekeyv2" }: crypto.PrivateKey
+
+    topics* {.
+      desc: "Default topics to subscribe to (space separated list)"
+      defaultValue: "/waku/2/default-waku/proto"
+      name: "topics" .}: string
+
+    store* {.
+      desc: "Flag whether to start store protocol",
+      defaultValue: true
+      name: "store" }: bool
+
+    filter* {.
+      desc: "Flag whether to start filter protocol",
+      defaultValue: false
+      name: "filter" }: bool
+
+    relay* {.
+      desc: "Flag whether to start relay protocol",
+      defaultValue: true
+      name: "relay" }: bool
+
+    storenode* {.
+      desc: "Multiaddr of peer to connect with for waku store protocol"
+      defaultValue: ""
+      name: "storenode" }: string
+
+    filternode* {.
+      desc: "Multiaddr of peer to connect with for waku filter protocol"
+      defaultValue: ""
+      name: "filternode" }: string
+    
+    # Matterbridge options
+    mbHostUri* {.
+      desc: "Matterbridge host API address"
+      defaultValue: "http://127.0.0.1:4242"
+      name: "mb-host-uri" }: string
+    
+    mbGateway* {.
+      desc: "Matterbridge gateway"
+      defaultValue: "gateway1"
+      name: "mb-gateway" }: string
+
+proc parseCmdArg*(T: type keys.KeyPair, p: TaintedString): T =
+  try:
+    let privkey = keys.PrivateKey.fromHex(string(p)).tryGet()
+    result = privkey.toKeyPair()
+  except CatchableError:
+    raise newException(ConfigurationError, "Invalid private key")
+
+proc completeCmdArg*(T: type keys.KeyPair, val: TaintedString): seq[string] =
+  return @[]
+
+proc parseCmdArg*(T: type crypto.PrivateKey, p: TaintedString): T =
+  let key = SkPrivateKey.init(p)
+  if key.isOk():
+    crypto.PrivateKey(scheme: Secp256k1, skkey: key.get())
+  else:
+    raise newException(ConfigurationError, "Invalid private key")
+
+proc completeCmdArg*(T: type crypto.PrivateKey, val: TaintedString): seq[string] =
+  return @[]
+
+proc parseCmdArg*(T: type ValidIpAddress, p: TaintedString): T =
+  try:
+    result = ValidIpAddress.init(p)
+  except CatchableError:
+    raise newException(ConfigurationError, "Invalid IP address")
+
+proc completeCmdArg*(T: type ValidIpAddress, val: TaintedString): seq[string] =
+  return @[]
+
+func defaultListenAddress*(conf: Chat2MatterbridgeConf): ValidIpAddress =
+  (static ValidIpAddress.init("0.0.0.0"))
--- waku/common/README.md
+++ waku/common/README.md
@@ -5,3 +5,4 @@ This folder contains (a) modules that use both Waku v1 and Waku v2. and (b) util
 Examples include:
 - Bridge between v1 and v2
 - NAT traversal
+- interworking with protocols external to Waku (such as Matterbridge)
