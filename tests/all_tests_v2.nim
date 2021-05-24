bc5dca5a6734d71d769e1a3256faf6be33d7ff87
Oskar Thoren <ot@oskarthoren.com>
2021-05-24 12:49:32 +0800

    Separate out wakunode lightpush test

--- tests/all_tests_v2.nim
+++ tests/all_tests_v2.nim
@@ -3,6 +3,7 @@ import
   # TODO: enable this when it is altered into a proper waku relay test
   # ./v2/test_waku,
   ./v2/test_wakunode,
+  ./v2/test_wakunode_light,
   ./v2/test_waku_store,
   ./v2/test_waku_filter,
   ./v2/test_waku_pagination,
