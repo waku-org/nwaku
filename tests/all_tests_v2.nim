40578f42fd4336494a74756cbfb41aaec1072ff5
Oskar Thoren <ot@oskarthoren.com>
2021-05-24 12:51:05 +0800

    Temp disable other tests for Win CI feedback loop

--- tests/all_tests_v2.nim
+++ tests/all_tests_v2.nim
@@ -1,21 +1,22 @@
 import
+  ./v2/test_wakunode_light
   # Waku v2 tests
   # TODO: enable this when it is altered into a proper waku relay test
   # ./v2/test_waku,
-  ./v2/test_wakunode,
-  ./v2/test_wakunode_light,
-  ./v2/test_waku_store,
-  ./v2/test_waku_filter,
-  ./v2/test_waku_pagination,
-  ./v2/test_waku_payload,
-  ./v2/test_waku_swap,
-  ./v2/test_message_store,
-  ./v2/test_jsonrpc_waku,
-  ./v2/test_peer_manager,
-  ./v2/test_web3, # TODO  remove it when rln-relay tests get finalized
-  ./v2/test_waku_rln_relay,
-  ./v2/test_waku_bridge,
-  ./v2/test_peer_storage
+#  ./v2/test_wakunode,
+#  ./v2/test_wakunode_light,
+#  ./v2/test_waku_store,
+#  ./v2/test_waku_filter,
+#  ./v2/test_waku_pagination,
+#  ./v2/test_waku_payload,
+#  ./v2/test_waku_swap,
+#  ./v2/test_message_store,
+#  ./v2/test_jsonrpc_waku,
+#  ./v2/test_peer_manager,
+#  ./v2/test_web3, # TODO  remove it when rln-relay tests get finalized
+#  ./v2/test_waku_rln_relay,
+#  ./v2/test_waku_bridge,
+#  ./v2/test_peer_storage
 
 # TODO Only enable this once swap module is integrated more nicely as a dependency, i.e. as submodule with CI etc
 # For PoC execute it manually and run separate module here: https://github.com/vacp2p/swap-contracts-module
