e5068611222222839140ebcc33089ebf804524a4
Sanaz <staheri14@ku.edu.tr>
2021-06-03 11:53:15 -0700

    adds untitest

--- tests/v2/test_message_store.nim
+++ tests/v2/test_message_store.nim
@@ -1,7 +1,7 @@
 {.used.}
 
 import
-  std/[unittest, options, tables, sets],
+  std/[unittest, options, tables, sets, times],
   chronos, chronicles,
   ../../waku/v2/node/storage/message/waku_message_store,
   ../../waku/v2/protocol/waku_store/waku_store,
@@ -15,10 +15,13 @@ suite "Message Store":
       topic = ContentTopic("/waku/2/default-content/proto")
       pubsubTopic =  "/waku/2/default-waku/proto"
 
+      t1 = epochTime()
+      t2 = epochTime()
+      t3 = high(float64)
     var msgs = @[
-      WakuMessage(payload: @[byte 1, 2, 3], contentTopic: topic, version: uint32(0)),
-      WakuMessage(payload: @[byte 1, 2, 3, 4], contentTopic: topic, version: uint32(1)),
-      WakuMessage(payload: @[byte 1, 2, 3, 4, 5], contentTopic: topic, version: high(uint32)),
+      WakuMessage(payload: @[byte 1, 2, 3], contentTopic: topic, version: uint32(0), timestamp: t1),
+      WakuMessage(payload: @[byte 1, 2, 3, 4], contentTopic: topic, version: uint32(1), timestamp: t2),
+      WakuMessage(payload: @[byte 1, 2, 3, 4, 5], contentTopic: topic, version: high(uint32), timestamp: t3),
     ]
 
     defer: store.close()
@@ -28,16 +31,24 @@ suite "Message Store":
       check output.isOk
 
     var v0Flag, v1Flag, vMaxFlag: bool = false
+    var t1Flag, t2Flag, t3Flag: bool = false
     var responseCount = 0
     proc data(timestamp: uint64, msg: WakuMessage, psTopic: string) =
+      echo $msg
       responseCount += 1
       check msg in msgs
       check psTopic == pubsubTopic
+
       # check the correct retrieval of versions
       if msg.version == uint32(0): v0Flag = true
       if msg.version == uint32(1): v1Flag = true
       # high(uint32) is the largest value that fits in uint32, this is to make sure there is no overflow in the storage
       if msg.version == high(uint32): vMaxFlag = true
+
+      # check correct retrieval of timestamps
+      if msg.timestamp == t1: t1Flag = true
+      if msg.timestamp == t2: t2Flag = true
+      if msg.timestamp == t3: t3Flag = true
 
 
     let res = store.getAll(data)
@@ -45,6 +56,11 @@ suite "Message Store":
     check:
       res.isErr == false
       responseCount == 3
+      # await allFutures(times)
+      t1Flag == true
+      t2Flag == true
+      t3Flag == true
+
       v0Flag == true
       v1Flag == true
       vMaxFlag == true
