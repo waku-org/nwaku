c4e1f81b76cc354050c0eb15d61c243042f72fff
Sanaz <staheri14@ku.edu.tr>
2021-06-11 14:28:25 -0700

    updates script
    fixes a few bugs on the splitScript
    
    
    more debug logs
    
    
    adds float64 parameter support to sqlite3
    
    
    change in timestamp type in the script
    
    
    deletes float64 toBytes utils

--- waku/v2/node/storage/migration/migrations_scripts/message/00002_init.up.sql
+++ waku/v2/node/storage/migration/migrations_scripts/message/00002_init.up.sql
@@ -1,4 +1,3 @@
-/* Copy the accounts table into a temp table, EXCLUDE the `photoPath` and INCLUDE `identicon` column */
 CREATE TABLE IF NOT EXISTS Message_backup (
         id BLOB PRIMARY KEY,
         timestamp INTEGER NOT NULL,
@@ -10,24 +9,19 @@ CREATE TABLE IF NOT EXISTS Message_backup (
 
 INSERT INTO Message_backup SELECT id, timestamp, contentTopic, pubsubTopic, payload, version FROM Message;
 
-/* Drop the old Message table*/
 DROP TABLE Message;
 
-/* create a new Message Table with updated schema*/
 CREATE TABLE IF NOT EXISTS Message(
         id BLOB PRIMARY KEY,
-        receiverTimestamp BLOB NOT NULL,
+        receiverTimestamp REAL NOT NULL,
         contentTopic BLOB NOT NULL,
         pubsubTopic BLOB NOT NULL,
         payload BLOB,
         version INTEGER NOT NULL,
-        senderTimestamp BLOB NOT NULL
+        senderTimestamp REAL NOT NULL
     ) WITHOUT ROWID;
 
-/**
-trnasfer old content to the new table and set their sender timestamp to 0
-**/
+
 INSERT INTO Message SELECT id, timestamp, contentTopic, pubsubTopic, payload, version, 0  FROM Message_backup;
 
-/* Tidy up, drop the backup table */
 DROP TABLE Message_backup;
