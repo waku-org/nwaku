5a6e6467b8744fa7888f9a81d98a0c5abde8fbc1
Sanaz <staheri14@ku.edu.tr>
2021-06-10 17:16:15 -0700

    unit tests for migration utils

--- tests/v2/test_migration_utils.nim
+++ tests/v2/test_migration_utils.nim
@@ -1,16 +1,47 @@
 {.used.}
 
 import
-  std/unittest,
+  std/unittest, tables,strutils, os,
   chronicles,
+  stew/results,
   ../../waku/v2/node/storage/migration/[migration_types, migration_utils]
+
+template sourceDir: string = currentSourcePath.rsplit(DirSep, 1)[0]
+const MIGRATION_PATH = sourceDir / "../../waku/v2/node/storage/migration/migrations_scripts/message"
+
 suite "Migration utils":
   test "read migration scripts":
-    let migrationScripts = getMigrationScripts("/Users/sanaztaheri/GitHub/nim-waku-code/nim-waku/waku/v2/node/storage/migration/migrations_scripts/message")
+    let migrationScriptsRes = getMigrationScripts(MIGRATION_PATH)
     check:
-      migrationScripts.migrationUp.len == 1
+      migrationScriptsRes.isErr == false
+      len((migrationScriptsRes.value).migrationUp) == 1
   test "filter migration scripts":
-    let migrationScripts = getMigrationScripts("/Users/sanaztaheri/GitHub/nim-waku-code/nim-waku/waku/v2/node/storage/migration/migrations_scripts/message")
-    let scripts = filterMigrationScripts(migrationScripts, 0)
+    let migrationScripts = getMigrationScripts(MIGRATION_PATH)
+    let scripts = filterMigrationScripts(migrationScripts.value, 0)
     check:
       scripts.len == 1
+  test "split scripts with multiple queries":
+    let script = "; ;"
+    let queries = splitScript(script)
+    check queries.len == 0
+  test "split scripts with no queries":
+    let q1 = """CREATE TABLE contacts2 (
+                contact_id INTEGER PRIMARY KEY,
+                first_name TEXT NOT NULL,
+                last_name TEXT NOT NULL,
+                email TEXT NOT NULL UNIQUE,
+                phone TEXT NOT NULL UNIQUE
+                );"""
+    let q2 = """CREATE TABLE contacts2 (
+                contact_id INTEGER PRIMARY KEY,
+                first_name TEXT NOT NULL,
+                last_name TEXT NOT NULL,
+                email TEXT NOT NULL UNIQUE,
+                phone TEXT NOT NULL UNIQUE
+                );"""
+    let script = q1 & q2
+    let queries = splitScript(script)
+    check:
+      queries.len == 2
+      queries[0] == q1
+      queries[1] == q2
