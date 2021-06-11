0feb5ac98d64e8accf14164cc2553c3d6a14c2a4
Sanaz <staheri14@ku.edu.tr>
2021-06-11 15:54:31 -0700

    edits docstring

--- waku/v2/node/storage/migration/migration_utils.nim
+++ waku/v2/node/storage/migration/migration_utils.nim
@@ -5,7 +5,7 @@ import
   migration_types
 
 proc getScripts*(migrationPath: string): MigrationScriptsResult[MigrationScripts] =
-  ## this code is borrowed from https://github.com/status-im/nim-status/blob/21aebe41be03cb6450ea261793b800ed7d3e6cda/nim_status/migrations/sql_generate.nim#L4
+  ## the code in this procedure is an adaptation of https://github.com/status-im/nim-status/blob/21aebe41be03cb6450ea261793b800ed7d3e6cda/nim_status/migrations/sql_generate.nim#L4
   var migrationScripts = MigrationScripts(migrationUp:initOrderedTable[string, string](), migrationDown:initOrderedTable[string, string]())
   try:
     for kind, path in walkDir(migrationPath):
