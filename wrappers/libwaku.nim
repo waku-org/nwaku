61b97f6858584303a82e9b48fbdcb2fd5ed2dd1a
Oskar Thoren <ot@oskarthoren.com>
2021-06-13 18:44:19 +0800

    libwaku start node

--- wrappers/libwaku.nim
+++ wrappers/libwaku.nim
@@ -1,6 +1,27 @@
-# libwaku
-#
-# Exposes a C API that can be used by other environment than C.
+## libwaku
+##
+## Exposes a C API that can be used by other environment than C.
+
+import
+  std/[options, tables, strutils, sequtils],
+  chronos, chronicles, metrics,
+  confutils, json_rpc/rpcserver,
+
+  stew/shims/net as stewNet,
+  eth/keys,
+  libp2p/multiaddress,
+  libp2p/crypto/crypto,
+
+  ../waku/v2/node/wakunode2,
+  ../waku/v2/node/config,
+  ../waku/v2/node/jsonrpc/[admin_api,
+                           debug_api,
+                           filter_api,
+                           private_api,
+                           relay_api,
+                           store_api],
+  ../waku/common/utils/nat
+
 
 # TODO Start a node
 # TODO Mock info call
@@ -14,6 +35,22 @@ proc info(foo: cstring): cstring {.exportc, dynlib.} =
   echo "info about node"
   echo foo
   return foo
+
+# XXX Async seems tricky with C here
+proc nwaku_start(): bool {.exportc, dynlib.} =
+  echo "Start WakuNode"
+
+  let
+    conf = WakuNodeConf.load()
+    (extIp, extTcpPort, extUdpPort) = setupNat(conf.nat, clientId,
+      Port(uint16(conf.tcpPort) + conf.portsShift),
+      Port(uint16(conf.udpPort) + conf.portsShift))
+    node = WakuNode.init(conf.nodekey, conf.listenAddress,
+      Port(uint16(conf.tcpPort) + conf.portsShift), extIp, extTcpPort)
+
+  discard node.start()
+  return true
+
 
 proc echo() {.exportc.} =
  echo "echo"
@@ -36,3 +73,8 @@ proc echo() {.exportc.} =
 #main()
 
   # When main done stuff
+
+
+echo "hello there"
+var foo = nwaku_start()
+runForever()
