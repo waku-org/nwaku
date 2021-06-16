ddf93814fe865f66576e80fa056a27ffe864666d
Sanaz Taheri Boshrooyeh <35961250+staheri14@users.noreply.github.com>
2021-06-16 13:23:55 -0700

    Persisting Waku message timestamp & implementing DB migration & convert receiver timestamp data type to float64 (#607)
    * adds timestamp to waku message store impl
    
    * stores timestamp as int64
    
    * adds untitest
    
    * stores timestamp as seq of bytes
    
    * minor
    
    * re-orders unittest
    
    * changes receiver timestamp to float64
    
    * unit test for receiver timestamps
    
    * adds comments
    
    * reorder a few lines
    
    * updates changelog
    
    * more updates on changelog
    
    * WIP
    
    * WIP
    
    * adds migration
    
    * more debug messages
    
    * passes the path to the migration scripts from message store module
    
    * adds migration result type
    
    * replaces migrationScripts with migrationScriptsResult
    
    * adds path calculation to the message store
    
    * removes some tests binary file
    
    * removes redundant imports
    
    * comments out user_version assignment in sqlite  init
    
    * more descriptive err messages
    
    * clean up test file
    
    * more info logs
    
    * minor code format
    
    * removes a todo
    
    * minor updates
    
    * remove a binary file
    
    * unit tests for migration utils
    
    * adds split script
    
    * integrates split query to handle scripts with multiple commands
    
    * updates migration script for v1
    
    * updates the v1 migration script
    
    * update user version
    
    * updates script
    
    * fixes a few bugs on the splitScript
    
    * more debug logs
    
    * adds float64 parameter support to sqlite3
    
    * change in timestamp type in the script
    
    * deletes float64 toBytes utils
    
    * enables storage of timestamp as a real number in the sqlite db
    
    * bump up script index
    
    * comment edits
    
    * removes migrate unit test
    
    * adds todo and docstring
    
    * updates changelog
    
    * removes an unused item in .gitignore
    
    * minor
    
    * updates changelog
    
    * organizes imports
    
    * cleans up imports
    
    * WIP
    
    * updates script
    
    
    fixes a few bugs on the splitScript
    
    
    more debug logs
    
    
    adds float64 parameter support to sqlite3
    
    
    change in timestamp type in the script
    
    
    deletes float64 toBytes utils
    
    * edits migration util test
    
    * remove an empty test file
    
    * includes migration utils tests in
    
    * deletes unused codes
    
    * tides up imports
    
    * adds range based filter to the filterMigrationScripts
    
    * renames procs: removes Migration
    
    * tides up imports
    
    * edits docstring
    
    * edits docstring
    
    * edits docstring
    
    * removes unused imports
    
    * more clean up
    
    * groups std imports
    
    * updates changelog
    
    * adds docstring for setUserVersion
    
    * adds unittest for the migrate
    
    * Update waku/v2/node/storage/message/waku_message_store.nim
    
    Co-authored-by: RichΛrd <info@richardramos.me>
    
    * Update waku/v2/node/storage/sqlite.nim
    
    Co-authored-by: RichΛrd <info@richardramos.me>
    
    * Update waku/v2/node/storage/sqlite.nim
    
    Co-authored-by: RichΛrd <info@richardramos.me>
    
    * removes split scripts
    
    * fixes a naming issue
    
    * fixes a bug
    
    * fixes a typo
    
    * adds a log re updated user_version
    
    * fixes a proc naming mismatch
    
    * fixes naming mismatch
    
    * more descriptive var names
    
    * adds migration script of the first user version
    
    * moves migration to after persistMessages flag is checked
    
    * deletes unused comment
    
    * fixes a bug
    
    * brings back split script
    
    * adds unit tests for split scripts
    
    * runs scripts one command at a time
    
    * deletes a commented line
    
    * relocates the migrate proc to sqlite.nim
    
    * adds unit test for filter scripts
    
    * adds filterScripts unittest testing varying zero-prefixed user versions
    
    * minor
    
    Co-authored-by: RichΛrd <info@richardramos.me>

--- tests/v2/test_message_store.nim
+++ tests/v2/test_message_store.nim
@@ -1,9 +1,10 @@
 {.used.}
 
 import
-  std/[unittest, options, tables, sets],
-  chronos, chronicles,
+  std/[unittest, options, tables, sets, times, os, strutils],
+  chronos,
   ../../waku/v2/node/storage/message/waku_message_store,
+  ../../waku/v2/node/storage/sqlite,
   ../../waku/v2/protocol/waku_store/waku_store,
   ./utils
 
@@ -15,29 +16,53 @@ suite "Message Store":
       topic = ContentTopic("/waku/2/default-content/proto")
       pubsubTopic =  "/waku/2/default-waku/proto"
 
+      t1 = epochTime()
+      t2 = epochTime()
+      t3 = high(float64)
     var msgs = @[
-      WakuMessage(payload: @[byte 1, 2, 3], contentTopic: topic, version: uint32(0)),
-      WakuMessage(payload: @[byte 1, 2, 3, 4], contentTopic: topic, version: uint32(1)),
-      WakuMessage(payload: @[byte 1, 2, 3, 4, 5], contentTopic: topic, version: high(uint32)),
+      WakuMessage(payload: @[byte 1, 2, 3], contentTopic: topic, version: uint32(0), timestamp: t1),
+      WakuMessage(payload: @[byte 1, 2, 3, 4], contentTopic: topic, version: uint32(1), timestamp: t2),
+      WakuMessage(payload: @[byte 1, 2, 3, 4, 5], contentTopic: topic, version: high(uint32), timestamp: t3),
     ]
 
     defer: store.close()
 
+    var indexes: seq[Index] = @[]
     for msg in msgs:
-      let output = store.put(computeIndex(msg), msg, pubsubTopic)
+      var index = computeIndex(msg)
+      let output = store.put(index, msg, pubsubTopic)
       check output.isOk
+      indexes.add(index)
 
+
+    # flags for version
     var v0Flag, v1Flag, vMaxFlag: bool = false
+    # flags for sender timestamp
+    var t1Flag, t2Flag, t3Flag: bool = false
+    # flags for receiver timestamp
+    var rt1Flag, rt2Flag, rt3Flag: bool = false
+
     var responseCount = 0
-    proc data(timestamp: uint64, msg: WakuMessage, psTopic: string) =
+    proc data(receiverTimestamp: float64, msg: WakuMessage, psTopic: string) =
       responseCount += 1
       check msg in msgs
       check psTopic == pubsubTopic
+
       # check the correct retrieval of versions
       if msg.version == uint32(0): v0Flag = true
       if msg.version == uint32(1): v1Flag = true
       # high(uint32) is the largest value that fits in uint32, this is to make sure there is no overflow in the storage
       if msg.version == high(uint32): vMaxFlag = true
+
+      # check correct retrieval of sender timestamps
+      if msg.timestamp == t1: t1Flag = true
+      if msg.timestamp == t2: t2Flag = true
+      if msg.timestamp == t3: t3Flag = true
+
+      # check correct retrieval of receiver timestamps
+      if receiverTimestamp == indexes[0].receivedTime: rt1Flag = true
+      if receiverTimestamp == indexes[1].receivedTime: rt2Flag = true
+      if receiverTimestamp == indexes[2].receivedTime: rt3Flag = true
 
 
     let res = store.getAll(data)
@@ -45,8 +70,45 @@ suite "Message Store":
     check:
       res.isErr == false
       responseCount == 3
+      # check version
       v0Flag == true
       v1Flag == true
       vMaxFlag == true
+      # check sender timestamp
+      t1Flag == true
+      t2Flag == true
+      t3Flag == true
+      # check receiver timestamp
+      rt1Flag == true
+      rt2Flag == true
+      rt3Flag == true
+  test "set and get user version":
+    let 
+      database = SqliteDatabase.init("", inMemory = true)[]
+      store = WakuMessageStore.init(database)[]
+    defer: store.close()
 
+    let res = database.setUserVersion(5)
+    check res.isErr == false
 
+    let ver = database.getUserVersion()
+    check:
+      ver.isErr == false
+      ver.value == 5
+  test "migration":
+    let 
+      database = SqliteDatabase.init("", inMemory = true)[]
+      store = WakuMessageStore.init(database)[]
+    defer: store.close()
+
+    template sourceDir: string = currentSourcePath.rsplit(DirSep, 1)[0]
+    let migrationPath = sourceDir
+
+    let res = database.migrate(migrationPath, 10)
+    check:
+      res.isErr == false
+
+    let ver = database.getUserVersion()
+    check:
+      ver.isErr == false
+      ver.value == 10
--- waku/v2/node/storage/message/waku_message_store.nim
+++ waku/v2/node/storage/message/waku_message_store.nim
@@ -1,8 +1,9 @@
 {.push raises: [Defect].}
 
 import 
+  std/[os, algorithm, tables, strutils],
+  chronos, metrics, chronicles,
   sqlite3_abi,
-  chronos, metrics,
   libp2p/crypto/crypto,
   libp2p/protocols/protocol,
   libp2p/protobuf/minprotobuf,
@@ -11,11 +12,13 @@ import
   ./message_store,
   ../sqlite,
   ../../../protocol/waku_message,
-  ../../../utils/pagination
-
+  ../../../utils/pagination 
 export sqlite
 
 const TABLE_TITLE = "Message"
+template sourceDir: string = currentSourcePath.rsplit(DirSep, 1)[0]
+const MESSAGE_STORE_MIGRATION_PATH* = sourceDir / "../migration/migrations_scripts/message"
+
 # The code in this file is an adaptation of the Sqlite KV Store found in nim-eth.
 # https://github.com/status-im/nim-eth/blob/master/eth/db/kvstore_sqlite3.nim
 #
@@ -30,14 +33,16 @@ proc init*(T: type WakuMessageStore, db: SqliteDatabase): MessageStoreResult[T] 
   ## It contains:
   ##  - 4-Byte ContentTopic stored as an Integer
   ##  - Payload stored as a blob
+
   let prepare = db.prepareStmt("""
     CREATE TABLE IF NOT EXISTS """ & TABLE_TITLE & """ (
         id BLOB PRIMARY KEY,
-        timestamp INTEGER NOT NULL,
+        receiverTimestamp REAL NOT NULL,
         contentTopic BLOB NOT NULL,
         pubsubTopic BLOB NOT NULL,
         payload BLOB,
-        version INTEGER NOT NULL
+        version INTEGER NOT NULL,
+        senderTimestamp REAL NOT NULL
     ) WITHOUT ROWID;
     """, NoParams, void)
 
@@ -61,15 +66,15 @@ method put*(db: WakuMessageStore, cursor: Index, message: WakuMessage, pubsubTop
   ##     echo "error"
   ## 
   let prepare = db.database.prepareStmt(
-    "INSERT INTO " & TABLE_TITLE & " (id, timestamp, contentTopic, payload, pubsubTopic, version) VALUES (?, ?, ?, ?, ?, ?);",
-    (seq[byte], int64, seq[byte], seq[byte], seq[byte], int64),
+    "INSERT INTO " & TABLE_TITLE & " (id, receiverTimestamp, contentTopic, payload, pubsubTopic, version, senderTimestamp) VALUES (?, ?, ?, ?, ?, ?, ?);",
+    (seq[byte], float64, seq[byte], seq[byte], seq[byte], int64, float64),
     void
   )
 
   if prepare.isErr:
     return err("failed to prepare")
 
-  let res = prepare.value.exec((@(cursor.digest.data), int64(cursor.receivedTime), message.contentTopic.toBytes(), message.payload, pubsubTopic.toBytes(), int64(message.version)))
+  let res = prepare.value.exec((@(cursor.digest.data), cursor.receivedTime, message.contentTopic.toBytes(), message.payload, pubsubTopic.toBytes(), int64(message.version), message.timestamp))
   if res.isErr:
     return err("failed")
 
@@ -91,22 +96,31 @@ method getAll*(db: WakuMessageStore, onData: message_store.DataProc): MessageSto
   proc msg(s: ptr sqlite3_stmt) {.raises: [Defect, Exception].} =
     gotMessages = true
     let
-      timestamp = sqlite3_column_int64(s, 0)
+      receiverTimestamp = sqlite3_column_double(s, 0)
+
       topic = cast[ptr UncheckedArray[byte]](sqlite3_column_blob(s, 1))
-      topicL = sqlite3_column_bytes(s,1)
+      topicLength = sqlite3_column_bytes(s,1)
+      contentTopic = ContentTopic(string.fromBytes(@(toOpenArray(topic, 0, topicLength-1))))
+
       p = cast[ptr UncheckedArray[byte]](sqlite3_column_blob(s, 2))
-      l = sqlite3_column_bytes(s, 2)
-      pubsubTopic = cast[ptr UncheckedArray[byte]](sqlite3_column_blob(s, 3))
-      pubsubTopicL = sqlite3_column_bytes(s,3)
+      length = sqlite3_column_bytes(s, 2)
+      payload = @(toOpenArray(p, 0, length-1))
+
+      pubsubTopicPointer = cast[ptr UncheckedArray[byte]](sqlite3_column_blob(s, 3))
+      pubsubTopicLength = sqlite3_column_bytes(s,3)
+      pubsubTopic = string.fromBytes(@(toOpenArray(pubsubTopicPointer, 0, pubsubTopicLength-1)))
+
       version = sqlite3_column_int64(s, 4)
 
-      # TODO retrieve the version number
-    onData(uint64(timestamp),
-           WakuMessage(contentTopic: ContentTopic(string.fromBytes(@(toOpenArray(topic, 0, topicL-1)))),
-                       payload: @(toOpenArray(p, 0, l-1)), version: uint32(version)), 
-                       string.fromBytes(@(toOpenArray(pubsubTopic, 0, pubsubTopicL-1))))
+      senderTimestamp = sqlite3_column_double(s, 5)
 
-  let res = db.database.query("SELECT timestamp, contentTopic, payload, pubsubTopic, version FROM " & TABLE_TITLE & " ORDER BY timestamp ASC", msg)
+
+      # TODO retrieve the version number
+    onData(receiverTimestamp.float64,
+           WakuMessage(contentTopic: contentTopic, payload: payload , version: uint32(version), timestamp: senderTimestamp.float64), 
+                       pubsubTopic)
+
+  let res = db.database.query("SELECT receiverTimestamp, contentTopic, payload, pubsubTopic, version, senderTimestamp FROM " & TABLE_TITLE & " ORDER BY receiverTimestamp ASC", msg)
   if res.isErr:
     return err("failed")
 
@@ -115,3 +129,5 @@ method getAll*(db: WakuMessageStore, onData: message_store.DataProc): MessageSto
 proc close*(db: WakuMessageStore) = 
   ## Closes the database.
   db.database.close()
+
+    
