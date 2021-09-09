b4c7cf9fd613980e1a001b6bca2cf1000fbd1558
Sanaz <staheri14@ku.edu.tr>
2021-09-09 15:13:02 -0700

    reverts an unwanted change

--- tests/v2/test_utils.nim
+++ tests/v2/test_utils.nim
@@ -29,4 +29,4 @@ proc deployContract*(web3: Web3, code: string, gasPrice = 0, contractInput = "")
   return await web3.getMinedTransactionReceipt(r)
 
 proc ethToWei*(eth: UInt256): UInt256 =
-  eth * 1000000000000000000.u256
+  eth * 1000000000000000000.u256 
