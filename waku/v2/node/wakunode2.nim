9ef823a944e28472afc0ee7b0a3f1e1a3807e3e0
Sanaz <staheri14@ku.edu.tr>
2021-09-22 10:41:29 -0700

    minor

--- waku/v2/node/wakunode2.nim
+++ waku/v2/node/wakunode2.nim
@@ -516,7 +516,7 @@ when defined(rln):
       debug "peer is successfully registered into the membership contract"
 
     # adds a topic validator for the supplied pubsub topic
-    # messages published in this pubsub topic will be relayed upon a successful validation, otherwise they will be dropped
+    # messages published on this pubsub topic will be relayed upon a successful validation, otherwise they will be dropped
     # the topic validator checks for the correct non-spamming proof of the message
     addRLNRelayValidator(node, pubsubTopic)
     debug "rln relay topic validator is mounted successfully", pubsubTopic=pubsubTopic
