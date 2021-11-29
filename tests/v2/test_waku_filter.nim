375771c4e9561bb877d6a1284c224cf5f8baf2be
rshiv <reeshav96@gmail.com>
2021-11-29 19:48:02 +0000

    adds test
    Signed-off-by: rshiv <reeshav96@gmail.com>

--- tests/v2/test_waku_filter.nim
+++ tests/v2/test_waku_filter.nim
@@ -144,3 +144,68 @@ procSuite "Waku Filter":
 
     check:
       idOpt.isNone
+
+  asyncTest "Can subscribe and unsubscribe from content filter":
+    const defaultTopic = "/waku/2/default-waku/proto"
+
+    let
+      key = PrivateKey.random(ECDSA, rng[]).get()
+      peer = PeerInfo.new(key)
+      contentTopic = ContentTopic("/waku/2/default-content/proto")
+      post = WakuMessage(payload: @[byte 1, 2, 3], contentTopic: contentTopic)
+
+    var dialSwitch = newStandardSwitch()
+    discard await dialSwitch.start()
+
+    var listenSwitch = newStandardSwitch(some(key))
+    discard await listenSwitch.start()
+
+    var responseCompletionFuture = newFuture[bool]()
+    proc handle(requestId: string, msg: MessagePush) {.gcsafe, closure.} =
+      check:
+        msg.messages.len() == 1
+        msg.messages[0] == post
+      responseCompletionFuture.complete(true)
+
+    let
+      proto = WakuFilter.init(PeerManager.new(dialSwitch), crypto.newRng(), handle)
+      rpc = FilterRequest(contentFilters: @[ContentFilter(contentTopic: contentTopic)], pubSubTopic: defaultTopic, subscribe: true)
+
+    dialSwitch.mount(proto)
+    proto.setPeer(listenSwitch.peerInfo.toRemotePeerInfo())
+
+    proc emptyHandle(requestId: string, msg: MessagePush) {.gcsafe, closure.} =
+      discard
+
+    let proto2 = WakuFilter.init(PeerManager.new(listenSwitch), crypto.newRng(), emptyHandle)
+
+    listenSwitch.mount(proto2)
+
+    let id = (await proto.subscribe(rpc)).get()
+
+    await sleepAsync(2.seconds)
+
+    await proto2.handleMessage(defaultTopic, post)
+
+    check:
+      # Check that subscription works as expected
+      (await responseCompletionFuture.withTimeout(3.seconds)) == true
+    
+    # Reset to test unsubscribe
+    responseCompletionFuture = newFuture[bool]()
+
+    echo $proto.peerManager.peers()
+    discard dialSwitch.stop()
+    await sleepAsync(2.seconds)
+    #discard await listenSwitch.start()
+    await sleepAsync(2.seconds)
+    #listenSwitch.mount(proto2)
+    #await proto.unsubscribe(rpcU)
+    #await sleepAsync(2.seconds)
+    await proto2.handleMessage(defaultTopic, post)
+    await sleepAsync(2000.millis)
+    echo $proto.peerManager.peers()
+    await proto2.handleMessage(defaultTopic, post)
+    await sleepAsync(2000.millis)
+    echo $proto.peerManager.peers()
+    
