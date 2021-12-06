fffcdb25bec4a2c4a7613bdc2af6e361bc78c61c
Hanno Cornelius <hanno@status.im>
2021-12-06 19:04:12 +0100

    Improved tests. Added common test vector.

--- tests/v2/test_enr_utils.nim
+++ tests/v2/test_enr_utils.nim
@@ -34,12 +34,14 @@ procSuite "ENR utils":
       empty.toMultiAddresses().len() == 0
 
   asyncTest "Init ENR for Waku Usage":
+    # Tests RFC31 encoding "happy path"
     let
       enrIp = ValidIpAddress.init("127.0.0.1")
       enrTcpPort, enrUdpPort = Port(60000)
       enrKey = PrivateKey.random(Secp256k1, rng[])[]
       wakuFlags = initWakuFlags(false, true, false, true)
-      multiaddrs = @[MultiAddress.init("/ip4/127.0.0.1/tcp/442/ws")[], MultiAddress.init("/ip4/127.0.0.1/tcp/443/wss/p2p/16Uiu2HAm4v86W3bmT1BiH6oSPzcsSr31iDQpSN5Qa882BCjjwgrD")[]]
+      multiaddrs = @[MultiAddress.init("/ip4/127.0.0.1/tcp/442/ws")[],
+                     MultiAddress.init("/ip4/127.0.0.1/tcp/443/wss")[]]
 
     let
       record = initEnr(enrKey, some(enrIp),
@@ -62,4 +64,58 @@ procSuite "ENR utils":
     check:
       decodedFlags == @[wakuFlags.byte]
       decodedAddrs.contains(MultiAddress.init("/ip4/127.0.0.1/tcp/442/ws")[])
-      decodedAddrs.contains(MultiAddress.init("/ip4/127.0.0.1/tcp/443/wss")[]) # Peer Id has been stripped
+      decodedAddrs.contains(MultiAddress.init("/ip4/127.0.0.1/tcp/443/wss")[])
+  
+  asyncTest "Strip multiaddr peerId":
+    # Tests that peerId is stripped of multiaddrs as per RFC31
+    let
+      enrIp = ValidIpAddress.init("127.0.0.1")
+      enrTcpPort, enrUdpPort = Port(60000)
+      enrKey = PrivateKey.random(Secp256k1, rng[])[]
+      multiaddrs = @[MultiAddress.init("/ip4/127.0.0.1/tcp/443/wss/p2p/16Uiu2HAm4v86W3bmT1BiH6oSPzcsSr31iDQpSN5Qa882BCjjwgrD")[]]
+
+    let
+      record = initEnr(enrKey, some(enrIp),
+                       some(enrTcpPort), some(enrUdpPort),
+                       none(WakuEnrBitfield),
+                       multiaddrs)
+
+    # Check Waku ENR fields
+    let
+      decodedAddrs = record.get(MULTIADDR_ENR_FIELD, seq[byte]).toMultiAddresses()
+    
+    check decodedAddrs.contains(MultiAddress.init("/ip4/127.0.0.1/tcp/443/wss")[]) # Peer Id has been stripped
+
+  asyncTest "Decode ENR with multiaddrs field":
+    let
+      # Known values correspond to shared test vectors with other Waku implementations
+      knownIp = ValidIpAddress.init("18.223.219.100")
+      knownUdpPort = some(9000.int)
+      knownTcpPort = none(int)
+      knownMultiaddrs = @[MultiAddress.init("/dns4/node-01.do-ams3.wakuv2.test.statusim.net/tcp/443/wss")[],
+                          MultiAddress.init("/dns6/node-01.ac-cn-hongkong-c.wakuv2.test.statusim.net/tcp/443/wss")[]]
+      knownEnr = "enr:-QEnuEBEAyErHEfhiQxAVQoWowGTCuEF9fKZtXSd7H_PymHFhGJA3rGAYDVSH" &
+                  "KCyJDGRLBGsloNbS8AZF33IVuefjOO6BIJpZIJ2NIJpcIQS39tkim11bHRpYWRkcn" &
+                  "O4lgAvNihub2RlLTAxLmRvLWFtczMud2FrdXYyLnRlc3Quc3RhdHVzaW0ubmV0BgG" &
+                  "73gMAODcxbm9kZS0wMS5hYy1jbi1ob25na29uZy1jLndha3V2Mi50ZXN0LnN0YXR1" &
+                  "c2ltLm5ldAYBu94DACm9A62t7AQL4Ef5ZYZosRpQTzFVAB8jGjf1TER2wH-0zBOe1" &
+                  "-MDBNLeA4lzZWNwMjU2azGhAzfsxbxyCkgCqq8WwYsVWH7YkpMLnU2Bw5xJSimxKav-g3VkcIIjKA"
+
+    var enrRecord: Record
+    check:
+      enrRecord.fromURI(knownEnr)
+    
+    let typedRecord = enrRecord.toTypedRecord.get()
+
+     # Check EIP-778 ENR fields
+    check:
+      ipv4(typedRecord.ip.get()) == knownIp
+      typedRecord.tcp == knownTcpPort
+      typedRecord.udp == knownUdpPort
+    
+    # Check Waku ENR fields
+    let
+      decodedAddrs = enrRecord.get(MULTIADDR_ENR_FIELD, seq[byte]).toMultiAddresses()
+    
+    for knownMultiaddr in knownMultiaddrs:
+      check decodedAddrs.contains(knownMultiaddr)
