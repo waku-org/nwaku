988453f287313d3b4461c61140e7dbb97504965b
Hanno Cornelius <68783915+jm-clius@users.noreply.github.com>
2022-03-18 12:04:11 +0200

    Fix: timestamp type could overflow before conversion (#892)

--- waku/v2/utils/time.nim
+++ waku/v2/utils/time.nim
@@ -9,15 +9,15 @@ type Timestamp* = int64
 const TIMESTAMP_TABLE_TYPE* = "INTEGER"
 
 proc getNanosecondTime*[T](timeInSeconds: T): Timestamp = 
-  var ns = Timestamp(timeInSeconds*1000_000_000)
+  var ns = Timestamp(timeInSeconds.int64 * 1000_000_000.int64)
   return ns
 
 proc getMicrosecondTime*[T](timeInSeconds: T): Timestamp = 
-  var us = Timestamp(timeInSeconds*1000_000)
+  var us = Timestamp(timeInSeconds.int64 * 1000_000.int64)
   return us
 
 proc getMillisecondTime*[T](timeInSeconds: T): Timestamp = 
-  var ms = Timestamp(timeInSeconds*1000)
+  var ms = Timestamp(timeInSeconds.int64 * 1000.int64)
   return ms
 
 proc column_timestamp*(a1: ptr sqlite3_stmt, iCol: cint): int64 =
