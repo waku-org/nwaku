36b1104ed0a4056448f00bccd2c4b4ef182a5c34
s1fr0 <28568419+s1fr0@users.noreply.github.com>
2022-04-04 14:26:37 +0200

    refactor(noise): address reviewer's comments

--- waku/v2/protocol/waku_noise/noise.nim
+++ waku/v2/protocol/waku_noise/noise.nim
@@ -43,15 +43,34 @@ type
   NoiseError* = object of LPError
   NoiseDecryptTagError* = object of NoiseError
 
+#################################################################
+
+# Utilities
+
+# Generates random byte sequences of given size
+proc randomSeqByte*(rng: var BrHmacDrbgContext, size: uint32): seq[byte] =
+  var output = newSeq[byte](size)
+  brHmacDrbgGenerate(rng, output)
+  return output
+
+#################################################################
+
+# ChaChaPoly Symmetric Cipher
+
 # ChaChaPoly encryption
+# It takes a Cipher State (with key, nonce, and associated data) and encrypts a plaintext
 proc encrypt*(
     state: ChaChaPolyCipherState,
     plaintext: openArray[byte]): ChaChaPolyCiphertext
     {.noinit, raises: [Defect].} =
   #TODO: add padding
-  result.data.add plaintext
-  ChaChaPoly.encrypt(state.k, state.nonce, result.tag, result.data, state.ad)
+  var ciphertext: ChaChaPolyCiphertext
+  ciphertext.data.add plaintext
+  ChaChaPoly.encrypt(state.k, state.nonce, ciphertext.tag, ciphertext.data, state.ad)
+  return ciphertext
 
+# ChaChaPoly decryption
+# It takes a Cipher State (with key, nonce, and associated data) and decrypts a ciphertext
 proc decrypt*(
     state: ChaChaPolyCipherState, 
     ciphertext: ChaChaPolyCiphertext): seq[byte]
@@ -59,17 +78,20 @@ proc decrypt*(
   var
     tagIn = ciphertext.tag
     tagOut: ChaChaPolyTag
-  result = ciphertext.data
-  ChaChaPoly.decrypt(state.k, state.nonce, tagOut, result, state.ad)
+  var plaintext = ciphertext.data
+  ChaChaPoly.decrypt(state.k, state.nonce, tagOut, plaintext, state.ad)
   #TODO: add unpadding
   trace "decrypt", tagIn = tagIn.shortLog, tagOut = tagOut.shortLog, nonce = state.nonce
   if tagIn != tagOut:
-    debug "decrypt failed", result = shortLog(result)
+    debug "decrypt failed", plaintext = shortLog(plaintext)
     raise newException(NoiseDecryptTagError, "decrypt tag authentication failed.")
+  return plaintext
 
-
+# Generates a random Cipher Test for testing encryption/decryption
 proc randomChaChaPolyCipherState*(rng: var BrHmacDrbgContext): ChaChaPolyCipherState =
-  brHmacDrbgGenerate(rng, result.k)
-  brHmacDrbgGenerate(rng, result.nonce)
-  result.ad = newSeq[byte](32)
-  brHmacDrbgGenerate(rng, result.ad)
+  var randomCipherState: ChaChaPolyCipherState
+  brHmacDrbgGenerate(rng, randomCipherState.k)
+  brHmacDrbgGenerate(rng, randomCipherState.nonce)
+  randomCipherState.ad = newSeq[byte](32)
+  brHmacDrbgGenerate(rng, randomCipherState.ad)
+  return randomCipherState
