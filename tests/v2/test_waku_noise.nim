9d12464f1d0aaa17ff99ec02c324ce710702f725
s1fr0 <28568419+s1fr0@users.noreply.github.com>
2022-04-04 14:27:10 +0200

    test(noise): add test for ChaChaPoly Encryption/decryption

--- tests/v2/test_waku_noise.nim
+++ tests/v2/test_waku_noise.nim
@@ -4,3 +4,20 @@ import
   testutils/unittests,
   ../../waku/v2/protocol/waku_noise/noise,
   ../test_helpers
+
+
+procSuite "Waku Noise":
+  
+  let rng = rng()
+
+  test "ChaChaPoly Encryption/Decryption":
+
+    let cipherState = randomChaChaPolyCipherState(rng[])
+
+    let
+      plaintext: seq[byte] = randomSeqByte(rng[], 128)
+      ciphertext: ChaChaPolyCiphertext = encrypt(cipherState, plaintext)
+      dec_ciphertext: seq[byte] = decrypt(cipherState, ciphertext)
+
+    check: 
+      plaintext == dec_ciphertext
