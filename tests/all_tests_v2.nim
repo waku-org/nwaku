f4ced6f9939ef7c5d2d8cb53e4f321c33e6f8599
Sanaz <staheri14@ku.edu.tr>
2022-04-11 15:54:07 -0700

    adds the conditional compilation for onchain tests

--- tests/all_tests_v2.nim
+++ tests/all_tests_v2.nim
@@ -26,8 +26,9 @@ import
   ./v2/test_waku_noise
 
 when defined(rln):
-  import ./v2/test_waku_rln_relay
-  import ./v2/test_waku_rln_relay_onchain
+  # import ./v2/test_waku_rln_relay
+  when defined(onchain_rln):
+    import ./v2/test_waku_rln_relay_onchain
 
 
 # TODO Only enable this once swap module is integrated more nicely as a dependency, i.e. as submodule with CI etc
