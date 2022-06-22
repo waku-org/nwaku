a01dc0d0d174907116eb3035386b5ccc50e4fb68
Sanaz <staheri14@ku.edu.tr>
2022-06-22 09:44:12 -0700

    revises further

--- waku/v2/protocol/waku_rln_relay/rln.nim
+++ waku/v2/protocol/waku_rln_relay/rln.nim
@@ -32,20 +32,22 @@ type Auth* = object
 proc update_next_member*(ctx: RLN[Bn256],
                         input_buffer: ptr Buffer): bool {.importc: "update_next_member".}
 ## input_buffer points to the id commitment byte seq
+## the return bool value indicates the success or failure of the operation
 
 proc delete_member*(ctx: RLN[Bn256], index: uint): bool {.importc: "delete_member".}
 ## index is the position of the id commitment key to be deleted from the tree
-
+## the return bool value indicates the success or failure of the operation
 
 proc get_root*(ctx: RLN[Bn256], output_buffer: ptr Buffer): bool {.importc: "get_root".}
 ## get_root populates the passed pointer output_buffer with the current tree root
-## the passed buffer len must match the Merkle tree root size (which depends on the underlying lib)
-
+## the output_buffer holds the Merkle tree root of size 32 bytes
+## the return bool value indicates the success or failure of the operation
+## 
 #----------------------------------------------------------------------------------------------
 #-------------------------------- zkSNARKs operations -----------------------------------------
 proc key_gen*(ctx: RLN[Bn256], keypair_buffer: ptr Buffer): bool {.importc: "key_gen".}
 ## generates id key and id commitment key serialized inside keypair_buffer as | id_key <32 bytes>| id_commitment_key <32 bytes> |
-
+## the return bool value indicates the success or failure of the operation
 
 proc generate_proof*(ctx: RLN[Bn256],
                     input_buffer: ptr Buffer,
@@ -53,32 +55,35 @@ proc generate_proof*(ctx: RLN[Bn256],
 ## input_buffer serialized as  [ id_key<32> | id_index<8> | epoch<32> | signal_len<8> | signal<var> ]
 ## output_buffer holds the proof data and should be parsed as |proof<256>|root<32>|epoch<32>|share_x<32>|share_y<32>|nullifier<32>|
 ## integers wrapped in <> indicate value sizes in bytes
-
+## the return bool value indicates the success or failure of the operation
+## 
 proc verify*(ctx: RLN[Bn256],
             proof_buffer: ptr Buffer,
             result_ptr: ptr uint32): bool {.importc: "verify".}
 ## proof_buffer [ proof<256>| root<32>| epoch<32>| share_x<32>| share_y<32>| nullifier<32> | signal_len<8> | signal<var> ]
-
+## the return bool value indicates the success or failure of the verification
 
 
 #----------------------------------------------------------------------------------------------
 #-------------------------------- Common procedures -------------------------------------------
-# creates an instance of rln object as defined by the rln lib https://github.com/kilic/rln/blob/7ac74183f8b69b399e3bc96c1ae8ab61c026dc43/src/public.rs#L48
-# merkle_depth represent the depth of the Merkle tree
-# parameters_buffer holds prover and verifier keys
-# ctx holds the final created rln object
+
 proc new_circuit_from_params*(merkle_depth: uint,
                               parameters_buffer: ptr Buffer,
                               ctx: ptr RLN[Bn256]): bool {.importc: "new_circuit_from_params".}
+## creates an instance of rln object as defined by the rln lib https://github.com/kilic/rln/blob/7ac74183f8b69b399e3bc96c1ae8ab61c026dc43/src/public.rs#L48
+## merkle_depth represent the depth of the Merkle tree
+## parameters_buffer holds prover and verifier keys
+## ctx holds the final created rln object
+## the return bool value indicates the success or failure of the operation
 
 
-# as explained in https://github.com/kilic/rln/blob/7ac74183f8b69b399e3bc96c1ae8ab61c026dc43/src/public.rs#L135, it hashes (sha256) the plain text supplied in inputs_buffer and then maps it to a field element
-# this proc is used to map arbitrary signals to field element for the sake of proof generation
-# inputs_buffer holds the hash input as a byte seq 
-# the hash output is generated and populated inside output_buffer 
-# the length of the output_buffer should match the output of the hash function 
 proc hash*(ctx: RLN[Bn256],
           inputs_buffer: ptr Buffer,
           output_buffer: ptr Buffer): bool {.importc: "signal_to_field".}
+## as explained in https://github.com/kilic/rln/blob/7ac74183f8b69b399e3bc96c1ae8ab61c026dc43/src/public.rs#L135, it hashes (sha256) the plain text supplied in inputs_buffer and then maps it to a field element
+## this proc is used to map arbitrary signals to field element for the sake of proof generation
+## inputs_buffer holds the hash input as a byte seq 
+## the hash output is generated and populated inside output_buffer 
+## the output_buffer contains 32 bytes hash output 
 
 {.pop.}
