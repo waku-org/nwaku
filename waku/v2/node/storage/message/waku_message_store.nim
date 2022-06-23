589b707bf6f5d552f2375999cad1a7bcb68df400
Daniel Kaiser <git@kais3r.de>
2022-06-23 15:01:31 +0200

    Update waku/v2/node/storage/message/waku_message_store.nim
    Co-authored-by: Lorenzo Delgado <lorenzo@status.im>

--- waku/v2/node/storage/message/waku_message_store.nim
+++ waku/v2/node/storage/message/waku_message_store.nim
@@ -273,7 +273,8 @@ proc adjustDbPageSize(dbPageSize: uint64, matchCount: uint64, returnPageSize: ui
   var ret =
     if matchCount < 2: dbPageSize * returnPageSize
     else: dbPageSize * (returnPageSize div matchCount)
-  if ret >= maxDbPageSize: ret = maxDbPageSize
+  if ret >= maxDbPageSize:
+    ret = maxDbPageSize
   trace "dbPageSize adjusted to: ",  ret
   ret
 
