330d69c6d65ec004ff7869d2a1cc585766e6ae93
LNSD <LNSD@users.noreply.github.com>
2022-09-12 14:04:18 +0000

    deploy: 81526483a75c36307c59dd1f272729729ebbedfc

--- waku/v2/utils/time.nim
+++ waku/v2/utils/time.nim
@@ -1,12 +1,8 @@
 ## Contains types and utilities for timestamps.
-
 {.push raises: [Defect].}
 
-import sqlite3_abi
 
 type Timestamp* = int64 
-
-const TIMESTAMP_TABLE_TYPE* = "INTEGER"
 
 proc getNanosecondTime*[T](timeInSeconds: T): Timestamp = 
   var ns = Timestamp(timeInSeconds.int64 * 1000_000_000.int64)
@@ -19,6 +15,3 @@ proc getMicrosecondTime*[T](timeInSeconds: T): Timestamp =
 proc getMillisecondTime*[T](timeInSeconds: T): Timestamp = 
   var ms = Timestamp(timeInSeconds.int64 * 1000.int64)
   return ms
-
-proc column_timestamp*(a1: ptr sqlite3_stmt, iCol: cint): int64 =
-  return sqlite3_column_int64(a1, iCol)
