bf13a905870b904ea16443fea87cdcf34c1818cf
Hanno Cornelius <hanno@status.im>
2022-09-21 15:32:06 +0100

    fix: signing key for wakuv2 DNS lists has changed

--- examples/v2/chat2.nim
+++ examples/v2/chat2.nim
@@ -406,10 +406,10 @@ proc processInput(rfd: AsyncFD) {.async.} =
     echo "Connecting to " & $conf.fleet & " fleet using DNS discovery..."
     
     if conf.fleet == Fleet.test:
-      dnsDiscoveryUrl = some("enrtree://AOFTICU2XWDULNLZGRMQS4RIZPAZEHYMV4FYHAPW563HNRAOERP7C@test.waku.nodes.status.im")
+      dnsDiscoveryUrl = some("enrtree://AOGECG2SPND25EEFMAJ5WF3KSGJNSGV356DSTL2YVLLZWIV6SAYBM@test.waku.nodes.status.im")
     else:
       # Connect to prod by default
-      dnsDiscoveryUrl = some("enrtree://ANTL4SLG2COUILKAPE7EF2BYNL2SHSHVCHLRD5J7ZJLN5R3PRJD2Y@prod.waku.nodes.status.im")
+      dnsDiscoveryUrl = some("enrtree://AOGECG2SPND25EEFMAJ5WF3KSGJNSGV356DSTL2YVLLZWIV6SAYBM@prod.waku.nodes.status.im")
 
   elif conf.dnsDiscovery and conf.dnsDiscoveryUrl != "":
     # No pre-selected fleet. Discover nodes via DNS using user config
