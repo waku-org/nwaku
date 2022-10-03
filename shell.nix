eba17319e81f67ef17148cabd7d7961ea4ec1782
Jakub Sokołowski <jakub@status.im>
2022-10-03 13:23:42 +0200

    ci: use Nix shell to provide build dependencies
    Because we need Rust version newer than `1.62.0` and
    Ubuntu 22.04 only comes with `1.61.0`:
    https://packages.ubuntu.com/jammy/rustc
    
    And locking dependencies using Nix would make it easier
    for developers to track them and update them:
    https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/compilers/rust/1_63.nix
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import (builtins.fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/dbf1d73cd1a17276196afeee169b4cf7834b7a96.tar.gz";
+  sha256 = "sha256:1k5nvn2yzw370cqsfh62lncsgydq2qkbjrx34cprzf0k6b93v7ch";
+}) {} }:
+
+pkgs.mkShell {
+  name = "nim-waku-build-shell";
+
+  # Versions dependent on nixpkgs commit. Update manually.
+  buildInputs = with pkgs; [
+    git   # 2.37.3
+    which # 2.21
+    gcc   # 11.3.0
+    rustc # 1.63.0
+  ] ++ lib.optionals stdenv.isDarwin [ libiconv ];
+}
