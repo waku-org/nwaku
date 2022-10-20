3dbb084d4ca4a88960bcf259a400820760ee8f9f
Lorenzo Delgado <lorenzo@status.im>
2022-10-20 18:09:40 +0200

    feat(store): add waku store client module

--- tests/all_tests_v2.nim
+++ tests/all_tests_v2.nim
@@ -13,6 +13,7 @@ import
   ./v2/test_message_store_sqlite,
   ./v2/test_waku_store_rpc_codec,
   ./v2/test_waku_store,
+  ./v2/test_waku_store_client,
   # TODO: Re-enable store resume test cases (#1282)
   # ./v2/test_waku_store_resume,
   ./v2/test_wakunode_store,
