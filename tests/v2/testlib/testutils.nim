86a42a927429f08d40c089ec66fd9c2580a8ab9b
alrevuelta <alrevuelta@users.noreply.github.com>
2022-10-28 13:44:24 +0000

    deploy: 90b5c8a775091217cb986fd6fec4a5f389f5d745

---
+++ tests/v2/testlib/testutils.nim
@@ -0,0 +1,12 @@
+template suitex*(name: string, body: untyped) = discard
+template xsuite*(name: string, body: untyped) = discard
+
+template testx*(name: string, body: untyped) = discard
+template xtest*(name: string, body: untyped) = discard
+
+template procSuitex*(name: string, body: untyped) = discard
+template xprocSuite*(name: string, body: untyped) = discard
+
+template asyncTestx*(name: string, body: untyped) = discard
+template xasyncTest*(name: string, body: untyped) = discard
+  
