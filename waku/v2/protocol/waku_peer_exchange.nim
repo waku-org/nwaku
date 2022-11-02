bd5c2bf602bdd6a9ffb8cfef52cb5a72c61f63a2
Lorenzo Delgado <lorenzo@status.im>
2022-11-02 09:45:21 +0100

    refactor(peer-exchange): move peer management to waku_node module

--- waku/v2/protocol/waku_peer_exchange.nim
+++ waku/v2/protocol/waku_peer_exchange.nim
@@ -3,11 +3,8 @@
 import
   ./waku_peer_exchange/rpc,
   ./waku_peer_exchange/rpc_codec,
-  ./waku_peer_exchange/protocol,
-  ./waku_peer_exchange/client
-
+  ./waku_peer_exchange/protocol
 export
   rpc,
   rpc_codec,
-  protocol,
-  client
+  protocol
