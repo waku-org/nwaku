35c3040a7e46c125da8d1b8c3f0eb5bd41a712a9
LNSD <LNSD@users.noreply.github.com>
2022-11-03 18:46:39 +0000

    deploy: 2ff90443d60808f934c2874240654c5fbe4b24d5

---
+++ waku/common/envvar_serialization.nim
@@ -0,0 +1,63 @@
+when (NimMajor, NimMinor) < (1, 4):
+  {.push raises: [Defect].}
+else:
+  {.push raises: [].}
+
+
+import
+  stew/shims/macros,
+  serialization
+import
+   ./envvar_serialization/reader, 
+   ./envvar_serialization/writer
+
+export
+  serialization, 
+  reader, 
+  writer
+
+
+serializationFormat Envvar
+
+Envvar.setReader EnvvarReader
+Envvar.setWriter EnvvarWriter, PreferredOutput = void
+
+
+template supports*(_: type Envvar, T: type): bool =
+  # The Envvar format should support every type
+  true
+
+template decode*(_: type Envvar,
+                 prefix: string,
+                 RecordType: distinct type,
+                 params: varargs[untyped]): auto =
+  mixin init, ReaderType
+
+  {.noSideEffect.}:
+    var reader = unpackArgs(init, [EnvvarReader, prefix, params])
+    reader.readValue(RecordType)
+
+template encode*(_: type Envvar,
+                 prefix: string,
+                 value: auto,
+                 params: varargs[untyped]) =
+  mixin init, WriterType, writeValue
+
+  {.noSideEffect.}:
+    var writer = unpackArgs(init, [EnvvarWriter, prefix, params])
+    writeValue writer, value
+
+template loadFile*(_: type Envvar,
+                   prefix: string,
+                   RecordType: distinct type,
+                   params: varargs[untyped]): auto =
+  mixin init, ReaderType, readValue
+
+  var reader = unpackArgs(init, [EnvvarReader, prefix, params])
+  reader.readValue(RecordType)
+
+template saveFile*(_: type Envvar, prefix: string, value: auto, params: varargs[untyped]) =
+  mixin init, WriterType, writeValue
+
+  var writer = unpackArgs(init, [EnvvarWriter, prefix, params])
+  writer.writeValue(value)
---
+++ waku/common/envvar_serialization/reader.nim
@@ -0,0 +1,95 @@
+when (NimMajor, NimMinor) < (1, 4):
+  {.push raises: [Defect].}
+else:
+  {.push raises: [].}
+
+
+import
+  std/[tables, typetraits, options, os],
+  serialization/object_serialization,
+  serialization/errors
+import
+  ./utils
+
+
+type
+  EnvvarReader* = object
+    prefix: string
+    key: seq[string]
+
+  EnvvarError* = object of SerializationError
+
+  EnvvarReaderError* = object of EnvvarError
+
+  GenericEnvvarReaderError* = object of EnvvarReaderError
+    deserializedField*: string
+    innerException*: ref CatchableError
+
+
+proc handleReadException*(r: EnvvarReader,
+                          Record: type,
+                          fieldName: string,
+                          field: auto,
+                          err: ref CatchableError) {.raises: [GenericEnvvarReaderError].} =
+  var ex = new GenericEnvvarReaderError
+  ex.deserializedField = fieldName
+  ex.innerException = err
+  raise ex
+
+proc init*(T: type EnvvarReader, prefix: string): T =
+  result.prefix = prefix
+
+proc readValue*[T](r: var EnvvarReader, value: var T) {.raises: [ValueError, SerializationError].} =
+  mixin readValue
+
+  when T is string:
+    let key = constructKey(r.prefix, r.key)
+    value = os.getEnv(key)
+
+  elif T is (SomePrimitives or range):
+    let key = constructKey(r.prefix, r.key)
+    getValue(key, value)
+
+  elif T is Option:
+    template getUnderlyingType[T](_: Option[T]): untyped = T
+    let key = constructKey(r.prefix, r.key)
+    if os.existsEnv(key):
+      type uType = getUnderlyingType(value)
+      when uType is string:
+        value = some(os.getEnv(key))
+      else:
+        value = some(r.readValue(uType))
+
+  elif T is (seq or array):
+    when uTypeIsPrimitives(T):
+      let key = constructKey(r.prefix, r.key)
+      getValue(key, value)
+
+    else:
+      let key = r.key[^1]
+      for i in 0..<value.len:
+        r.key[^1] = key & $i
+        r.readValue(value[i])
+
+  elif T is (object or tuple):
+    type T = type(value)
+    when T.totalSerializedFields > 0:
+      let fields = T.fieldReadersTable(EnvvarReader)
+      var expectedFieldPos = 0
+      r.key.add ""
+      value.enumInstanceSerializedFields(fieldName, field):
+        when T is tuple:
+          r.key[^1] = $expectedFieldPos
+          var reader = fields[][expectedFieldPos].reader
+          expectedFieldPos += 1
+        else:
+          r.key[^1] = fieldName
+          var reader = findFieldReader(fields[], fieldName, expectedFieldPos)
+
+        if reader != nil:
+          reader(value, r)
+      discard r.key.pop()
+
+  else:
+    const typeName = typetraits.name(T)
+    {.fatal: "Failed to convert from Envvar an unsupported type: " & typeName.}
