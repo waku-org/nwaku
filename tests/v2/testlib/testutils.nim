d34c3bf41d5a284c59d2ef08d6eeb0f83ed93398
Lorenzo Delgado <lorenzo@status.im>
2022-11-03 17:58:28 +0100

    feat(testlib): mark commented test cases as skipped

--- tests/v2/testlib/testutils.nim
+++ tests/v2/testlib/testutils.nim
@@ -1,12 +1,25 @@
-template suitex*(name: string, body: untyped) = discard
+import
+  testutils/unittests
+
 template xsuite*(name: string, body: untyped) = discard
+template suitex*(name: string, body: untyped) = discard
 
-template testx*(name: string, body: untyped) = discard
-template xtest*(name: string, body: untyped) = discard
-
-template procSuitex*(name: string, body: untyped) = discard
 template xprocSuite*(name: string, body: untyped) = discard
+template procSuitex*(name: string, body: untyped) = discard
 
-template asyncTestx*(name: string, body: untyped) = discard
-template xasyncTest*(name: string, body: untyped) = discard
-  
+
+template xtest*(name: string, body: untyped) =
+  test name:
+    skip()
+
+template testx*(name: string, body: untyped) =
+  test name:
+    skip()
+
+template xasyncTest*(name: string, body: untyped) =
+  test name:
+    skip()
+
+template asyncTestx*(name: string, body: untyped) =
+  test name:
+    skip()
