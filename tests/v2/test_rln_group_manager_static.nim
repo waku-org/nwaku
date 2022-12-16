89883f61fec44d3e5e122b02069da8105d16fed2
rymnc <43716372+rymnc@users.noreply.github.com>
2022-12-16 10:17:57 +0530

    style(rln-relay): fmt test

--- tests/v2/test_rln_group_manager_static.nim
+++ tests/v2/test_rln_group_manager_static.nim
@@ -3,14 +3,14 @@ when (NimMajor, NimMinor) < (1, 4):
 else:
   {.push raises: [].}
 
-import 
-    testutils/unittests,
-    stew/results,
-    options,
-    ../../waku/v2/protocol/waku_rln_relay/protocol_types,
-    ../../waku/v2/protocol/waku_rln_relay/ffi,
-    ../../waku/v2/protocol/waku_rln_relay/conversion_utils,
-    ../../waku/v2/protocol/waku_rln_relay/group_manager/static/group_manager
+import
+  testutils/unittests,
+  stew/results,
+  options,
+  ../../waku/v2/protocol/waku_rln_relay/protocol_types,
+  ../../waku/v2/protocol/waku_rln_relay/ffi,
+  ../../waku/v2/protocol/waku_rln_relay/conversion_utils,
+  ../../waku/v2/protocol/waku_rln_relay/group_manager/static/group_manager
 
 import
   stew/shims/net,
@@ -20,164 +20,164 @@ import
   discovery/dnsdisc/builder
 
 proc generateCredentials(rlnInstance: ptr RLN): IdentityCredential =
-    let credRes = membershipKeyGen(rlnInstance)
-    return credRes.get()
+  let credRes = membershipKeyGen(rlnInstance)
+  return credRes.get()
 
 proc generateCredentials(rlnInstance: ptr RLN, n: int): seq[IdentityCredential] =
-    var credentials: seq[IdentityCredential] 
-    for i in 0 ..< n:
-        credentials.add(generateCredentials(rlnInstance))
-    return credentials
+  var credentials: seq[IdentityCredential]
+  for i in 0 ..< n:
+    credentials.add(generateCredentials(rlnInstance))
+  return credentials
 
 suite "Static group manager":
-    setup:
-        let rlnInstanceRes = createRlnInstance()
-        require:
-            rlnInstanceRes.isOk()
+  setup:
+    let rlnInstanceRes = createRlnInstance()
+    require:
+      rlnInstanceRes.isOk()
 
-        let rlnInstance = rlnInstanceRes.get()
-        let credentials = generateCredentials(rlnInstance, 10)
+    let rlnInstance = rlnInstanceRes.get()
+    let credentials = generateCredentials(rlnInstance, 10)
 
-        let staticConfig = StaticGroupManagerConfig(groupSize: 10,
-                                            membershipIndex: 5,
-                                            groupKeys: credentials)
+    let staticConfig = StaticGroupManagerConfig(groupSize: 10,
+                                                membershipIndex: 5,
+                                                groupKeys: credentials)
 
-        let manager {.used.} = StaticGroupManager(config: staticConfig,
-                                         rlnInstance: rlnInstance)
+    let manager {.used.} = StaticGroupManager(config: staticConfig,
+                                              rlnInstance: rlnInstance)
 
-    asyncTest "should initialize successfully":
-        let merkleRootBeforeRes = manager.rlnInstance.getMerkleRoot()
-        require:
-            merkleRootBeforeRes.isOk()
-        let merkleRootBefore = merkleRootBeforeRes.get()
+  asyncTest "should initialize successfully":
+    let merkleRootBeforeRes = manager.rlnInstance.getMerkleRoot()
+    require:
+      merkleRootBeforeRes.isOk()
+    let merkleRootBefore = merkleRootBeforeRes.get()
 
-        await manager.init()
-        let merkleRootAfterRes = manager.rlnInstance.getMerkleRoot()
-        require:
-            merkleRootAfterRes.isOk()
-        let merkleRootAfter = merkleRootAfterRes.get()
-        check:
-            manager.idCredentials.isSome()
-            manager.config.groupKeys.len == 10
-            manager.config.groupSize == 10
-            manager.config.membershipIndex == 5
-            manager.config.groupKeys[5] == manager.idCredentials.get()
-            manager.latestIndex == 9
-            merkleRootAfter.inHex() != merkleRootBefore.inHex()
+    await manager.init()
+    let merkleRootAfterRes = manager.rlnInstance.getMerkleRoot()
+    require:
+      merkleRootAfterRes.isOk()
+    let merkleRootAfter = merkleRootAfterRes.get()
+    check:
+      manager.idCredentials.isSome()
+      manager.config.groupKeys.len == 10
+      manager.config.groupSize == 10
+      manager.config.membershipIndex == 5
+      manager.config.groupKeys[5] == manager.idCredentials.get()
+      manager.latestIndex == 9
+      merkleRootAfter.inHex() != merkleRootBefore.inHex()
 
-    asyncTest "startGroupSync: should start group sync":
-        await manager.init()
-        await manager.startGroupSync()
+  asyncTest "startGroupSync: should start group sync":
+    await manager.init()
+    await manager.startGroupSync()
 
-    asyncTest "startGroupSync: should guard against uninitialized state":
-        let staticConfig = StaticGroupManagerConfig(groupSize: 0,
-                                                    membershipIndex: 0,
-                                                    groupKeys: @[])
+  asyncTest "startGroupSync: should guard against uninitialized state":
+    let staticConfig = StaticGroupManagerConfig(groupSize: 0,
+                                                membershipIndex: 0,
+                                                groupKeys: @[])
 
-        let manager = StaticGroupManager(config: staticConfig,
-                                         rlnInstance: rlnInstance)
+    let manager = StaticGroupManager(config: staticConfig,
+                                     rlnInstance: rlnInstance)
 
-        expect(ValueError):
-            await manager.startGroupSync()
+    expect(ValueError):
+      await manager.startGroupSync()
 
-    asyncTest "register: should guard against uninitialized state":
-        let staticConfig = StaticGroupManagerConfig(groupSize: 0,
-                                                    membershipIndex: 0,
-                                                    groupKeys: @[])
+  asyncTest "register: should guard against uninitialized state":
+    let staticConfig = StaticGroupManagerConfig(groupSize: 0,
+                                                membershipIndex: 0,
+                                                groupKeys: @[])
 
-        let manager = StaticGroupManager(config: staticConfig,
-                                         rlnInstance: rlnInstance)
+    let manager = StaticGroupManager(config: staticConfig,
+                                     rlnInstance: rlnInstance)
 
-        let dummyCommitment = default(IDCommitment)
+    let dummyCommitment = default(IDCommitment)
 
-        expect(ValueError):
-            await manager.register(dummyCommitment)
+    expect(ValueError):
+      await manager.register(dummyCommitment)
 
-    asyncTest "register: should register successfully":
-        await manager.init()
-        await manager.startGroupSync()
+  asyncTest "register: should register successfully":
+    await manager.init()
+    await manager.startGroupSync()
 
-        let idCommitment = generateCredentials(manager.rlnInstance).idCommitment
-        let merkleRootBeforeRes = manager.rlnInstance.getMerkleRoot()
-        require:
-            merkleRootBeforeRes.isOk()
-        let merkleRootBefore = merkleRootBeforeRes.get()
-        await manager.register(idCommitment)
-        let merkleRootAfterRes = manager.rlnInstance.getMerkleRoot()
-        require:
-            merkleRootAfterRes.isOk()
-        let merkleRootAfter = merkleRootAfterRes.get()
-        check:
-            merkleRootAfter.inHex() != merkleRootBefore.inHex()
-            manager.latestIndex == 10
-    
-    asyncTest "register: callback is called":
-        var callbackCalled = false
-        let idCommitment = generateCredentials(manager.rlnInstance).idCommitment
+    let idCommitment = generateCredentials(manager.rlnInstance).idCommitment
+    let merkleRootBeforeRes = manager.rlnInstance.getMerkleRoot()
+    require:
+        merkleRootBeforeRes.isOk()
+    let merkleRootBefore = merkleRootBeforeRes.get()
+    await manager.register(idCommitment)
+    let merkleRootAfterRes = manager.rlnInstance.getMerkleRoot()
+    require:
+      merkleRootAfterRes.isOk()
+    let merkleRootAfter = merkleRootAfterRes.get()
+    check:
+      merkleRootAfter.inHex() != merkleRootBefore.inHex()
+      manager.latestIndex == 10
 
-        let fut = newFuture[void]()
+  asyncTest "register: callback is called":
+    var callbackCalled = false
+    let idCommitment = generateCredentials(manager.rlnInstance).idCommitment
 
-        proc callback(registrations: seq[(IDCommitment, MembershipIndex)]): Future[void] {.async.} =
-            require:
-                registrations.len == 1
-                registrations[0][0] == idCommitment
-                registrations[0][1] == 10
-            callbackCalled = true
-            fut.complete()
+    let fut = newFuture[void]()
 
-        manager.onRegister(callback)
-        await manager.init()
-        await manager.startGroupSync()
+    proc callback(registrations: seq[(IDCommitment, MembershipIndex)]): Future[void] {.async.} =
+      require:
+        registrations.len == 1
+        registrations[0][0] == idCommitment
+        registrations[0][1] == 10
+      callbackCalled = true
+      fut.complete()
 
-        await manager.register(idCommitment)
+    manager.onRegister(callback)
+    await manager.init()
+    await manager.startGroupSync()
 
-        await fut
-        check:
-            callbackCalled
+    await manager.register(idCommitment)
 
-    asyncTest "withdraw: should guard against uninitialized state":
-        let idSecretHash = credentials[0].idSecretHash
+    await fut
+    check:
+      callbackCalled
 
-        expect(ValueError):
-            await manager.withdraw(idSecretHash)
+  asyncTest "withdraw: should guard against uninitialized state":
+    let idSecretHash = credentials[0].idSecretHash
 
-    asyncTest "withdraw: should withdraw successfully":
-        await manager.init()
-        await manager.startGroupSync()
+    expect(ValueError):
+      await manager.withdraw(idSecretHash)
 
-        let idSecretHash = credentials[0].idSecretHash
-        let merkleRootBeforeRes = manager.rlnInstance.getMerkleRoot()
-        require:
-            merkleRootBeforeRes.isOk()
-        let merkleRootBefore = merkleRootBeforeRes.get()
-        await manager.withdraw(idSecretHash)
-        let merkleRootAfterRes = manager.rlnInstance.getMerkleRoot()
-        require:
-            merkleRootAfterRes.isOk()
-        let merkleRootAfter = merkleRootAfterRes.get()
-        check:
-            merkleRootAfter.inHex() != merkleRootBefore.inHex()
+  asyncTest "withdraw: should withdraw successfully":
+    await manager.init()
+    await manager.startGroupSync()
 
-    asyncTest "withdraw: callback is called":
-        var callbackCalled = false
-        let idSecretHash = credentials[0].idSecretHash
-        let idCommitment = credentials[0].idCommitment
-        let fut = newFuture[void]()
+    let idSecretHash = credentials[0].idSecretHash
+    let merkleRootBeforeRes = manager.rlnInstance.getMerkleRoot()
+    require:
+      merkleRootBeforeRes.isOk()
+    let merkleRootBefore = merkleRootBeforeRes.get()
+    await manager.withdraw(idSecretHash)
+    let merkleRootAfterRes = manager.rlnInstance.getMerkleRoot()
+    require:
+      merkleRootAfterRes.isOk()
+    let merkleRootAfter = merkleRootAfterRes.get()
+    check:
+      merkleRootAfter.inHex() != merkleRootBefore.inHex()
 
-        proc callback(withdrawals: seq[(IdentitySecretHash, MembershipIndex)]): Future[void] {.async.} =
-            require:
-                withdrawals.len == 1
-                withdrawals[0][0] == idCommitment
-                withdrawals[0][1] == 0
-            callbackCalled = true
-            fut.complete()
+  asyncTest "withdraw: callback is called":
+    var callbackCalled = false
+    let idSecretHash = credentials[0].idSecretHash
+    let idCommitment = credentials[0].idCommitment
+    let fut = newFuture[void]()
 
-        manager.onWithdraw(callback)
-        await manager.init()
-        await manager.startGroupSync()
+    proc callback(withdrawals: seq[(IdentitySecretHash, MembershipIndex)]): Future[void] {.async.} =
+      require:
+        withdrawals.len == 1
+        withdrawals[0][0] == idCommitment
+        withdrawals[0][1] == 0
+      callbackCalled = true
+      fut.complete()
 
-        await manager.withdraw(idSecretHash)
+    manager.onWithdraw(callback)
+    await manager.init()
+    await manager.startGroupSync()
 
-        await fut
-        check:
-            callbackCalled
+    await manager.withdraw(idSecretHash)
+
+    await fut
+    check:
+      callbackCalled
