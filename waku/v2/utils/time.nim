e9ba53bbd09b60a2993d60bd16b284efad02ab0f
Hanno Cornelius <68783915+jm-clius@users.noreply.github.com>
2023-02-03 10:06:21 +0200

    chore: add better e2 trace logging for relay (#1526)

--- waku/v2/utils/time.nim
+++ waku/v2/utils/time.nim
@@ -4,26 +4,29 @@ when (NimMajor, NimMinor) < (1, 4):
 else:
   {.push raises: [].}
 
-import 
+import
   std/times,
   metrics
 
-type Timestamp* = int64 
+type Timestamp* = int64
 
-proc getNanosecondTime*[T](timeInSeconds: T): Timestamp = 
+proc getNanosecondTime*[T](timeInSeconds: T): Timestamp =
   var ns = Timestamp(timeInSeconds.int64 * 1000_000_000.int64)
   return ns
 
-proc getMicrosecondTime*[T](timeInSeconds: T): Timestamp = 
+proc getMicrosecondTime*[T](timeInSeconds: T): Timestamp =
   var us = Timestamp(timeInSeconds.int64 * 1000_000.int64)
   return us
 
-proc getMillisecondTime*[T](timeInSeconds: T): Timestamp = 
+proc getMillisecondTime*[T](timeInSeconds: T): Timestamp =
   var ms = Timestamp(timeInSeconds.int64 * 1000.int64)
   return ms
 
 proc nowInUnixFloat(): float =
   return getTime().toUnixFloat()
+
+proc getNowInNanosecondTime*(): Timestamp =
+  return getNanosecondTime(nowInUnixFloat())
 
 template nanosecondTime*(collector: Summary | Histogram, body: untyped) =
   when defined(metrics):
