a135568e83edfc17d50cb02be0aa598393a7b910
s1fr0 <28568419+s1fr0@users.noreply.github.com>
2023-02-06 00:20:19 +0100

    fix(credentials): restore connectToNodes call

--- waku/v2/node/waku_node.nim
+++ waku/v2/node/waku_node.nim
@@ -285,7 +285,7 @@ proc info*(node: WakuNode): WakuInfo =
 proc connectToNodes*(node: WakuNode, nodes: seq[RemotePeerInfo] | seq[string], source = "api") {.async.} =
   ## `source` indicates source of node addrs (static config, api call, discovery, etc)
   # NOTE This is dialing on WakuRelay protocol specifically
-  await peer_manager.connectToNodes(node.peerManager, nodes, WakuRelayCodec, source=source)
+  await connectToNodes(node.peerManager, nodes, WakuRelayCodec, source=source)
 
 
 ## Waku relay
