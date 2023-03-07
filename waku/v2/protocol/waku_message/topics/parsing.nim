67db35e29d0a42d1151cdb6956fd939e7df9cd69
Lorenzo Delgado <lorenzo@status.im>
2023-03-07 11:10:36 +0100

    feat(message): differentiate content and pubsub topic namespacing

---
+++ waku/v2/protocol/waku_message/topics/parsing.nim
@@ -0,0 +1,36 @@
+when (NimMajor, NimMinor) < (1, 4):
+  {.push raises: [Defect].}
+else:
+  {.push raises: [].}
+
+import stew/results
+
+
+type
+  ParsingErrorKind* {.pure.} = enum
+    InvalidFormat,
+    MissingPart
+
+  ParsingError* = object
+    case kind*: ParsingErrorKind
+    of InvalidFormat:
+      cause*: string
+    of MissingPart:
+      part*: string
+
+type ParsingResult*[T] = Result[T, ParsingError]
+
+
+proc invalidFormat*(T: type ParsingError, cause = "invalid format"): T =
+  ParsingError(kind: ParsingErrorKind.InvalidFormat, cause: cause)
+
+proc missingPart*(T: type ParsingError, part = "unknown"): T =
+  ParsingError(kind: ParsingErrorKind.MissingPart, part: part)
+
+
+proc `$`*(err: ParsingError): string =
+  case err.kind:
+  of ParsingErrorKind.InvalidFormat:
+    return "invalid format: " & err.cause
+  of ParsingErrorKind.MissingPart:
+    return "missing part: " & err.part
---
+++ waku/v2/protocol/waku_message/topics/pubsub_topic.nim
@@ -0,0 +1,118 @@
+## Waku pub-sub topics definition and namespacing utils
+##
+## See 23/WAKU2-TOPICS RFC: https://rfc.vac.dev/spec/23/
+
+when (NimMajor, NimMinor) < (1, 4):
+  {.push raises: [Defect].}
+else:
+  {.push raises: [].}
+
+
+import
+  std/strutils,
+  stew/[results, base10]
+import
+  ./parsing
+
+export parsing
+
+
+## Pub-sub topic
+
+type PubsubTopic* = string
+
+const DefaultPubsubTopic* = PubsubTopic("/waku/2/default-waku/proto")
+
+
+## Namespaced pub-sub topic
+
+type
+  NsPubsubTopicKind* {.pure.} = enum
+    StaticSharding,
+    NamedSharding
+
+type
+  NsPubsubTopic* = object
+    case kind*: NsPubsubTopicKind
+    of NsPubsubTopicKind.StaticSharding:
+      cluster*: uint16
+      shard*: uint16
+    of NsPubsubTopicKind.NamedSharding:
+      name*: string
+
+proc staticSharding*(T: type NsPubsubTopic, cluster, shard: uint16): T =
+  NsPubsubTopic(
+    kind: NsPubsubTopicKind.StaticSharding,
+    cluster: cluster,
+    shard: shard
+  )
+
+proc named*(T: type NsPubsubTopic, name: string): T =
+  NsPubsubTopic(
+    kind: NsPubsubTopicKind.NamedSharding,
+    name: name
+  )
+
+
+# Serialization
+
+proc `$`*(topic: NsPubsubTopic): string =
+  ## Returns a string representation of a namespaced topic
+  ## in the format `/waku/2/<raw-topic>
+  case topic.kind:
+  of NsPubsubTopicKind.NamedSharding:
+    "/waku/2/" & topic.name
+  of NsPubsubTopicKind.StaticSharding:
+    "/waku/2/rs/" & $topic.cluster & "/" & $topic.shard
+
+
+# Deserialization
+
+const
+  Waku2PubsubTopicPrefix = "/waku/2"
+  StaticShardingPubsubTopicPrefix = Waku2PubsubTopicPrefix & "/rs"
+
+
+proc parseStaticSharding*(T: type NsPubsubTopic, topic: PubsubTopic|string): ParsingResult[NsPubsubTopic] =
+  if not topic.startsWith(StaticShardingPubsubTopicPrefix):
+    return err(ParsingError.invalidFormat("must start with " & StaticShardingPubsubTopicPrefix))
+
+  let parts = topic[11..<topic.len].split("/")
+  if parts.len != 2:
+    return err(ParsingError.invalidFormat("invalid topic structure"))
+
+  let clusterPart = parts[0]
+  if clusterPart.len == 0:
+    return err(ParsingError.missingPart("shard_cluster_index"))
+  let cluster = ?Base10.decode(uint16, clusterPart).mapErr(proc(err: auto): auto = ParsingError.invalidFormat($err))
+
+  let shardPart = parts[1]
+  if shardPart.len == 0:
+    return err(ParsingError.missingPart("shard_number"))
+  let shard = ?Base10.decode(uint16, shardPart).mapErr(proc(err: auto): auto = ParsingError.invalidFormat($err))
+
+  ok(NsPubsubTopic.staticSharding(cluster, shard))
+
+proc parseNamedSharding*(T: type NsPubsubTopic, topic: PubsubTopic|string): ParsingResult[NsPubsubTopic] =
+  if not topic.startsWith(Waku2PubsubTopicPrefix):
+    return err(ParsingError.invalidFormat("must start with " & Waku2PubsubTopicPrefix))
+
+  let raw = topic[8..<topic.len]
+  if raw.len == 0:
+    return err(ParsingError.missingPart("topic-name"))
+
+  ok(NsPubsubTopic.named(name=raw))
+
+proc parse*(T: type NsPubsubTopic, topic: PubsubTopic|string): ParsingResult[NsPubsubTopic] =
+  ## Splits a namespaced topic string into its constituent parts.
+  ## The topic string has to be in the format `/<application>/<version>/<topic-name>/<encoding>`
+  if topic.startsWith(StaticShardingPubsubTopicPrefix):
+    NsPubsubTopic.parseStaticSharding(topic)
+  else:
+    NsPubsubTopic.parseNamedSharding(topic)
+
+
+# Pubsub topic compatibility
+
+converter toPubsubTopic*(topic: NsPubsubTopic): PubsubTopic =
+  $topic
