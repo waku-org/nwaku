1995bbec6cc8dc798ce8a8aee3845c68586759fa
Lorenzo Delgado <lorenzo@status.im>
2023-03-10 01:33:52 +0100

    feat(common): added the enr builder

--- tests/v2/testlib/waku2.nim
+++ tests/v2/testlib/waku2.nim
@@ -2,7 +2,9 @@ import
   std/options,
   stew/byteutils,
   libp2p/switch,
-  libp2p/builders
+  libp2p/builders,
+  libp2p/crypto/crypto as libp2p_keys,
+  eth/keys as eth_keys
 import
   ../../../waku/v2/protocol/waku_message,
   ./common
@@ -12,17 +14,20 @@ export switch
 
 # Switch
 
-proc generateEcdsaKey*(): PrivateKey =
-  PrivateKey.random(ECDSA, rng[]).get()
+proc generateEcdsaKey*(): libp2p_keys.PrivateKey =
+  libp2p_keys.PrivateKey.random(ECDSA, rng[]).get()
 
-proc generateEcdsaKeyPair*(): KeyPair =
-  KeyPair.random(ECDSA, rng[]).get()
+proc generateEcdsaKeyPair*(): libp2p_keys.KeyPair =
+  libp2p_keys.KeyPair.random(ECDSA, rng[]).get()
 
-proc generateSecp256k1Key*(): PrivateKey =
-  PrivateKey.random(Secp256k1, rng[]).get()
+proc generateSecp256k1Key*(): libp2p_keys.PrivateKey =
+  libp2p_keys.PrivateKey.random(Secp256k1, rng[]).get()
+
+proc ethSecp256k1Key*(hex: string): eth_keys.PrivateKey =
+  eth_keys.PrivateKey.fromHex(hex).get()
 
 
-proc newTestSwitch*(key=none(PrivateKey), address=none(MultiAddress)): Switch =
+proc newTestSwitch*(key=none(libp2p_keys.PrivateKey), address=none(MultiAddress)): Switch =
   let peerKey = key.get(generateSecp256k1Key())
   let peerAddr = address.get(MultiAddress.init("/ip4/127.0.0.1/tcp/0").get())
   return newStandardSwitch(some(peerKey), addrs=peerAddr)
