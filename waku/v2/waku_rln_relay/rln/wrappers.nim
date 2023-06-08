f8774ee0ec9a7d62bcef103c5b86d35b507cdb7d
Aaryamann Challani <43716372+rymnc@users.noreply.github.com>
2023-06-08 16:02:40 +0530

    fix(rln-relay): address visibility
    Co-authored-by: Ivan Folgueira Bande <128452529+Ivansete-status@users.noreply.github.com>

--- waku/v2/waku_rln_relay/rln/wrappers.nim
+++ waku/v2/waku_rln_relay/rln/wrappers.nim
@@ -53,14 +53,14 @@ proc membershipKeyGen*(ctxPtr: ptr RLN): RlnRelayResult[IdentityCredential] =
 
   return ok(identityCredential)
 
-type RlnTreeConfig* = ref object of RootObj
+type RlnTreeConfig = ref object of RootObj
   cache_capacity*: int
   mode*: string
   compression*: bool
   flush_interval*: int
   path*: string
 
-type RlnConfig* = ref object of RootObj
+type RlnConfig = ref object of RootObj
   resources_folder*: string
   tree_config*: RlnTreeConfig
 
@@ -75,7 +75,7 @@ proc `%`(c: RlnConfig): JsonNode =
   return %[("resources_folder", %c.resources_folder),
            ("tree_config", %tree_config)]
 
-proc createRLNInstanceLocal*(d = MerkleTreeDepth, 
+proc createRLNInstanceLocal(d = MerkleTreeDepth, 
                              tree_path = DefaultRlnTreePath): RLNResult =
   ## generates an instance of RLN
   ## An RLN instance supports both zkSNARKs logics and Merkle tree data structure and operations
