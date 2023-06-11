4b8ea80e80c176dc22909890e051092f02777295
Ivan Folgueira Bande <ivansete@status.im>
2023-06-11 14:42:14 +0200

    Adding example of nwaku integration in Go

---
+++ examples/golang/waku.go
@@ -0,0 +1,89 @@
+package main
+
+/*
+	#cgo LDFLAGS: -L./ -lwaku -Wl,--allow-multiple-definition
+
+	#include "libwaku.h"
+
+	static NimStringDesc wakuString;
+	NimStringDesc* mResp = &wakuString;
+
+	char* data(NimStringDesc* a) {
+		return a->data;
+	}
+
+	extern void eventHandler(char* msg);
+
+	void cGoEventHandler(char* msg) {
+		eventHandler(msg);
+	}
+
+	void setCallback() {
+		waku_set_event_callback(cGoEventHandler);
+	}
+
+	struct ConfigNode {
+		NCSTRING host;
+		NU       port;
+		NCSTRING key;
+		NIM_BOOL relay;
+		NCSTRING peers;
+	};
+*/
+import "C"
+
+import (
+	"os"
+	"fmt"
+	"runtime"
+)
+
+//export eventHandler
+func eventHandler(msg *C.char) {
+	fmt.Println("Event received: ", C.GoString(msg))
+}
+
+func main() {
+	runtime.LockOSThread()
+
+	C.NimMain()
+
+	cfgNode := C.ConfigNode{}
+
+	cfgNode.host = C.CString("0.0.0.0")
+	cfgNode.port = 60000
+	cfgNode.key  = C.CString("364d111d729a6eb6d2e6113e163f017b5ef03a6f94c9b5b7bb1bb36fa5cb07a9")
+	cfgNode.relay = true
+	cfgNode.peers = nil
+
+	fmt.Println("Git Version: ", C.GoString( C.waku_version() ))
+	C.waku_default_pubsub_topic(&C.mResp);
+	defaultPubsubTopic := C.data( C.mResp )
+
+	fmt.Println("Default pubsub topic: ", C.GoString( defaultPubsubTopic ) )
+	fmt.Println("Bind addr: ", C.GoString(cfgNode.host), ":", cfgNode.port)
+
+	if cfgNode.relay {
+		fmt.Println("Waku Relay enabled")
+	} else {
+		fmt.Println("Waku Relay disabled")
+	}
+
+	if ! C.waku_new(&cfgNode, &C.mResp) {
+		fmt.Println("Error creating WakuNode: ", C.GoString(C.data(C.mResp)));
+		os.Exit(-1)
+	}
+
+	C.setCallback()
+	C.waku_start()
+
+	if ! C.waku_relay_subscribe(defaultPubsubTopic, &C.mResp) {
+		fmt.Println("Error subscribing to PubsubTopic: ",
+					C.GoString(C.data(C.mResp)));
+		os.Exit(-1)
+	}
+
+	for {
+		C.waku_poll()
+	}
+}
