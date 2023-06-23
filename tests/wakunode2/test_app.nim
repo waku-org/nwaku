4573e8c59202fb61d8579bc98465cce0749e46e7
Alvaro Revuelta <alvrevuelta@gmail.com>
2023-06-23 14:50:58 +0200

    fix(ci): fix broken test with wrong import (#1820)

--- tests/wakunode2/test_app.nim
+++ tests/wakunode2/test_app.nim
@@ -10,7 +10,7 @@ import
   libp2p/multiaddress,
   libp2p/switch
 import
-  ../../apps/wakunode2/config,
+  ../../apps/wakunode2/external_config,
   ../../apps/wakunode2/app,
   ../v2/testlib/common,
   ../v2/testlib/wakucore
