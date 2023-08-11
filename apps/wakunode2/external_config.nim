1be8171fdedc26d0dca3a5ec2290a5191ba060b7
Simon-Pierre Vivier <simvivier@status.im>
2023-08-11 11:28:24 -0400

    fix: bring back default topic in config (#1902)
    * fix: bring back default topic in config
    
    * fix
    
    * Fix a bug
    
    * Fix enr creation

--- apps/wakunode2/external_config.nim
+++ apps/wakunode2/external_config.nim
@@ -225,9 +225,13 @@ type
       defaultValue: false
       name: "keep-alive" }: bool
 
+    topics* {.
+      desc: "Default topic to subscribe to. Argument may be repeated. Deprecated! Please use pubsub-topic and/or content-topic instead."
+      defaultValue: @["/waku/2/default-waku/proto"]
+      name: "topic" .}: seq[string]
+
     pubsubTopics* {.
       desc: "Default pubsub topic to subscribe to. Argument may be repeated."
-      defaultValue: @["/waku/2/default-waku/proto"]
       name: "pubsub-topic" .}: seq[string]
 
     contentTopics* {.
