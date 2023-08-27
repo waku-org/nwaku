3f90a693d49e465f6f5579196b72fefb0df26274
Ivan Folgueira Bande <ivansete@status.im>
2023-08-27 10:34:17 +0200

    alloc.nim: allocShared -> allocShared0

--- library/alloc.nim
+++ library/alloc.nim
@@ -2,6 +2,6 @@
 proc alloc*(str: cstring): cstring =
   # Byte allocation from the given address.
   # There should be the corresponding manual deallocation with deallocShared !
-  let ret = cast[cstring](allocShared(len(str) + 1))
+  let ret = cast[cstring](allocShared0(len(str) + 1))
   copyMem(ret, str, len(str) + 1)
   return ret
