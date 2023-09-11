0033507c8946eee04c9e9580851767be18cab5cf
Álex Cabeza Romero <alex93cabeza@gmail.com>
2023-09-11 17:50:43 +0200

    Move waku filter utility functions to a different file for reuse.

--- tests/waku_filter_v2/test_waku_filter.nim
+++ tests/waku_filter_v2/test_waku_filter.nim
@@ -6,41 +6,23 @@ import
   chronos,
   chronicles,
   libp2p/peerstore
+
 import
   ../../../waku/node/peer_manager,
   ../../../waku/waku_filter_v2,
   ../../../waku/waku_filter_v2/client,
   ../../../waku/waku_core,
   ../testlib/common,
-  ../testlib/wakucore
-
-proc newTestWakuFilter(switch: Switch): Future[WakuFilter] {.async.} =
-  let
-    peerManager = PeerManager.new(switch)
-    proto = WakuFilter.new(peerManager)
-
-  await proto.start()
-  switch.mount(proto)
-
-  return proto
-
-proc newTestWakuFilterClient(switch: Switch, messagePushHandler: MessagePushHandler): Future[WakuFilterClient] {.async.} =
-  let
-    peerManager = PeerManager.new(switch)
-    proto = WakuFilterClient.new(rng, messagePushHandler, peerManager)
-
-  await proto.start()
-  switch.mount(proto)
-
-  return proto
+  ../testlib/wakucore,
+  ./client_utils.nim
 
 suite "Waku Filter - end to end":
-
   asyncTest "ping":
     # Given
     var
       voidHandler: MessagePushHandler = proc(pubsubTopic: PubsubTopic, message: WakuMessage) =
         discard
+
     let
       serverSwitch = newStandardSwitch()
       clientSwitch = newStandardSwitch()
