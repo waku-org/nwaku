40b5ec8056e8c274a529c54f14935ae0f959ab97
Álex Cabeza Romero <alex93cabeza@gmail.com>
2023-09-21 21:01:24 +0200

    Add test aggregator to all directories.

---
+++ tests/common/test_all.nim
@@ -0,0 +1,7 @@
+import
+  ./test_base64_codec,
+  ./test_confutils_envvar,
+  ./test_enr_builder,
+  ./test_envvar_serialization,
+  ./test_protobuf_validation,
+  ./test_sqlite_migrations
---
+++ tests/waku_store/test_all.nim
@@ -0,0 +1,7 @@
+{.used.}
+
+import
+  ./test_resume,
+  ./test_rpc_codec,
+  ./test_waku_store,
+  ./test_wakunode_store
