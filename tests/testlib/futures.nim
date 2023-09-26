c06e84743dba5d019a7b3f1c24ee15e47c57d6dd
Álex Cabeza Romero <alex93cabeza@gmail.com>
2023-09-26 19:20:00 +0200

    Remove commented out imports.

--- tests/testlib/futures.nim
+++ tests/testlib/futures.nim
@@ -1,11 +1,6 @@
 import
-#   std/sequtils,
-#   std/options,
-#   stew/shims/net as stewNet,
-#   testutils/unittests,
   chronicles,
   chronos
-#   libp2p/crypto/crypto
 
 import ../../../waku/waku_core/message
 
