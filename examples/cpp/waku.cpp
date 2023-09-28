bb5f2636a395e75b492eb3b10d2713b2bd018839
Ivan Folgueira Bande <128452529+Ivansete-status@users.noreply.github.com>
2023-09-28 11:03:20 +0200

    typo
    Co-authored-by: NagyZoltanPeter <113987313+NagyZoltanPeter@users.noreply.github.com>

--- examples/cpp/waku.cpp
+++ examples/cpp/waku.cpp
@@ -297,7 +297,7 @@ int main(int argc, char** argv) {
     // example on how to retrieve a value from the `libwaku` callback.
     std::string defaultPubsubTopic;
     WAKU_CALL(waku_default_pubsub_topic(cify([&defaultPubsubTopic](const char* msg, size_t len) {
-        defautPubsubTopic = msg;
+        defaultPubsubTopic = msg;
     })));
 
     std::cout << "Default pubsub topic: " << defautPubsubTopic << std::endl;
