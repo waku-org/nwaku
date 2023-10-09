94d050e5371aacc005bbd3392943ccff7f0f4a24
Álex Cabeza Romero <alex93cabeza@gmail.com>
2023-10-09 17:35:21 +0200

    Move common code to separate modules.

--- tests/testlib/futures.nim
+++ tests/testlib/futures.nim
@@ -6,3 +6,6 @@ import ../../../waku/waku_core/message
 
 proc newPushHandlerFuture*(): Future[(string, WakuMessage)] =
     newFuture[(string, WakuMessage)]()
+
+proc newBoolFuture*(): Future[bool] =
+    newFuture[bool]()
