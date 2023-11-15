ee0d6004367e521fb45a40a39532c4ca356ba37d
Álex Cabeza Romero <alex93cabeza@gmail.com>
2023-11-15 10:25:08 +0100

    test(waku-filter): Unsubscribe all, payloads and security tests (#2095)
    * Implement unsubscribe all and payload tests.
    * Implement waku node filter Security and Privacy tests.

---
+++ tests/waku_filter_v2/test_data.nim
@@ -0,0 +1,70 @@
+import
+  std/json
+
+const
+  ALPHABETIC* = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  ALPHANUMERIC* = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  ALPHANUMERIC_SPECIAL* = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;':\\\",./<>?`~"
+  EMOJI* = "😀 😃 😄 😁 😆 😅 🤣 😂 🙂 🙃 😉 😊 😇 🥰 😍 🤩 😘 😗 😚 😙"
+  CODE* = "def main():\n\tprint('Hello, world!')"
+  QUERY* = """
+    SELECT
+      u.id,
+      u.name,
+      u.email,
+      u.created_at,
+      u.updated_at,
+      (
+        SELECT
+          COUNT(*)
+        FROM
+          posts p
+        WHERE
+          p.user_id = u.id
+      ) AS post_count
+    FROM
+      users u
+    WHERE
+      u.id = 1
+    """
+  TEXT_SMALL* = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  TEXT_LARGE* = """
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida vulputate semper. Proin 
+    eleifend varius cursus. Morbi lacinia posuere quam sit amet pretium. Sed non metus fermentum, 
+    venenatis nisl id, vestibulum eros. Quisque non lorem sit amet lectus faucibus elementum eu 
+    sit amet odio. Mauris tortor justo, malesuada quis volutpat vitae, tristique at nisl. Proin 
+    eleifend eu arcu ac sodales. In efficitur ipsum urna, ut viverra turpis sodales ut. Phasellus 
+    nec tortor eu urna suscipit euismod eget vel ligula. Phasellus vestibulum sollicitudin tellus, 
+    ac sodales tellus tempor id. Curabitur sed congue velit. 
+    """
+
+proc getSampleJsonDictionary*(): JsonNode =
+  %*{
+    "shapes": [
+      {
+        "type": "circle",
+        "radius": 10
+      },
+      {
+        "type": "square",
+        "side": 10
+      }
+    ],
+    "colours": [
+      "red",
+      "green",
+      "blue"
+    ]
+  }
+
+proc getSampleJsonList*(): JsonNode = 
+  %*[
+    {
+      "type": "cat",
+      "name": "Salem"
+    },
+    {
+      "type": "dog",
+      "name": "Oberon"
+    },
+  ]
