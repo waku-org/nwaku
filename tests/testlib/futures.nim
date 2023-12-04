e708159c0cd8060df18e7dc74aed3aea0863f5db
Álex Cabeza Romero <alex93cabeza@gmail.com>
2023-12-04 20:46:59 +0100

    Add resources and future to result converter.

--- tests/testlib/futures.nim
+++ tests/testlib/futures.nim
@@ -14,3 +14,13 @@ proc newBoolFuture*(): Future[bool] =
 
 proc newHistoryFuture*(): Future[HistoryQuery] =
   newFuture[HistoryQuery]()
+
+proc toResult*[T](future: Future[T]): Result[T, string] =
+  if future.finished():
+    return chronos.ok(future.read())
+  else:
+    return chronos.err("Future not finished.")
+
+proc waitForResult*[T](future: Future[T], timeout = FUTURE_TIMEOUT): Future[Result[T, string]] {.async.} =
+  discard await future.withTimeout(timeout)
+  return future.toResult()
