88e219a66c3a5c82bc5302bf3c0ce001f2f90c91
Álex Cabeza Romero <alex93cabeza@gmail.com>
2023-12-20 14:42:50 +0100

    Fix function name.

--- tests/resources/pubsub_topics.nim
+++ tests/resources/pubsub_topics.nim
@@ -1,11 +1,11 @@
-proc getPubsunTopic*(contentTopicName: string): string =
-  return "/waku/2/$contentTopicName"
+proc getPubsubTopic*(pubsubTopicName: string): string =
+  return "/waku/2/$pubsubTopicName"
 
 
 const
-  CURRENT* = getPubsunTopic("test")
-  CURRENT_NESTED* = getPubsunTopic("test/nested")
-  SHARDING* = getPubsunTopic("waku-9_shard-0")
+  CURRENT* = getPubsubTopic("test")
+  CURRENT_NESTED* = getPubsubTopic("test/nested")
+  SHARDING* = getPubsubTopic("waku-9_shard-0")
   PLAIN* = "test"
   LEGACY* = "/waku/1/test"
   LEGACY_NESTED* = "/waku/1/test/nested"
