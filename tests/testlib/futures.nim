3d6012685fad57624e1f777a261405699bc35626
Álex Cabeza Romero <alex93cabeza@gmail.com>
2024-01-24 16:46:07 +0100

    Rln utilities and abstractions.

--- tests/testlib/futures.nim
+++ tests/testlib/futures.nim
@@ -1,20 +1,16 @@
-import
-  chronos
+import chronos
 
-import 
-  ../../../waku/[
-    waku_core/message,
-    waku_store
-  ]
+import ../../../waku/[waku_core/message, waku_store]
 
-
-let FUTURE_TIMEOUT* = 1.seconds
+const
+  FUTURE_TIMEOUT* = 1.seconds
+  FUTURE_TIMEOUT_LONG* = 10.seconds
 
 proc newPushHandlerFuture*(): Future[(string, WakuMessage)] =
-    newFuture[(string, WakuMessage)]()
+  newFuture[(string, WakuMessage)]()
 
 proc newBoolFuture*(): Future[bool] =
-    newFuture[bool]()
+  newFuture[bool]()
 
 proc newHistoryFuture*(): Future[HistoryQuery] =
-    newFuture[HistoryQuery]()
+  newFuture[HistoryQuery]()
