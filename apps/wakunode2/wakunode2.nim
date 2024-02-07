34d207c4c2f3f4c6c712f546709d1ec4121e91fa
Alvaro Revuelta <alvrevuelta@gmail.com>
2024-02-07 12:42:20 +0100

    chore: Simplify configuration for the waku network (#2404)

--- apps/wakunode2/wakunode2.nim
+++ apps/wakunode2/wakunode2.nim
@@ -4,7 +4,7 @@ else:
   {.push raises: [].}
 
 import
-  std/[options, strutils, os],
+  std/[options, strutils, os, sequtils],
   stew/shims/net as stewNet,
   chronicles,
   chronos,
@@ -16,12 +16,37 @@ import
   ../../tools/rln_keystore_generator/rln_keystore_generator,
   ../../waku/common/logging,
   ./external_config,
+  ./networks_config,
   ./app
 
 logScope:
   topics = "wakunode main"
 
-{.pop.} # @TODO confutils.nim(775, 17) Error: can raise an unlisted exception: ref IOError
+proc logConfig(conf: WakuNodeConf) =
+  info "Configuration: Enabled protocols",
+    relay = conf.relay,
+    rlnRelay = conf.rlnRelay,
+    store = conf.store,
+    filter = conf.filter,
+    lightpush = conf.lightpush,
+    peerExchange = conf.peerExchange
+
+  info "Configuration. Network",
+    cluster = conf.clusterId,
+    pubsubTopics = conf.pubsubTopics,
+    maxPeers = conf.maxRelayPeers
+
+  for i in conf.discv5BootstrapNodes:
+    info "Configuration. Bootstrap nodes", node = i
+
+  if conf.rlnRelay and conf.rlnRelayDynamic:
+    info "Configuration. Validation",
+      mechanism = "onchain rln",
+      contract = conf.rlnRelayEthContractAddress,
+      maxMessageSize = conf.maxMessageSize
+
+{.pop.}
+  # @TODO confutils.nim(775, 17) Error: can raise an unlisted exception: ref IOError
 when isMainModule:
   ## Node setup happens in 6 phases:
   ## 1. Set up storage
@@ -34,23 +59,48 @@ when isMainModule:
   const versionString = "version / git commit hash: " & app.git_version
   let rng = crypto.newRng()
 
-  let confRes = WakuNodeConf.load(version=versionString)
+  let confRes = WakuNodeConf.load(version = versionString)
   if confRes.isErr():
-    error "failure while loading the configuration", error=confRes.error
+    error "failure while loading the configuration", error = confRes.error
     quit(QuitFailure)
 
-  let conf = confRes.get()
+  var conf = confRes.get()
+
+  # The Waku Network config (cluster-id=1)
+  if conf.clusterId == 1:
+    let twnClusterConf = ClusterConf.TheWakuNetworkConf()
+    if len(conf.shards) != 0:
+      conf.pubsubTopics = conf.shards.mapIt(twnClusterConf.pubsubTopics[it.uint16])
+    else:
+      conf.pubsubTopics = twnClusterConf.pubsubTopics
+
+    # Override configuration
+    conf.maxMessageSize = twnClusterConf.maxMessageSize
+    conf.clusterId = twnClusterConf.clusterId
+    conf.rlnRelay = twnClusterConf.rlnRelay
+    conf.rlnRelayEthContractAddress = twnClusterConf.rlnRelayEthContractAddress
+    conf.rlnRelayDynamic = twnClusterConf.rlnRelayDynamic
+    conf.rlnRelayBandwidthThreshold = twnClusterConf.rlnRelayBandwidthThreshold
+    conf.discv5Discovery = twnClusterConf.discv5Discovery
+    conf.discv5BootstrapNodes =
+      conf.discv5BootstrapNodes & twnClusterConf.discv5BootstrapNodes
 
   ## Logging setup
 
   # Adhere to NO_COLOR initiative: https://no-color.org/
-  let color = try: not parseBool(os.getEnv("NO_COLOR", "false"))
-              except CatchableError: true
+  let color =
+    try:
+      not parseBool(os.getEnv("NO_COLOR", "false"))
+    except CatchableError:
+      true
 
   logging.setupLogLevel(conf.logLevel)
   logging.setupLogFormat(conf.logFormat, color)
 
-  case conf.cmd:
+  info "Running nwaku node", version = app.git_version
+  logConfig(conf)
+
+  case conf.cmd
   of generateRlnKeystore:
     doRlnKeystoreGenerator(conf)
   of noCommand:
@@ -65,42 +115,42 @@ when isMainModule:
     ## Peer persistence
     let res1 = wakunode2.setupPeerPersistence()
     if res1.isErr():
-      error "1/7 Setting up storage failed", error=res1.error
+      error "1/7 Setting up storage failed", error = res1.error
       quit(QuitFailure)
 
     debug "2/7 Retrieve dynamic bootstrap nodes"
 
     let res3 = wakunode2.setupDyamicBootstrapNodes()
     if res3.isErr():
-      error "2/7 Retrieving dynamic bootstrap nodes failed", error=res3.error
+      error "2/7 Retrieving dynamic bootstrap nodes failed", error = res3.error
       quit(QuitFailure)
 
     debug "3/7 Initializing node"
 
     let res4 = wakunode2.setupWakuApp()
     if res4.isErr():
-      error "3/7 Initializing node failed", error=res4.error
+      error "3/7 Initializing node failed", error = res4.error
       quit(QuitFailure)
 
     debug "4/7 Mounting protocols"
 
     let res5 = waitFor wakunode2.setupAndMountProtocols()
     if res5.isErr():
-      error "4/7 Mounting protocols failed", error=res5.error
+      error "4/7 Mounting protocols failed", error = res5.error
       quit(QuitFailure)
 
     debug "5/7 Starting node and mounted protocols"
 
     let res6 = wakunode2.startApp()
     if res6.isErr():
-      error "5/7 Starting node and protocols failed", error=res6.error
+      error "5/7 Starting node and protocols failed", error = res6.error
       quit(QuitFailure)
 
     debug "6/7 Starting monitoring and external interfaces"
 
     let res7 = wakunode2.setupMonitoringAndExternalInterfaces()
     if res7.isErr():
-      error "6/7 Starting monitoring and external interfaces failed", error=res7.error
+      error "6/7 Starting monitoring and external interfaces failed", error = res7.error
       quit(QuitFailure)
 
     debug "7/7 Setting up shutdown hooks"
@@ -133,7 +183,7 @@ when isMainModule:
     when defined(posix):
       proc handleSigsegv(signal: cint) {.noconv.} =
         # Require --debugger:native
-        fatal "Shutting down after receiving SIGSEGV", stacktrace=getBacktrace()
+        fatal "Shutting down after receiving SIGSEGV", stacktrace = getBacktrace()
 
         # Not available in -d:release mode
         writeStackTrace()
