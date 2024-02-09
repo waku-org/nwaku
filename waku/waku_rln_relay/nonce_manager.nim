c3e7c589e318b49d81a7d5ee8dd1b1396a964840
rymnc <43716372+rymnc@users.noreply.github.com>
2024-02-09 18:00:46 +0530

    feat(rln-relay-v2): nonce/messageId manager

---
+++ waku/waku_rln_relay/nonce_manager.nim
@@ -0,0 +1,69 @@
+when (NimMajor, NimMinor) < (1, 4):
+  {.push raises: [Defect].}
+else:
+  {.push raises: [].}
+
+import
+  chronos,
+  stew/results,
+  times
+import
+  ./constants
+
+export
+  chronos,
+  times,
+  results,
+  constants
+
+# This module contains the NonceManager interface
+# The NonceManager is responsible for managing the messageId used to generate RLN proofs
+# It should be used to fetch a new messageId every time a proof is generated
+# It refreshes the messageId every `epoch` seconds
+
+type
+  Nonce* = uint64
+  NonceManager* = ref object of RootObj
+    epoch*: float64
+    nextNonce*: Nonce
+    lastNonceTime*: float64
+    nonceLimit*: Nonce
+
+  NonceManagerErrorKind* = enum
+    NonceLimitReached
+
+  NonceManagerError* = object
+    kind*: NonceManagerErrorKind
+    error*: string
+
+  NonceManagerResult*[T] = Result[T, NonceManagerError]
+
+proc `$`*(ne: NonceManagerError): string =
+  case ne.kind
+  of NonceLimitReached:
+    return "NonceLimitReached: " & ne.error
+
+proc init*(T: type NonceManager, nonceLimit: Nonce, epoch = EpochUnitSeconds): T =
+  return NonceManager(
+    epoch: epoch,
+    nextNonce: 0,
+    lastNonceTime: 0,
+    nonceLimit: nonceLimit
+  )
+
+
+proc get*(n: NonceManager): NonceManagerResult[Nonce] =
+  let now = getTime().toUnixFloat()
+  var retNonce = n.nextNonce
+
+  if now - n.lastNonceTime < n.epoch: retNonce = n.nextNonce
+  else:                               retNonce = 0
+
+  n.nextNonce = retNonce + 1
+  n.lastNonceTime = now
+
+  if retNonce >= n.nonceLimit:
+    return err(NonceManagerError(kind: NonceLimitReached, 
+                                 error: "Nonce limit reached. Please wait for the next epoch"))
+  
+  return ok(retNonce)
