5afc1c837275d11b54fa8dee1df0097228ebba6a
SionoiS <simvivier@status.im>
2024-02-09 09:18:53 -0500

    Wrapping

--- waku/waku_sync/raw_bindings.nim
+++ waku/waku_sync/raw_bindings.nim
@@ -3,8 +3,11 @@ when (NimMajor, NimMinor) < (1, 4):
 else:
   {.push raises: [].}
 
+import
+   ../waku_core/message
 
-{.link: "../waku_sync/negentropy.so".}
+
+{.link: "../waku_sync/negentropy.so".} #TODO build the dyn lib
 
 type
   StdString {.header: "<string>", importcpp: "std::string".} = object
@@ -17,10 +20,10 @@ type
   Storage = ptr StorageObj
 
 # https://github.com/hoytech/negentropy/blob/6e1e6083b985adcdce616b6bb57b6ce2d1a48ec1/cpp/negentropy/storage/btree/core.h#L163
-proc insert(this: Storage, timestamp: clong, id: StdString) {.importcpp: "negentropy::storage::btree::insert".}
+proc raw_insert(this: Storage, timestamp: clong, id: StdString) {.importcpp: "negentropy::storage::btree::insert".}
 
 # https://github.com/hoytech/negentropy/blob/6e1e6083b985adcdce616b6bb57b6ce2d1a48ec1/cpp/negentropy/storage/btree/core.h#L300
-proc erase(this: Storage, timestamp: clong, id: StdString) {.importcpp: "negentropy::storage::btree::erase".}
+proc raw_erase(this: Storage, timestamp: clong, id: StdString) {.importcpp: "negentropy::storage::btree::erase".}
 
 ### Negentropy ###
 
@@ -33,13 +36,39 @@ type
 proc constructNegentropy(storage: Storage, frameSizeLimit: culong): Negentropy {.importcpp: "negentropy::Negentropy(@)", constructor.}
 
 # https://github.com/hoytech/negentropy/blob/6e1e6083b985adcdce616b6bb57b6ce2d1a48ec1/cpp/negentropy.h#L46
-proc initiate(this: Negentropy): StdString {.importcpp: "negentropy::initiate".}
+proc raw_initiate(this: Negentropy): StdString {.importcpp: "negentropy::initiate".}
 
 # https://github.com/hoytech/negentropy/blob/6e1e6083b985adcdce616b6bb57b6ce2d1a48ec1/cpp/negentropy.h#L58
-proc setInitiator(this: Negentropy) {.importcpp: "negentropy::setInitiator".}
+proc raw_setInitiator(this: Negentropy) {.importcpp: "negentropy::setInitiator".}
 
 # https://github.com/hoytech/negentropy/blob/6e1e6083b985adcdce616b6bb57b6ce2d1a48ec1/cpp/negentropy.h#L62
-proc reconcile(this: Negentropy, query: StdString): StdString {.importcpp: "negentropy::reconcile".}
+proc raw_reconcile(this: Negentropy, query: StdString): StdString {.importcpp: "negentropy::reconcile".}
+
+# How to update haveIDs and needIds from C++ side ???
 
 # https://github.com/hoytech/negentropy/blob/6e1e6083b985adcdce616b6bb57b6ce2d1a48ec1/cpp/negentropy.h#L69
-proc reconcile(this: Negentropy, query: StdString, haveIds: var seq[StdString], needIds: var seq[StdString]): StdString {.importcpp: "negentropy::reconcile".}
+proc raw_reconcile(this: Negentropy, query: StdString, haveIds: var seq[StdString], needIds: var seq[StdString]): StdString {.importcpp: "negentropy::reconcile".}
+
+### Bindings ###
+
+#[ proc new*(T: type Negentropy, frameSizeLimit: uint64): T =
+
+  let storage = Storage() # if there's no constructor how do you instantiate this ???
+
+  let negentropy = constructNegentropy(storage, frameSizeLimit)
+  
+  return negentropy ]#
+
+#[ proc reconcile*(self: Negentropy, query: seq[byte], haveIds: var seq[WakuMessageHash], needIds: var seq[WakuMessageHash]): seq[byte] =
+  let cppQuery = query.into() # TODO How do we turn Nim seq[byte] into C++ StdString ???
+  
+  # How to go from seq[WakuMessageHash] to std::vector<std::string> ???
+  var 
+    haveIds: seq[WakuMessageHash]
+    needIds: seq[WakuMessageHash]
+
+  let cppString = self.raw_reconcile(cppQuery, var haveIds, var needIds)
+
+  let payload = cppString.into()  # TODO How do we turn C++ StdString into a useable Nim seq[byte] ???
+
+  return payload ]#
