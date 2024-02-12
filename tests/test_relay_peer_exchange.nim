f462c0636cd168a40e952afddfd11150a632be31
Álex Cabeza Romero <alex93cabeza@gmail.com>
2024-02-12 20:58:32 +0100

    Implement mount relay handler without gossipsub's peer exchange handler.

--- tests/test_relay_peer_exchange.nim
+++ tests/test_relay_peer_exchange.nim
@@ -17,6 +17,28 @@ import
   ./testlib/wakunode
 
 procSuite "Relay (GossipSub) Peer Exchange":
+  asyncTest "Mount relay without peer exchange handler":
+    # Given two nodes
+    let
+      listenAddress = parseIpAddress("0.0.0.0")
+      port = Port(0)
+      node1Key = generateSecp256k1Key()
+      node1 = newTestWakuNode(node1Key, listenAddress, port)
+      node2Key = generateSecp256k1Key()
+      node2 =
+        newTestWakuNode(node2Key, listenAddress, port, sendSignedPeerRecord = true)
+
+    # When both client and server mount relay without a handler
+    await node1.mountRelay(@[DefaultPubsubTopic])
+    await node2.mountRelay(@[DefaultPubsubTopic], none(RoutingRecordsHandler))
+
+    # Then the relays are mounted without a handler
+    check:
+      node1.wakuRelay.parameters.enablePX == false
+      node1.wakuRelay.routingRecordsHandler.len == 0
+      node2.wakuRelay.parameters.enablePX == false
+      node2.wakuRelay.routingRecordsHandler.len == 0
+
   asyncTest "Mount relay with peer exchange handler":
     ## Given three nodes
     # Create nodes and ENR. These will be added to the discoverable list
