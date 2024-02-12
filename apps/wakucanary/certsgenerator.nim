aab8b002222b5b4df25df4060186801b797067eb
Gabriel mermelstein <gabrielmermelstein@gmail.com>
2024-02-12 10:44:52 +0200

    Using IP in certificate

--- apps/wakucanary/certsgenerator.nim
+++ apps/wakucanary/certsgenerator.nim
@@ -1,4 +1,17 @@
-import osproc, os
+import 
+  osproc, 
+  os, 
+  httpclient, 
+  strutils
+
+proc getPublicIP(): string =
+  let client = newHttpClient()
+  try:
+    let response = client.get("http://api.ipify.org")
+    return response.body
+  except Exception as e:
+    echo "Could not fetch public IP: " & e.msg
+    return "127.0.0.1"
 
 # Function to generate a self-signed certificate
 proc generateSelfSignedCertificate*(certPath: string, keyPath: string) : int =
@@ -8,10 +21,16 @@ proc generateSelfSignedCertificate*(certPath: string, keyPath: string) : int =
     echo "OpenSSL is not installed or not in the PATH."
     return 1
 
+  let publicIP = getPublicIP()
+  
+  if publicIP != "127.0.0.1":
+    echo "Your public IP address is: ", publicIP 
+  
   # Command to generate private key and cert
   let 
     cmd = "openssl req -x509 -newkey rsa:4096 -keyout " & keyPath & " -out " & certPath &
-            " -sha256 -days 3650 -nodes -subj '/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname'"
+            " -sha256 -days 3650 -nodes -subj '/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=" & 
+            publicIP & "'"
     res = execCmd(cmd)
 
   if res == 0:
