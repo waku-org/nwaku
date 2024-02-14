5751afb9b9a6bb672e846c275ee5422146915dad
Richard Ramos <info@richardramos.me>
2024-02-14 16:50:24 -0400

    fix(bindings): base64 payload and key for content topic

--- library/events/json_message_event.nim
+++ library/events/json_message_event.nim
@@ -1,16 +1,33 @@
 
 import
+    system,
     std/json
 import
+    ../../waku/common/base64,
     ../../waku/waku_core/message/message,
     ./json_base_event
 
-type JsonMessage = ref object
-  # https://rfc.vac.dev/spec/36/#jsonmessage-type
-  payload: string
-  contentTopic: string
-  version: uint
-  timestamp: int64
+type
+  JsonMessage* = ref object
+    # https://rfc.vac.dev/spec/36/#jsonmessage-type
+    payload*: Base64String
+    contentTopic*: string
+    version: uint
+    timestamp: int64
+
+func toJsonMessage*(msg: WakuMessage): JsonMessage =
+  JsonMessage(
+    payload: base64.encode(msg.payload),
+    contentTopic: msg.contentTopic,
+    version: msg.version,
+    timestamp: msg.timestamp
+  )
+
+proc `%`*(value: Base64String): JsonNode =
+  %(value.string)
+
+proc fromJson*(n: JsonNode, argName: string, value: var Base64String) =
+  value = Base64String(n.getStr())
 
 type JsonMessageEvent* = ref object of JsonEvent
     pubsubTopic*: string
@@ -23,7 +40,7 @@ proc new*(T: type JsonMessageEvent,
   # Returns a WakuMessage event as indicated in
   # https://rfc.vac.dev/spec/36/#jsonmessageevent-type
 
-  var payload = newString(len(msg.payload))
+  var payload = newSeq[byte](len(msg.payload))
   copyMem(addr payload[0], unsafeAddr msg.payload[0], len(msg.payload))
 
   return JsonMessageEvent(
@@ -31,7 +48,7 @@ proc new*(T: type JsonMessageEvent,
     pubSubTopic: pubSubTopic,
     messageId: "TODO",
     wakuMessage: JsonMessage(
-        payload: payload,
+        payload: base64.encode(payload),
         contentTopic: msg.contentTopic,
         version: msg.version,
         timestamp: int64(msg.timestamp)
