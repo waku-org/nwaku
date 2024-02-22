0887a344a4791bccab5e0b9d4071ab74fbb352c3
Ivan FB <128452529+Ivansete-status@users.noreply.github.com>
2024-02-22 16:55:37 +0100

    refactor: new proc to foster different size retention policy implementations (#2463)
    * new proc to foster different size retention policy implementations
      The new proc, decreaseDatabaseSize, will have different implementations
      per each driver. For example, in future commits we will implement a size
      retention policy thanks to partitions management, in Postgres.
    * RetentionPolicy: use of new instead of init for ref object types
    * waku_archive: fix signatures in decreaseDatabaseSize methods
    * retention_policy_size: minor cleanup of comments and imports

--- waku/waku_archive/driver/queue_driver/queue_driver.nim
+++ waku/waku_archive/driver/queue_driver/queue_driver.nim
@@ -317,6 +317,11 @@ method deleteOldestMessagesNotWithinLimit*(driver: QueueDriver,
   # TODO: Implement this message_store method
   return err("interface method not implemented")
 
+method decreaseDatabaseSize*(driver: QueueDriver,
+                             targetSizeInBytes: int64):
+                             Future[ArchiveDriverResult[void]] {.async.} =
+  return err("interface method not implemented")
+
 method close*(driver: QueueDriver):
               Future[ArchiveDriverResult[void]] {.async.} =
   return ok()
