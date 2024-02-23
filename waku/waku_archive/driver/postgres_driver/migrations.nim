26d7cf66238c270e25a8cf2e1163cf8c46876923
Ivan Folgueira Bande <ivansete@status.im>
2024-02-23 01:42:23 +0100

    Add postgres_driver/migrations.nim

---
+++ waku/waku_archive/driver/postgres_driver/migrations.nim
@@ -0,0 +1,58 @@
+{.push raises: [].}
+
+import
+  std/[tables, strutils, os],
+  stew/results,
+  chronicles,
+  chronos
+import
+  ../../../common/databases/common,
+  ../../../../migrations/migration_utils,
+  ../postgres_driver
+
+logScope:
+  topics = "waku archive migration"
+
+const SchemaVersion* = 1 # increase this when there is an update in the database schema
+
+template projectRoot: string = currentSourcePath.rsplit(DirSep, 1)[0] / ".." / ".." / ".." / ".."
+const StoreMigrationPath: string = projectRoot / "migrations" / "message_store_postgres"
+
+proc migrate*(driver: PostgresDriver,
+              targetVersion = SchemaVersion):
+              Future[DatabaseResult[void]] {.async.} =
+
+  debug "starting message store's postgres database migration"
+
+  let currentVersion = (await driver.getCurrentVersion()).valueOr:
+    return err("migrate error could not retrieve current version: " & $error)
+
+  if currentVersion == targetVersion:
+    debug "database schema is up to date",
+          currentVersion=currentVersion, targetVersion=targetVersion
+    return ok()
+
+  info "database schema is outdated", currentVersion=currentVersion, targetVersion=targetVersion
+
+  # Load migration scripts
+  let scripts = migration_utils.loadMigrationScripts(migrationsScriptsDir=StoreMigrationPath,
+                                                     currentVersion,
+                                                     targetVersion).valueOr:
+                return err("postgres could not load migration scripts: " & $error)
+
+  # Run the migration scripts
+  for script in scripts:
+
+    for statement in script.breakIntoStatements():
+      debug "executing migration statement", statement=statement
+
+      (await driver.performWriteQuery(statement)).isOkOr:
+        error "failed to execute migration statement", statement=statement, error=error
+        return err("failed to execute migration statement")
+
+      debug "migration statement executed succesfully", statement=statement
+
+  debug "finished message store's postgres database migration"
+
+  return ok()
+
