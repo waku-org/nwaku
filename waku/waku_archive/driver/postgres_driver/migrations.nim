e7df8bb6cc32b1f9ea8c5e54884a2e831b712217
Ivan Folgueira Bande <ivansete@status.im>
2024-02-27 10:01:22 +0100

    postgres_driver/migrations.nim: add breakInfoStatements proc

--- waku/waku_archive/driver/postgres_driver/migrations.nim
+++ waku/waku_archive/driver/postgres_driver/migrations.nim
@@ -16,6 +16,49 @@ logScope:
 
 const SchemaVersion* = 1 # increase this when there is an update in the database schema
 
+proc breakIntoStatements*(script: string): seq[string] =
+  ## Given a full migration script, that can potentially contain a list
+  ## of SQL statements, this proc splits it into the contained isolated statements
+  ## that should be executed one after the other.
+  var statements = newSeq[string]()
+
+  let lines = script.split('\n')
+
+  var simpleStmt: string
+  var plSqlStatement: string
+  var insidePlSqlScript = false
+  for line in lines:
+    if line.strip().len == 0:
+      continue
+
+    if insidePlSqlScript:
+      if line.contains("END $$"):
+        ## End of the Pl/SQL script
+        plSqlStatement &= line
+        statements.add(plSqlStatement)
+        plSqlStatement = ""
+        insidePlSqlScript = false
+        continue
+
+      else:
+        plSqlStatement &= line & "\n"
+
+    if line.contains("DO $$"):
+      ## Beginning of the Pl/SQL script
+      insidePlSqlScript = true
+      plSqlStatement &= line & "\n"
+
+    if not insidePlSqlScript:
+      if line.contains(';'):
+        ## End of simple statement
+        simpleStmt &= line
+        statements.add(simpleStmt)
+        simpleStmt = ""
+      else:
+        simpleStmt &= line & "\n"
+
+  return statements
+
 proc migrate*(driver: PostgresDriver,
               targetVersion = SchemaVersion):
               Future[DatabaseResult[void]] {.async.} =
