c45bc69ea1122ebfe581906538b6e1c65585f6a2
Ivan Folgueira Bande <ivansete@status.im>
2024-02-27 08:30:09 +0100

    partially revert migration-logic changes in sql

--- migrations/migration_utils.nim
+++ migrations/migration_utils.nim
@@ -1,24 +1,10 @@
 import
-  std/[strutils,sequtils,os,algorithm],
-  stew/results
-import
-  chronicles
-import
-  ../waku/common/databases/common
-
-## Migration scripts
-
-proc getMigrationScriptVersion(path: string): DatabaseResult[int64] =
-  let name = extractFilename(path)
-  let parts = name.split("_", 1)
-
-  try:
-    let version = parseInt(parts[0])
-    return ok(version)
-  except ValueError:
-    return err("failed to parse file version: " & name)
+  std/strutils
 
 proc breakIntoStatements*(script: string): seq[string] =
+  ## Given a full migration script, that can potentially contain a list
+  ## of SQL statements, this proc splits it into the contained isolated statements
+  ## that should be executed one after the other.
   var statements = newSeq[string]()
 
   let lines = script.split('\n')
@@ -57,68 +43,3 @@ proc breakIntoStatements*(script: string): seq[string] =
 
   return statements
 
-proc isSqlScript(path: string): bool =
-  path.toLower().endsWith(".sql")
-
-proc listSqlScripts(path: string): DatabaseResult[seq[string]] =
-  var scripts = newSeq[string]()
-
-  try:
-    for scriptPath in walkDirRec(path):
-      if isSqlScript(scriptPath):
-        scripts.add(scriptPath)
-      else:
-        debug "invalid migration script", file=scriptPath
-  except OSError:
-    return err("failed to list migration scripts: " & getCurrentExceptionMsg())
-
-  ok(scripts)
-
-proc filterMigrationScripts(paths: seq[string],
-                            lowVersion, highVersion: int64,
-                            direction: string = "up"):
-                            seq[string] =
-  ## Returns migration scripts whose version fall between lowVersion and highVersion (inclusive)
-  let filterPredicate = proc(script: string): bool =
-      if not isSqlScript(script):
-        return false
-
-      if direction != "" and not script.toLower().endsWith("." & direction & ".sql"):
-        return false
-
-      let scriptVersionRes = getMigrationScriptVersion(script)
-      if scriptVersionRes.isErr():
-        return false
-
-      let scriptVersion = scriptVersionRes.value
-      return lowVersion < scriptVersion and scriptVersion <= highVersion
-
-  paths.filter(filterPredicate)
-
-proc sortMigrationScripts(paths: seq[string]): seq[string] =
-  ## Sort migration scripts paths alphabetically
-  paths.sorted(system.cmp[string])
-
-proc loadMigrationScripts*(migrationsScriptsDir: string,
-                           userVersion: int64,
-                           targetVersion: int64): DatabaseResult[seq[string]] =
-
-  var migrationScriptsPaths = ? listSqlScripts(migrationsScriptsDir)
-  migrationScriptsPaths = filterMigrationScripts(migrationScriptsPaths,
-                                                 lowVersion=userVersion,
-                                                 highVersion=targetVersion,
-                                                 direction="up")
-  migrationScriptsPaths = sortMigrationScripts(migrationScriptsPaths)
-
-  if migrationScriptsPaths.len <= 0:
-    return err("no scripts to be run")
-
-  var loadedScripts = newSeq[string]()
-
-  for script in migrationScriptsPaths:
-    try:
-      loadedScripts.add(readFile(script))
-    except OSError, IOError:
-      return err("failed to load script '" & script & "': " & getCurrentExceptionMsg())
-
-  ok(loadedScripts)
--- waku/common/databases/db_sqlite.nim
+++ waku/common/databases/db_sqlite.nim
@@ -363,6 +363,71 @@ proc setUserVersion*(database: SqliteDatabase, version: int64):
 
   ok()
 
+## Migration scripts
+
+proc getMigrationScriptVersion(path: string): DatabaseResult[int64] =
+  let name = extractFilename(path)
+  let parts = name.split("_", 1)
+
+  try:
+    let version = parseInt(parts[0])
+    return ok(version)
+  except ValueError:
+    return err("failed to parse file version: " & name)
+
+proc isSqlScript(path: string): bool =
+  path.toLower().endsWith(".sql")
+
+proc listSqlScripts(path: string): DatabaseResult[seq[string]] =
+  var scripts = newSeq[string]()
+
+  try: 
+    for scriptPath in walkDirRec(path):
+      if isSqlScript(scriptPath):
+        scripts.add(scriptPath)
+      else:
+        debug "invalid migration script", file=scriptPath
+  except OSError:
+    return err("failed to list migration scripts: " & getCurrentExceptionMsg())
+
+  ok(scripts)
+
+proc filterMigrationScripts(paths: seq[string],
+                            lowVersion, highVersion: int64,
+                            direction: string = "up"):
+                            seq[string] =
+  ## Returns migration scripts whose version fall between lowVersion and highVersion (inclusive)
+  let filterPredicate = proc(script: string): bool =
+      if not isSqlScript(script):
+        return false
+
+      if direction != "" and not script.toLower().endsWith("." & direction & ".sql"):
+        return false
+
+      let scriptVersionRes = getMigrationScriptVersion(script)
+      if scriptVersionRes.isErr():
+        return false
+
+      let scriptVersion = scriptVersionRes.value
+      return lowVersion < scriptVersion and scriptVersion <= highVersion
+
+  paths.filter(filterPredicate)
+
+proc sortMigrationScripts(paths: seq[string]): seq[string] =
+  ## Sort migration scripts paths alphabetically
+  paths.sorted(system.cmp[string])
+
+proc loadMigrationScripts(paths: seq[string]): DatabaseResult[seq[string]] =
+  var loadedScripts = newSeq[string]()
+
+  for script in paths:
+    try:
+      loadedScripts.add(readFile(script))
+    except OSError, IOError:
+      return err("failed to load script '" & script & "': " & getCurrentExceptionMsg())
+
+  ok(loadedScripts)
+
 proc migrate*(db: SqliteDatabase,
               targetVersion: int64,
               migrationsScriptsDir: string):
@@ -382,9 +447,16 @@ proc migrate*(db: SqliteDatabase,
   info "database schema is outdated", userVersion=userVersion, targetVersion=targetVersion
 
   # Load migration scripts
-  let scripts = ? migration_utils.loadMigrationScripts(migrationsScriptsDir,
-                                                       userVersion,
-                                                       targetVersion)
+  var migrationScriptsPaths = ?listSqlScripts(migrationsScriptsDir)
+  migrationScriptsPaths = filterMigrationScripts(migrationScriptsPaths, lowVersion=userVersion, highVersion=targetVersion, direction="up")
+  migrationScriptsPaths = sortMigrationScripts(migrationScriptsPaths)
+
+  if migrationScriptsPaths.len <= 0:
+    debug "no scripts to be run"
+    return ok()
+
+  let scripts = ?loadMigrationScripts(migrationScriptsPaths)
+
   # Run the migration scripts
   for script in scripts:
 
