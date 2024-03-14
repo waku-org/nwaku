ed4a30caa2b7210e0ecbb28e95944604ab4d6b81
Álex Cabeza Romero <alex93cabeza@gmail.com>
2024-03-14 18:27:07 +0100

    Update nested test_all

--- tests/waku_rln_relay/test_all.nim
+++ tests/waku_rln_relay/test_all.nim
@@ -1,10 +1,10 @@
 {.used.}
 
 import ./rln/test_all,
-  # ./test_rln_group_manager_onchain,
-  # ./test_rln_group_manager_static,
+  ./test_rln_group_manager_onchain,
+  ./test_rln_group_manager_static,
   ./test_rln_nonce_manager,
-  # ./test_waku_rln_relay,
+  ./test_waku_rln_relay,
   ./test_wakunode_rln_relay
 
 when defined(rln_v2):
--- tests/wakunode_rest/test_all.nim
+++ tests/wakunode_rest/test_all.nim
@@ -2,6 +2,7 @@
 
 import
   ./test_rest_admin,
+  ./test_rest_cors,
   ./test_rest_debug_serdes,
   ./test_rest_debug,
   ./test_rest_filter,
@@ -11,6 +12,4 @@ import
   ./test_rest_relay_serdes,
   ./test_rest_relay,
   ./test_rest_serdes,
-  ./test_rest_store,
-  ./test_rest_admin,
-  ./test_rest_cors
+  ./test_rest_store
