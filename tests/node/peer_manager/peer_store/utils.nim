125df48bf100255b133c6ec0688e19c09254c5ec
Álex Cabeza Romero <alex93cabeza@gmail.com>
2024-03-19 17:57:31 +0100

    Make some useful consts public, add some utils.

--- tests/node/peer_manager/peer_store/utils.nim
+++ tests/node/peer_manager/peer_store/utils.nim
@@ -1,12 +1,9 @@
-import std/options, stew/results, libp2p/peerstore
+import stew/results
 
 import
-  waku/node/peer_manager/[waku_peer_store, peer_store/waku_peer_storage],
+  ../../../../waku/node/peer_manager/peer_store/waku_peer_storage,
   ../../../waku_archive/archive_utils
 
-proc newTestWakuPeerStorage*(path: Option[string] = string.none()): WakuPeerStorage =
-  let db = newSqliteDatabase(path)
+proc newTestWakuPeerStorage*(): WakuPeerStorage =
+  let db = newSqliteDatabase()
   WakuPeerStorage.new(db).value()
-
-proc peerExists*(peerStore: PeerStore, peerId: PeerId): bool =
-  return peerStore[AddressBook].contains(peerId)
