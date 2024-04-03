cf485ab54a02116165d295507ac142d873835d2a
Álex Cabeza Romero <alex93cabeza@gmail.com>
2024-04-03 17:25:00 +0200

    Implement various utilities.

--- tests/node/peer_manager/peer_store/utils.nim
+++ tests/node/peer_manager/peer_store/utils.nim
@@ -1,9 +1,12 @@
-import stew/results
+import std/options, stew/results, libp2p/peerstore
 
 import
-  ../../../../waku/node/peer_manager/peer_store/waku_peer_storage,
+  ../../../../waku/node/peer_manager/[waku_peer_store, peer_store/waku_peer_storage],
   ../../../waku_archive/archive_utils
 
-proc newTestWakuPeerStorage*(): WakuPeerStorage =
-  let db = newSqliteDatabase()
+proc newTestWakuPeerStorage*(path: Option[string] = string.none()): WakuPeerStorage =
+  let db = newSqliteDatabase(path)
   WakuPeerStorage.new(db).value()
+
+proc peerExists*(peerStore: PeerStore, peerId: PeerId): bool =
+  return peerStore[AddressBook].contains(peerId)
