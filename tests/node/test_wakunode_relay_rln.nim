f5544330a7007a1b2df32d3836abc55bf5196ca7
Álex Cabeza Romero <alex93cabeza@gmail.com>
2024-04-18 15:50:05 +0200

    Move relay utils to their own module.

--- tests/node/test_wakunode_relay_rln.nim
+++ tests/node/test_wakunode_relay_rln.nim
@@ -16,70 +16,9 @@ import
     [node/waku_node, node/peer_manager, waku_core, waku_node, waku_rln_relay],
   ../waku_store/store_utils,
   ../waku_archive/archive_utils,
+  ../waku_relay/utils,
   ../testlib/[wakucore, wakunode, testasync, futures],
   ../resources/payloads
-
-proc setupRln(node: WakuNode, identifier: uint) {.async.} =
-  await node.mountRlnRelay(
-    WakuRlnConfig(
-      rlnRelayDynamic: false,
-      rlnRelayCredIndex: some(identifier),
-      rlnRelayTreePath: genTempPath("rln_tree", "wakunode_" & $identifier),
-      rlnEpochSizeSec: 1,
-    )
-  )
-
-proc setupRelayWithRln(
-    node: WakuNode, identifier: uint, pubsubTopics: seq[string]
-) {.async.} =
-  await node.mountRelay(pubsubTopics)
-  await setupRln(node, identifier)
-
-proc subscribeCompletionHandler(node: WakuNode, pubsubTopic: string): Future[bool] =
-  var completionFut = newFuture[bool]()
-  proc relayHandler(
-      topic: PubsubTopic, msg: WakuMessage
-  ): Future[void] {.async, gcsafe.} =
-    if topic == pubsubTopic:
-      completionFut.complete(true)
-
-  node.subscribe((kind: PubsubSub, topic: pubsubTopic), some(relayHandler))
-  return completionFut
-
-proc sendRlnMessage(
-    client: WakuNode,
-    pubsubTopic: string,
-    contentTopic: string,
-    completionFuture: Future[bool],
-    payload: seq[byte] = "Hello".toBytes(),
-): Future[bool] {.async.} =
-  var message = WakuMessage(payload: payload, contentTopic: contentTopic)
-  doAssert(client.wakuRlnRelay.appendRLNProof(message, epochTime()).isOk())
-  discard await client.publish(some(pubsubTopic), message)
-  let isCompleted = await completionFuture.withTimeout(FUTURE_TIMEOUT)
-  return isCompleted
-
-proc sendRlnMessageWithInvalidProof(
-    client: WakuNode,
-    pubsubTopic: string,
-    contentTopic: string,
-    completionFuture: Future[bool],
-    payload: seq[byte] = "Hello".toBytes(),
-): Future[bool] {.async.} =
-  let
-    extraBytes: seq[byte] = @[byte(1), 2, 3]
-    rateLimitProofRes = client.wakuRlnRelay.groupManager.generateProof(
-      concat(payload, extraBytes),
-        # we add extra bytes to invalidate proof verification against original payload
-      client.wakuRlnRelay.getCurrentEpoch(),
-    )
-    rateLimitProof = rateLimitProofRes.get().encode().buffer
-    message =
-      WakuMessage(payload: @payload, contentTopic: contentTopic, proof: rateLimitProof)
-
-  discard await client.publish(some(pubsubTopic), message)
-  let isCompleted = await completionFuture.withTimeout(FUTURE_TIMEOUT)
-  return isCompleted
 
 suite "Waku RlnRelay - End to End":
   var
