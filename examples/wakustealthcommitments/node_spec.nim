40df1c3dc8910e0137d03c7aa14a3fad81cd193d
Ivan Folgueira Bande <ivansete@status.im>
2024-05-02 14:42:52 +0200

    adapt wakustealthcommitments codebase to the current refactor

--- examples/wakustealthcommitments/node_spec.nim
+++ examples/wakustealthcommitments/node_spec.nim
@@ -3,8 +3,8 @@ when (NimMajor, NimMinor) < (1, 4):
 else:
   {.push raises: [].}
 
-import ../../apps/wakunode2/[networks_config, app, external_config]
-import ../../waku/common/logging
+import
+  ../../waku/common/logging, ../../waku/factory/[waku, networks_config, external_config]
 import
   std/[options, strutils, os, sequtils],
   stew/shims/net as stewNet,
@@ -15,11 +15,11 @@ import
   libp2p/crypto/crypto
 
 export
-  networks_config, app, logging, options, strutils, os, sequtils, stewNet, chronicles,
+  networks_config, waku, logging, options, strutils, os, sequtils, stewNet, chronicles,
   chronos, metrics, libbacktrace, crypto
 
-proc setup*(): App =
-  const versionString = "version / git commit hash: " & app.git_version
+proc setup*(): Waku =
+  const versionString = "version / git commit hash: " & waku.git_version
   let rng = crypto.newRng()
 
   let confRes = WakuNodeConf.load(version = versionString)
@@ -48,48 +48,17 @@ proc setup*(): App =
   conf.rlnEpochSizeSec = twnClusterConf.rlnEpochSizeSec
   conf.rlnRelayUserMessageLimit = twnClusterConf.rlnRelayUserMessageLimit
 
-  var wakunode2 = App.init(rng, conf)
-  ## Peer persistence
-  let res1 = wakunode2.setupPeerPersistence()
-  if res1.isErr():
-    error "1/5 Setting up storage failed", error = $res1.error
+  debug "Starting node"
+  var waku = Waku.init(conf).valueOr:
+    error "Waku initialization failed", error = error
     quit(QuitFailure)
 
-  debug "2/5 Retrieve dynamic bootstrap nodes"
-
-  let res3 = wakunode2.setupDyamicBootstrapNodes()
-  if res3.isErr():
-    error "2/5 Retrieving dynamic bootstrap nodes failed", error = $res3.error
+  (waitFor startWaku(addr waku)).isOkOr:
+    error "Starting waku failed", error = error
     quit(QuitFailure)
-
-  debug "3/5 Initializing node"
-
-  let res4 = wakunode2.setupWakuApp()
-  if res4.isErr():
-    error "3/5 Initializing node failed", error = $res4.error
-    quit(QuitFailure)
-
-  debug "4/5 Mounting protocols"
-
-  var res5: Result[void, string]
-  try:
-    res5 = waitFor wakunode2.setupAndMountProtocols()
-    if res5.isErr():
-      error "4/5 Mounting protocols failed", error = $res5.error
-      quit(QuitFailure)
-  except Exception:
-    error "4/5 Mounting protocols failed", error = getCurrentExceptionMsg()
-    quit(QuitFailure)
-
-  debug "5/5 Starting node and mounted protocols"
 
   # set triggerSelf to false, we don't want to process our own stealthCommitments
-  wakunode2.node.wakuRelay.triggerSelf = false
-
-  let res6 = wakunode2.startApp()
-  if res6.isErr():
-    error "5/5 Starting node and protocols failed", error = $res6.error
-    quit(QuitFailure)
+  waku.node.wakuRelay.triggerSelf = false
 
   info "Node setup complete"
-  return wakunode2
+  return waku
