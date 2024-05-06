a89c8820f922b4740d3f717ddec4525918e0bc87
Simon-Pierre Vivier <simvivier@status.im>
2024-05-06 11:14:48 -0400

    feat: pruning storage mehcanism (#2673)

--- waku/waku_sync/common.nim
+++ waku/waku_sync/common.nim
@@ -6,20 +6,29 @@ else:
 import std/[options], chronos
 import ../waku_core
 
-const DefaultSyncInterval*: timer.Duration = Hour
-const WakuSyncCodec* = "/vac/waku/sync/1.0.0"
-const DefaultFrameSize* = 153600
+const
+  DefaultSyncInterval*: Duration = 1.hours
+  DefaultPruneInterval*: Duration = 30.minutes
+  WakuSyncCodec* = "/vac/waku/sync/1.0.0"
+  DefaultFrameSize* = 153600
+  DefaultGossipSubJitter*: Duration = 20.seconds
 
-type WakuSyncCallback* = proc(hashes: seq[WakuMessageHash], syncPeer: RemotePeerInfo) {.
-  async: (raises: []), closure
-.}
+type
+  SyncCallback* =
+    proc(hashes: seq[WakuMessageHash], syncPeer: RemotePeerInfo) {.async: (raises: []).}
 
-type SyncPayload* = object
-  rangeStart*: Option[uint64]
-  rangeEnd*: Option[uint64]
+  PruneCallback* = proc(
+    startTime: Timestamp, endTime: Timestamp, cursor = none(WakuMessageHash)
+  ): Future[
+    Result[(seq[(WakuMessageHash, Timestamp)], Option[WakuMessageHash]), string]
+  ] {.async: (raises: []).}
 
-  frameSize*: Option[uint64]
+  SyncPayload* = object
+    rangeStart*: Option[uint64]
+    rangeEnd*: Option[uint64]
 
-  negentropy*: seq[byte] # negentropy protocol payload
+    frameSize*: Option[uint64]
 
-  hashes*: seq[WakuMessageHash]
+    negentropy*: seq[byte] # negentropy protocol payload
+
+    hashes*: seq[WakuMessageHash]
