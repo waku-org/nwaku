c2e51deae9dbf990d64b7d5ca5e0483ffcb05867
shash256 <111925100+shash256@users.noreply.github.com>
2024-05-06 14:54:22 +0400

    test updates

--- tests/waku_lightpush/test_validation.nim
+++ tests/waku_lightpush/test_validation.nim
@@ -1,24 +1,30 @@
+{.used.}
+
 import
+  ../../../waku/waku_core,
   ../../../waku/waku_lightpush/protocol,
+  testutils/unittests,
+  ../resources/payloads,
+  std/strformat,
   chronos,
+  sequtils,
   stew/results
 
 suite "WakuLightPush Protocol Tests":
+  asyncTest "Validate message size exceeds limit":
+    let
+      wl = WakuLightPush() # Assuming a constructor or initialization method exists
+      pubsubTopic = "testTopic"
+      # Create a message that exceeds the default maximum size
+      #payload = newSeqWith(DefaultMaxWakuMessageSize + 1, 'a')
+      payload = getByteSequence(DefaultMaxWakuMessageSize) # Payload that is too large
+      msg = WakuMessage(payload: payload)
 
-  test "Validate message size exceeds limit":
-    asyncCheck:
-      let
-        wl = WakuLightPush() # Assuming a constructor or initialization method exists
-        pubsubTopic = "testTopic"
-        # Create a message that exceeds the default maximum size
-        payload = newSeqWith(DefaultMaxWakuMessageSize + 1, 'a') # Payload that is too large
-        msg = WakuMessage(payload: payload.toBytes())
-
-      # Call the validateMessage function and check the result
-      let result = await wl.validateMessage(pubsubTopic, msg)
-      check:
-        result.isErr()
-        result.error == fmt"Message size exceeded maximum of {DefaultMaxWakuMessageSize} bytes"
+    # Call the validateMessage function and check the result
+    let result = await wl.validateMessage(pubsubTopic, msg)
+    check:
+      result.isErr()
+      result.error == fmt"Message size exceeded maximum of {DefaultMaxWakuMessageSize} bytes"
 
 
 # import ./test_client, ./test_ratelimit
--- waku/waku_lightpush/protocol.nim
+++ waku/waku_lightpush/protocol.nim
@@ -4,7 +4,7 @@ else:
   {.push raises: [].}
 
 import
-  std/options, stew/results, stew/byteutils, chronicles, chronos, metrics, bearssl/rand
+  std/options, stew/results, stew/byteutils, chronicles, chronos, metrics, bearssl/rand, std/strformat
 import
   ../node/peer_manager/peer_manager,
   ../waku_core,
@@ -13,10 +13,10 @@ import
   ./rpc_codec,
   ./protocol_metrics,
   ../common/ratelimit,
-  # ../common/waku_service_metrics,
-  # ../waku_rln_relay/rln_relay,
-  # ../waku_rln_relay/protocol_types,
-  # ../common/error_handling
+  ../common/waku_service_metrics
+# ../waku_rln_relay/rln_relay,
+# ../waku_rln_relay/protocol_types,
+# ../common/error_handling
 
 export ratelimit
 
@@ -35,7 +35,7 @@ proc validateMessage*(
   let messageSizeBytes = msg.encode().buffer.len
   let msgHash = computeMessageHash(pubsubTopic, msg).to0xHex()
   let maxMessageSize = int(DefaultMaxWakuMessageSize)
-  
+
   if messageSizeBytes > maxMessageSize:
     let message = fmt"Message size exceeded maximum of {maxMessageSize} bytes"
     error "too large Waku message",
@@ -94,93 +94,93 @@ proc validateMessage*(
 #     wl: WakuLightPush, msg: WakuMessage, timeOption = none(float64)
 # ): MessageValidationResult =
 
-  # let wakuRlnRelay = (await WakuRlnRelay.new(wakuRlnConfig)).valueOr:
-  #   raiseAssert $error
-  # let validationRes = WakuRlnRelay.validateMessage(msg, timeOption)
-  # return validationRes
+# let wakuRlnRelay = (await WakuRlnRelay.new(wakuRlnConfig)).valueOr:
+#   raiseAssert $error
+# let validationRes = WakuRlnRelay.validateMessage(msg, timeOption)
+# return validationRes
 
-  ## validate the supplied `msg` based on the waku-rln-relay routing protocol i.e.,
-  ## the `msg`'s epoch is within MaxEpochGap of the current epoch
-  ## the `msg` has valid rate limit proof
-  ## the `msg` does not violate the rate limit
-  ## `timeOption` indicates Unix epoch time (fractional part holds sub-seconds)
-  ## if `timeOption` is supplied, then the current epoch is calculated based on that
+## validate the supplied `msg` based on the waku-rln-relay routing protocol i.e.,
+## the `msg`'s epoch is within MaxEpochGap of the current epoch
+## the `msg` has valid rate limit proof
+## the `msg` does not violate the rate limit
+## `timeOption` indicates Unix epoch time (fractional part holds sub-seconds)
+## if `timeOption` is supplied, then the current epoch is calculated based on that
 
-  # let decodeRes = RateLimitProof.init(msg.proof)
-  # if decodeRes.isErr():
-  #   return MessageValidationResult.Invalid
+# let decodeRes = RateLimitProof.init(msg.proof)
+# if decodeRes.isErr():
+#   return MessageValidationResult.Invalid
 
-  # let proof = decodeRes.get()
+# let proof = decodeRes.get()
 
-  # # checks if the `msg`'s epoch is far from the current epoch
-  # # it corresponds to the validation of rln external nullifier
-  # var epoch: Epoch
-  # if timeOption.isSome():
-  #   epoch = wl.calcEpoch(timeOption.get())
-  # else:
-  #   # get current rln epoch
-  #   epoch = wl.getCurrentEpoch()
+# # checks if the `msg`'s epoch is far from the current epoch
+# # it corresponds to the validation of rln external nullifier
+# var epoch: Epoch
+# if timeOption.isSome():
+#   epoch = wl.calcEpoch(timeOption.get())
+# else:
+#   # get current rln epoch
+#   epoch = wl.getCurrentEpoch()
 
-  # let
-  #   msgEpoch = proof.epoch
-  #   # calculate the gaps
-  #   gap = absDiff(epoch, msgEpoch)
+# let
+#   msgEpoch = proof.epoch
+#   # calculate the gaps
+#   gap = absDiff(epoch, msgEpoch)
 
-  # trace "epoch info", currentEpoch = fromEpoch(epoch), msgEpoch = fromEpoch(msgEpoch)
+# trace "epoch info", currentEpoch = fromEpoch(epoch), msgEpoch = fromEpoch(msgEpoch)
 
-  # # validate the epoch
-  # if gap > wl.rlnMaxEpochGap:
-  #   # message's epoch is too old or too ahead
-  #   # accept messages whose epoch is within +-MaxEpochGap from the current epoch
-  #   warn "invalid message: epoch gap exceeds a threshold",
-  #     gap = gap, payloadLen = msg.payload.len, msgEpoch = fromEpoch(proof.epoch)
-  #   waku_rln_invalid_messages_total.inc(labelValues = ["invalid_epoch"])
-  #   return MessageValidationResult.Invalid
+# # validate the epoch
+# if gap > wl.rlnMaxEpochGap:
+#   # message's epoch is too old or too ahead
+#   # accept messages whose epoch is within +-MaxEpochGap from the current epoch
+#   warn "invalid message: epoch gap exceeds a threshold",
+#     gap = gap, payloadLen = msg.payload.len, msgEpoch = fromEpoch(proof.epoch)
+#   waku_rln_invalid_messages_total.inc(labelValues = ["invalid_epoch"])
+#   return MessageValidationResult.Invalid
 
-  # let rootValidationRes = rlnPeer.groupManager.validateRoot(proof.merkleRoot)
-  # if not rootValidationRes:
-  #   warn "invalid message: provided root does not belong to acceptable window of roots",
-  #     provided = proof.merkleRoot.inHex(),
-  #     validRoots = rlnPeer.groupManager.validRoots.mapIt(it.inHex())
-  #   waku_rln_invalid_messages_total.inc(labelValues = ["invalid_root"])
-  #   return MessageValidationResult.Invalid
+# let rootValidationRes = rlnPeer.groupManager.validateRoot(proof.merkleRoot)
+# if not rootValidationRes:
+#   warn "invalid message: provided root does not belong to acceptable window of roots",
+#     provided = proof.merkleRoot.inHex(),
+#     validRoots = rlnPeer.groupManager.validRoots.mapIt(it.inHex())
+#   waku_rln_invalid_messages_total.inc(labelValues = ["invalid_root"])
+#   return MessageValidationResult.Invalid
 
-  # # verify the proof
-  # let
-  #   contentTopicBytes = msg.contentTopic.toBytes
-  #   input = concat(msg.payload, contentTopicBytes)
+# # verify the proof
+# let
+#   contentTopicBytes = msg.contentTopic.toBytes
+#   input = concat(msg.payload, contentTopicBytes)
 
-  # waku_rln_proof_verification_total.inc()
-  # waku_rln_proof_verification_duration_seconds.nanosecondTime:
-  #   let proofVerificationRes = rlnPeer.groupManager.verifyProof(input, proof)
+# waku_rln_proof_verification_total.inc()
+# waku_rln_proof_verification_duration_seconds.nanosecondTime:
+#   let proofVerificationRes = rlnPeer.groupManager.verifyProof(input, proof)
 
-  # if proofVerificationRes.isErr():
-  #   waku_rln_errors_total.inc(labelValues = ["proof_verification"])
-  #   warn "invalid message: proof verification failed", payloadLen = msg.payload.len
-  #   return MessageValidationResult.Invalid
-  # if not proofVerificationRes.value():
-  #   # invalid proof
-  #   warn "invalid message: invalid proof", payloadLen = msg.payload.len
-  #   waku_rln_invalid_messages_total.inc(labelValues = ["invalid_proof"])
-  #   return MessageValidationResult.Invalid
+# if proofVerificationRes.isErr():
+#   waku_rln_errors_total.inc(labelValues = ["proof_verification"])
+#   warn "invalid message: proof verification failed", payloadLen = msg.payload.len
+#   return MessageValidationResult.Invalid
+# if not proofVerificationRes.value():
+#   # invalid proof
+#   warn "invalid message: invalid proof", payloadLen = msg.payload.len
+#   waku_rln_invalid_messages_total.inc(labelValues = ["invalid_proof"])
+#   return MessageValidationResult.Invalid
 
-  # check if double messaging has happened
-  # let proofMetadataRes = proof.extractMetadata()
-  # if proofMetadataRes.isErr():
-  #   waku_rln_errors_total.inc(labelValues = ["proof_metadata_extraction"])
-  #   return MessageValidationResult.Invalid
-  # let hasDup = wl.hasDuplicate(msgEpoch, proofMetadataRes.get())
-  # if hasDup.isErr():
-  #   waku_rln_errors_total.inc(labelValues = ["duplicate_check"])
-  # elif hasDup.value == true:
-  #   trace "invalid message: message is spam", payloadLen = msg.payload.len
-  #   waku_rln_spam_messages_total.inc()
-  #   return MessageValidationResult.Spam
+# check if double messaging has happened
+# let proofMetadataRes = proof.extractMetadata()
+# if proofMetadataRes.isErr():
+#   waku_rln_errors_total.inc(labelValues = ["proof_metadata_extraction"])
+#   return MessageValidationResult.Invalid
+# let hasDup = wl.hasDuplicate(msgEpoch, proofMetadataRes.get())
+# if hasDup.isErr():
+#   waku_rln_errors_total.inc(labelValues = ["duplicate_check"])
+# elif hasDup.value == true:
+#   trace "invalid message: message is spam", payloadLen = msg.payload.len
+#   waku_rln_spam_messages_total.inc()
+#   return MessageValidationResult.Spam
 
-  # trace "message is valid", payloadLen = msg.payload.len
-  # let rootIndex = rlnPeer.groupManager.indexOfRoot(proof.merkleRoot)
-  # waku_rln_valid_messages_total.observe(rootIndex.toFloat())
-  # return MessageValidationResult.Valid
+# trace "message is valid", payloadLen = msg.payload.len
+# let rootIndex = rlnPeer.groupManager.indexOfRoot(proof.merkleRoot)
+# waku_rln_valid_messages_total.observe(rootIndex.toFloat())
+# return MessageValidationResult.Valid
 
 # proc validateMessageAndUpdateLog*(
 #     rlnPeer: WakuRLNRelay, msg: WakuMessage, timeOption = none(float64)
@@ -226,16 +226,19 @@ proc handleRequest*(
       request = pushRpcRequest.request
       pubSubTopic = request.get().pubSubTopic
       message = request.get().message
-    let validateMessageRes = wl.validateMessage(pubSubTopic,message)
+    let validateMessageRes = await wl.validateMessage(pubSubTopic, message)
 
     # validate msg before relay
     if validateMessageRes.isErr():
-      pushResponseInfo = messageValidationFailure & ": " & $validateMessageRes.error
+      pushResponseInfo = messageValidationFailure & ": " &
+          $validateMessageRes.error
+      waku_service_requests_rejected.inc(labelValues = ["Lightpush"])
     elif wl.requestRateLimiter.isSome() and not wl.requestRateLimiter.get().tryConsume(1):
       isRejectedDueRateLimit = true
       debug "lightpush request rejected due rate limit exceeded",
         peerId = peerId, requestId = requestId
       pushResponseInfo = TooManyRequestsMessage
+      waku_service_requests_rejected.inc(labelValues = ["Lightpush"])
     else:
       waku_service_requests.inc(labelValues = ["Lightpush"])
 
