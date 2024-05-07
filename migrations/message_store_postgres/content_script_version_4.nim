365fda11b9ad22689ae887606ea307f24ef4668b
Ivan Folgueira Bande <ivansete@status.im>
2024-05-07 10:44:28 +0200

    content_script_version_4: ensure meta attribute is null by default

--- migrations/message_store_postgres/content_script_version_4.nim
+++ migrations/message_store_postgres/content_script_version_4.nim
@@ -1,6 +1,6 @@
 const ContentScriptVersion_4* =
   """
-ALTER TABLE messages ADD meta VARCHAR;
+ALTER TABLE messages ADD meta VARCHAR default null;
 
 CREATE INDEX IF NOT EXISTS i_query ON messages (contentTopic, pubsubTopic, storedAt, id);
 
