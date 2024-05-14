e0ff07c4b6cda7bc75d523782ff6ae713ae9aba1
Ivan Folgueira Bande <ivansete@status.im>
2024-05-14 09:06:12 +0200

    all_tests_waku: comment part of the tests

--- tests/all_tests_waku.nim
+++ tests/all_tests_waku.nim
@@ -1,30 +1,30 @@
 ## Waku v2
 
 # Waku core test suite
-import
-  ./waku_core/test_namespaced_topics,
-  ./waku_core/test_time,
-  ./waku_core/test_message_digest,
-  ./waku_core/test_peers,
-  ./waku_core/test_published_address
+# import
+#   ./waku_core/test_namespaced_topics,
+#   ./waku_core/test_time,
+#   ./waku_core/test_message_digest,
+#   ./waku_core/test_peers,
+#   ./waku_core/test_published_address
 
-# Waku archive test suite
-import
-  ./waku_archive/test_driver_queue_index,
-  ./waku_archive/test_driver_queue_pagination,
-  ./waku_archive/test_driver_queue_query,
-  ./waku_archive/test_driver_queue,
-  ./waku_archive/test_driver_sqlite_query,
-  ./waku_archive/test_driver_sqlite,
-  ./waku_archive/test_retention_policy,
-  ./waku_archive/test_waku_archive
+# # Waku archive test suite
+# import
+#   ./waku_archive/test_driver_queue_index,
+#   ./waku_archive/test_driver_queue_pagination,
+#   ./waku_archive/test_driver_queue_query,
+#   ./waku_archive/test_driver_queue,
+#   ./waku_archive/test_driver_sqlite_query,
+#   ./waku_archive/test_driver_sqlite,
+#   ./waku_archive/test_retention_policy,
+#   ./waku_archive/test_waku_archive
 
-const os* {.strdefine.} = ""
-when os == "Linux" and
-    # GitHub only supports container actions on Linux
-    # and we need to start a postgress database in a docker container
-    defined(postgres):
-  import ./waku_archive/test_driver_postgres_query, ./waku_archive/test_driver_postgres
+# const os* {.strdefine.} = ""
+# when os == "Linux" and
+#     # GitHub only supports container actions on Linux
+#     # and we need to start a postgress database in a docker container
+#     defined(postgres):
+#   import ./waku_archive/test_driver_postgres_query, ./waku_archive/test_driver_postgres
 
 # Waku store test suite
 import
