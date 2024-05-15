eff35adb0488211261ed07bd9a28095a6c8eead1
shash256 <111925100+shash256@users.noreply.github.com>
2024-05-15 13:28:37 +0400

    address review comments

--- waku/node/waku_node.nim
+++ waku/node/waku_node.nim
@@ -920,17 +920,17 @@ proc mountLightPush*(
       let validationRes = await node.wakuRelay.validateMessage(pubSubTopic, message)
       if validationRes.isErr():
         return err(validationRes.error)
-      else:
-        let publishedCount =
-          await node.wakuRelay.publish(pubsubTopic, message.encode().buffer)
 
-        if publishedCount == 0:
-          ## Agreed change expected to the lightpush protocol to better handle such case. https://github.com/waku-org/pm/issues/93
-          let msgHash = computeMessageHash(pubsubTopic, message).to0xHex()
-          debug "Lightpush request has not been published to any peers",
-            msg_hash = msgHash
+      let publishedCount =
+        await node.wakuRelay.publish(pubsubTopic, message.encode().buffer)
 
-        return ok()
+      if publishedCount == 0:
+        ## Agreed change expected to the lightpush protocol to better handle such case. https://github.com/waku-org/pm/issues/93
+        let msgHash = computeMessageHash(pubsubTopic, message).to0xHex()
+        debug "Lightpush request has not been published to any peers",
+          msg_hash = msgHash
+
+      return ok()
 
   debug "mounting lightpush with relay"
   node.wakuLightPush =
