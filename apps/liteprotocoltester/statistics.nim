08c600afd78d9d689f010e3ea59098230feff052
Gabriel mermelstein <gabrielmermelstein@gmail.com>
2024-05-31 13:30:26 +0200

    fixing index

--- apps/liteprotocoltester/statistics.nim
+++ apps/liteprotocoltester/statistics.nim
@@ -125,7 +125,7 @@ proc echoStat*(self: Statistics) =
   let printable = catch:
     """*-----------------------------------------------------------------------------*
 |  Expected  |  Received  |    Loss    |  Misorder  |    Late    |  Duplicate |
-|{self.helper.prevIndex+1:>11} |{self.receivedMessages:>11} |{self.lossCount():>11} |{self.misorderCount:>11} |{self.lateCount:>11} |{self.duplicateCount:>11} |
+|{self.helper.prevIndex:>11} |{self.receivedMessages:>11} |{self.lossCount():>11} |{self.misorderCount:>11} |{self.lateCount:>11} |{self.duplicateCount:>11} |
 *-----------------------------------------------------------------------------*
 | Latency stat:                                                               |
 |    avg latency: {$self.averageLatency():<60}|
@@ -140,7 +140,7 @@ proc echoStat*(self: Statistics) =
 
 proc jsonStat*(self: Statistics): string =
   let json = catch:
-    """{{"expected":{self.helper.prevIndex+1},
+    """{{"expected":{self.helper.prevIndex},
          "received": {self.receivedMessages},
          "loss": {self.lossCount()},
          "misorder": {self.misorderCount},
