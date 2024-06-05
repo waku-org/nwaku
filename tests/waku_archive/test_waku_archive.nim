abadc0ad9de84f91714ff0483fdd18327c74e7db
Simon-Pierre Vivier <simvivier@status.im>
2024-06-05 07:50:01 -0400

    chore(archive): tests refactor (#2756)
    renaming to pubsub

--- tests/waku_archive/test_waku_archive.nim
+++ tests/waku_archive/test_waku_archive.nim
@@ -1,21 +1,13 @@
 {.used.}
 
-import
-  std/[options, sequtils],
-  testutils/unittests,
-  chronicles,
-  chronos,
-  libp2p/crypto/crypto
+import std/[options, sequtils], testutils/unittests, chronos, libp2p/crypto/crypto
 
 import
-  ../../../waku/common/databases/db_sqlite,
   ../../../waku/common/paging,
   ../../../waku/waku_core,
   ../../../waku/waku_core/message/digest,
-  ../../../waku/waku_archive/driver/sqlite_driver,
   ../../../waku/waku_archive,
   ../waku_archive/archive_utils,
-  ../testlib/common,
   ../testlib/wakucore
 
 suite "Waku Archive - message handling":
@@ -58,7 +50,7 @@ suite "Waku Archive - message handling":
     check:
       (waitFor driver.getMessagesCount()).tryGet() == 2
 
-  test "it should archive a message with no sender timestamp":
+  test "it should not archive a message with no sender timestamp":
     ## Setup
     let driver = newSqliteArchiveDriver()
     let archive = newWakuArchive(driver)
@@ -72,7 +64,7 @@ suite "Waku Archive - message handling":
 
     ## Then
     check:
-      (waitFor driver.getMessagesCount()).tryGet() == 1
+      (waitFor driver.getMessagesCount()).tryGet() == 0
 
   test "it should not archive a message with a sender time variance greater than max time variance (future)":
     ## Setup
@@ -158,11 +150,7 @@ procSuite "Waku Archive - find messages":
     for msg in msgListA:
       require (
         waitFor driver.put(
-          DefaultPubsubTopic,
-          msg,
-          computeDigest(msg),
-          computeMessageHash(DefaultPubsubTopic, msg),
-          msg.timestamp,
+          computeMessageHash(DefaultPubsubTopic, msg), DefaultPubsubTopic, msg
         )
       ).isOk()
 
@@ -248,13 +236,11 @@ procSuite "Waku Archive - find messages":
     let queryRes = waitFor archive.findMessages(req)
 
     ## Then
-    check:
-      queryRes.isErr()
+    assert queryRes.isOk(), $queryRes.error
 
-    let error = queryRes.tryError()
+    let response = queryRes.tryGet()
     check:
-      error.kind == ArchiveErrorKind.INVALID_QUERY
-      error.cause == "too many content topics"
+      response.messages.len() == 0
 
   test "handle query with pubsub topic filter":
     ## Setup
@@ -392,8 +378,8 @@ procSuite "Waku Archive - find messages":
 
     ## Then
     check:
-      cursors[0] == some(computeArchiveCursor(DefaultPubsubTopic, msgListA[3]))
-      cursors[1] == some(computeArchiveCursor(DefaultPubsubTopic, msgListA[7]))
+      cursors[0] == some(computeMessageHash(DefaultPubsubTopic, msgListA[3]))
+      cursors[1] == some(computeMessageHash(DefaultPubsubTopic, msgListA[7]))
       cursors[2] == none(ArchiveCursor)
 
     check:
@@ -426,8 +412,8 @@ procSuite "Waku Archive - find messages":
 
     ## Then
     check:
-      cursors[0] == some(computeArchiveCursor(DefaultPubsubTopic, msgListA[6]))
-      cursors[1] == some(computeArchiveCursor(DefaultPubsubTopic, msgListA[2]))
+      cursors[0] == some(computeMessageHash(DefaultPubsubTopic, msgListA[6]))
+      cursors[1] == some(computeMessageHash(DefaultPubsubTopic, msgListA[2]))
       cursors[2] == none(ArchiveCursor)
 
     check:
@@ -458,11 +444,7 @@ procSuite "Waku Archive - find messages":
     for msg in msgList:
       require (
         waitFor driver.put(
-          DefaultPubsubTopic,
-          msg,
-          computeDigest(msg),
-          computeMessageHash(DefaultPubsubTopic, msg),
-          msg.timestamp,
+          computeMessageHash(DefaultPubsubTopic, msg), DefaultPubsubTopic, msg
         )
       ).isOk()
 
