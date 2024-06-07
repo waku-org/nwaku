3fbcc601014f14cb16f1ad7062e4ebe1ec7359fd
Gabriel mermelstein <gabrielmermelstein@gmail.com>
2024-06-07 13:02:43 +0200

    comment for test purposes

--- waku/waku_node.nim
+++ waku/waku_node.nim
@@ -2,6 +2,6 @@ import
   ./node/config,
   ./node/waku_switch as switch,
   ./node/waku_node as node,
-  ./node/health_monitor as health_monitor
+  ./node/health_monitor as health_monitor #
 
 export config, switch, node, health_monitor
