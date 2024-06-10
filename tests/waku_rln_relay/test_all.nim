0ab337de7bac45ad96d0bdcec3bc40c2dadf5c88
Gabriel mermelstein <gabrielmermelstein@gmail.com>
2024-06-10 12:38:56 +0200

    only running on chain group manager tests

--- tests/waku_rln_relay/test_all.nim
+++ tests/waku_rln_relay/test_all.nim
@@ -1,11 +1,6 @@
 {.used.}
 
-import
-  ./test_rln_group_manager_onchain,
-  ./test_rln_group_manager_static,
-  ./test_waku_rln_relay,
-  ./test_wakunode_rln_relay,
-  ./test_rln_nonce_manager
+import ./test_rln_group_manager_onchain
 
 when defined(rln_v2):
   import ./rln_v2/test_rln_relay_v2_serde
