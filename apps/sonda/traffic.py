a330aa09379962329ea692d1b291e7a9254afd3b
Gabriel mermelstein <gabrielmermelstein@gmail.com>
2024-07-09 13:30:04 +0300

    adding message querying

--- apps/sonda/traffic.py
+++ apps/sonda/traffic.py
@@ -8,20 +8,20 @@ import urllib.parse
 import requests
 import argparse
 
-def send_sonda_msg(node_address, pubsub_topic, content_topic):
+def send_sonda_msg(rest_address, pubsub_topic, content_topic, timestamp):
     
     message = "Hi, I'm Sonda"
     base64_message = base64.b64encode(message.encode('utf-8')).decode('ascii')
     body = {
-        "payload": base64_message,
-        "contentTopic": content_topic,
-        "version": 1,  # You can adjust the version as needed
-        #"timestamp": int(time.time())
+        'payload': base64_message,
+        'contentTopic': content_topic,
+        'version': 1,  # You can adjust the version as needed
+        'timestamp': timestamp
     }
 
     encoded_pubsub_topic = urllib.parse.quote(pubsub_topic, safe='')
 
-    url = f"{node_address}/relay/v1/messages/{encoded_pubsub_topic}"
+    url = f'{rest_address}/relay/v1/messages/{encoded_pubsub_topic}'
     headers = {'content-type': 'application/json'}
 
     print('Waku REST API: %s PubSubTopic: %s, ContentTopic: %s' % (url, pubsub_topic, content_topic))
@@ -30,21 +30,59 @@ def send_sonda_msg(node_address, pubsub_topic, content_topic):
     response = None
 
     try:
-      print("Sending request")
+      print('Sending request')
       response = requests.post(url, json=body, headers=headers)
     except Exception as e:
-      print(f"Error sending request: {e}")
+      print(f'Error sending request: {e}')
 
     if(response != None):
       elapsed_ms = (time.time() - s_time) * 1000
-      print('Response from %s: status:%s content:%s [%.4f ms.]' % (node_address, \
+      print('Response from %s: status:%s content:%s [%.4f ms.]' % (rest_address, \
         response.status_code, response.text, elapsed_ms))
+    
+      if(response.status_code == 200):
+        return True
+    
+    return False
 
 parser = argparse.ArgumentParser(description='')
 
+def send_store_query(rest_address, store_node, encoded_pubsub_topic, encoded_content_topic, timestamp):
+    url = f'{rest_address}/store/v3/messages'
+    params = {'peerAddr': urllib.parse.quote(store_node, safe=''), 'pubsubTopic': encoded_pubsub_topic, \
+      'contentTopics': encoded_content_topic, 'includeData': 'true'}
+    
+    s_time = time.time()
+    response = None
+    
+    try:
+      print(f'Sending store request to {store_node}')
+      response = requests.get(url, params=params)
+    except Exception as e:
+      print(f'Error sending request: {e}')
+    
+    if(response != None):
+      elapsed_ms = (time.time() - s_time) * 1000
+      print('Response from %s: status:%s content:%s [%.4f ms.]' % (rest_address, \
+        response.status_code, response.text, elapsed_ms))
+      
+      if(response.status_code == 200):
+        return True
+    
+    return False
+    
 
 
-parser.add_argument('-p', '--pubsub-topic', type=str, help='pubsub topic', default="/waku/2/rs/1/0")
+def send_store_queries(rest_address, store_nodes, pubsub_topic, content_topic, timestamp):
+    print(f'Sending store queries. nodes = {store_nodes}')
+    encoded_pubsub_topic = urllib.parse.quote(pubsub_topic, safe='')
+    encoded_content_topic = urllib.parse.quote(content_topic, safe='')
+    
+    for node in store_nodes:
+      send_store_query(rest_address, node, encoded_pubsub_topic, encoded_content_topic, timestamp)
+
+
+parser.add_argument('-p', '--pubsub-topic', type=str, help='pubsub topic', default='/waku/2/rs/1/0')
 parser.add_argument('-s', '--msg-size-kbytes', type=int, help='message size in kBytes', default=10)
 parser.add_argument('-d', '--delay-seconds', type=int, help='delay in second between messages', default=60)
 parser.add_argument('-n', '--store-nodes', type=str, help='comma separated list of store nodes to query', required=True)
@@ -52,16 +90,24 @@ args = parser.parse_args()
 
 print(args)
 
-nodes = []
+store_nodes = []
 if args.store_nodes is not None:
-    nodes = [s.strip() for s in args.store_nodes.split(",")]
-print(nodes)
+    store_nodes = [s.strip() for s in args.store_nodes.split(",")]
+print(store_nodes)
 
+sonda_content_topic = '/sonda/2/polls/proto'
+node_rest_address = 'http://nwaku:8645'
 while True:
     # calls are blocking
     # limited by the time it takes the REST API to reply
 
-    send_sonda_msg('http://nwaku:8645', args.pubsub_topic, "random_content_topic")
+    timestamp = int(time.time())
+    
+    res = send_sonda_msg(node_rest_address, args.pubsub_topic, sonda_content_topic, timestamp)
 
-    print("sleeping: ", args.delay_seconds, " seconds")
+    print(f'sleeping: {args.delay_seconds} seconds')
     time.sleep(args.delay_seconds)
+
+    # Only send store query if message was successfully published
+    if(res):
+      send_store_queries(node_rest_address, store_nodes, args.pubsub_topic, sonda_content_topic, timestamp)
