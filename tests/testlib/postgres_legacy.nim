e6161f17111db8374f0e05542154d419a16803a7
SionoiS <simvivier@status.im>
2024-07-09 11:24:30 -0400

    one more fix

--- tests/testlib/postgres_legacy.nim
+++ tests/testlib/postgres_legacy.nim
@@ -1,9 +1,9 @@
 import chronicles, chronos
 import
-  ../../../waku/waku_archive_legacy,
-  ../../../waku/waku_archive_legacy/driver as driver_module,
-  ../../../waku/waku_archive_legacy/driver/builder,
-  ../../../waku/waku_archive_legacy/driver/postgres_driver
+  waku/waku_archive_legacy,
+  waku/waku_archive_legacy/driver as driver_module,
+  waku/waku_archive_legacy/driver/builder,
+  waku/waku_archive_legacy/driver/postgres_driver
 
 const storeMessageDbUrl = "postgres://postgres:test123@localhost:5432/postgres"
 
