7260c71b97f87600cb105bfcf1cd8e9795d86248
Ivan Folgueira Bande <ivansete@status.im>
2024-07-11 16:54:06 +0200

    migration script 6: we still need the id column
    The id column is needed because it contains the message digest
    which is used in store v2, and we need to keep support to
    store v2 for a while

--- migrations/message_store_postgres/content_script_version_6.nim
+++ migrations/message_store_postgres/content_script_version_6.nim
@@ -3,9 +3,6 @@ const ContentScriptVersion_6* =
 -- we can drop the timestamp column because this data is also kept in the storedAt column
 ALTER TABLE messages DROP COLUMN timestamp;
 
--- drop unused column
-ALTER TABLE messages DROP COLUMN id;
-
 -- from now on we are only interested in the message timestamp
 ALTER TABLE messages RENAME COLUMN storedAt TO timestamp;
 
