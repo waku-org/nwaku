23c89290ff4ebfba515dd8a30132e13f9e931297
Ivan Folgueira Bande <ivansete@status.im>
2024-07-24 16:28:14 +0200

    upgrade postgres schema to add messages_lookup table

---
+++ migrations/message_store_postgres/content_script_version_7.nim
@@ -0,0 +1,29 @@
+const ContentScriptVersion_7* =
+  """
+
+-- Create lookup table
+CREATE TABLE IF NOT EXISTS messages_lookup (
+   timestamp BIGINT NOT NULL,
+   messageHash VARCHAR NOT NULL,
+   CONSTRAINT messageIndexLookupTable PRIMARY KEY (messageHash, timestamp)
+  );
+
+-- Put data into lookup table
+INSERT INTO messages_lookup (messageHash, timestamp) SELECT messageHash, timestamp from messages;
+
+-- Create indexes
+CREATE INDEX IF NOT EXISTS idx_messages_messagehash ON messages (messagehash);
+CREATE INDEX IF NOT EXISTS idx_messages_timestamp ON messages (timestamp);
+CREATE INDEX IF NOT EXISTS idx_messages_lookup_messagehash ON messages_lookup (messagehash);
+CREATE INDEX IF NOT EXISTS idx_messages_lookup_timestamp ON messages_lookup (timestamp);
+
+DROP INDEX IF EXISTS i_query_storedat;
+DROP INDEX IF EXISTS i_query;
+
+CREATE INDEX IF NOT EXISTS idx_query_pubsubtopic ON messages (pubsubTopic);
+CREATE INDEX IF NOT EXISTS idx_query_contenttopic ON messages (contentTopic);
+
+-- Update to new version
+UPDATE version SET version = 7 WHERE version = 6;
+
+"""
