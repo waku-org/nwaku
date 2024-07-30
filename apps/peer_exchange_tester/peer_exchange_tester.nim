12a61a022a7cb3e01e716b5e68dff010890c4d0d
DarshanBPatel <darshan@status.im>
2024-07-30 16:15:14 +0530

    test: connect remote peers

--- apps/peer_exchange_tester/peer_exchange_tester.nim
+++ apps/peer_exchange_tester/peer_exchange_tester.nim
@@ -1,15 +1,24 @@
-import
-  std/[options, sequtils, random],
-  chronos, chronicles, stew/byteutils
-  
-import 
-  waku/[waku_peer_exchange, node/peer_manager]
+# import random, strutils, asyncdispatch, chronos, chronicles, stew/byteutils, waku/[waku_peer_exchange, node/peer_manager], tests/testlib/wakucore
+import random, chronos, chronicles, stew/byteutils
+import waku/[waku_peer_exchange, node/peer_manager]
+import tests/testlib/wakucore
+import asyncdispatch as asyncd
 
-# import chronicles, chronos, stew/byteutils, results
-# import waku/[common/logging, node/peer_manager, waku_core, waku_filter_v2/client]
+proc createSwitch(): Switch =
+  let addrs = MultiAddress.init("/ip4/0.0.0.0/tcp/0").tryGet()
+  newStandardSwitch(addrs = addrs)
+
+proc connectToPeer(sw: Switch, peerAddr: string) {.async.} =
+  let ma = MultiAddress.init(peerAddr).tryGet()
+  let str = "16Uiu2HAm2uZtDK3T4zgseP16uGB6s2q2i8zpviLBmXyukjU5BPVT"
+  let peerIdData = str.mapIt(cast[byte](it))
+  let peerId = PeerId(data: peerIdData)
+  let connection = await sw.dial(peerId, @[ma], @["defaultProto"])
 
 proc main() {.async.} =
-  let switch = newStandardSwitch()
+  let addrs = "/ip4/139.99.173.27/tcp/30304"
+  let switch = createSwitch()
+  await switch.connectToPeer(addrs)
   let peerManager = PeerManager.new(switch)
   let wakuPx = WakuPeerExchange(peerManager: peerManager)
 
@@ -24,4 +33,4 @@ proc main() {.async.} =
     echo "Error requesting peers: ", res.error
 
 when isMainModule:
-  waitFor main()
+  asyncMain(main)
