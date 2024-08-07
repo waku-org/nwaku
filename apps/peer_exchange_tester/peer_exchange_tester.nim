abebded2b7d7af169597aee6f485925a36a2b3fa
DarshanBPatel <darshan@status.im>
2024-08-08 02:33:11 +0530

    chore: update PR

--- apps/peer_exchange_tester/peer_exchange_tester.nim
+++ apps/peer_exchange_tester/peer_exchange_tester.nim
@@ -6,21 +6,25 @@ import asyncdispatch
 
 proc createSwitch(): Switch =
   let addrs = MultiAddress.init("/ip4/0.0.0.0/tcp/0").tryGet()
-  newStandardSwitch(addrs = addrs)
+  return newStandardSwitch(addrs = addrs)
 
 proc connectToPeer(sw: Switch, peerAddr: string) {.async.} =
   let ma = MultiAddress.init(peerAddr).tryGet()
   let str = "16Uiu2HAkykgaECHswi3YKJ5dMLbq2kPVCo89fcyTd38UcQD6ej5W"
   let peerIdData = cast[seq[byte]](str)
   let peerId = PeerId(data: peerIdData)
-  # sw.dial(peerId, @[ma], @["/vac/waku/peer-exchange/2.0.0-alpha1"])
+  await sw.connect(peerId, @[ma])
+  echo "Connected to peer with address: ", peerAddr
 
 proc main() {.async.} =
   let addrs = "/dns4/node-01.do-ams3.waku.test.status.im/tcp/30303/p2p"
   let switch = createSwitch()
-  await switch.connectToPeer(addrs)
+  await connectToPeer(switch, addrs)
+  let connectedPeers = switch.connectedPeers()
+  if connectedPeers > 0:
+    echo "Successfully connected with Waku test fleet"
   let peerManager = PeerManager.new(switch)
   let wakuPx = WakuPeerExchange(peerManager: peerManager)
 
 when isMainModule:
-  asyncMain(main)
+  waitFor main()
