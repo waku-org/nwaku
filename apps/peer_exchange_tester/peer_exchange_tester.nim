64a1d244593736d642efc129743aa1f7e41cf33b
DarshanBPatel <darshan@status.im>
2024-08-07 14:44:43 +0530

    chore: update the PR

--- apps/peer_exchange_tester/peer_exchange_tester.nim
+++ apps/peer_exchange_tester/peer_exchange_tester.nim
@@ -1,8 +1,8 @@
 # import random, strutils, asyncdispatch, chronos, chronicles, stew/byteutils, waku/[waku_peer_exchange, node/peer_manager], tests/testlib/wakucore
-import random, chronos, chronicles, stew/byteutils
+import random, chronicles, stew/byteutils
 import waku/[waku_peer_exchange, node/peer_manager]
 import tests/testlib/wakucore
-import asyncdispatch as asyncd
+import asyncdispatch
 
 proc createSwitch(): Switch =
   let addrs = MultiAddress.init("/ip4/0.0.0.0/tcp/0").tryGet()
@@ -10,27 +10,20 @@ proc createSwitch(): Switch =
 
 proc connectToPeer(sw: Switch, peerAddr: string) {.async.} =
   let ma = MultiAddress.init(peerAddr).tryGet()
-  let str = "16Uiu2HAm2uZtDK3T4zgseP16uGB6s2q2i8zpviLBmXyukjU5BPVT"
-  let peerIdData = str.mapIt(cast[byte](it))
+  let str = "16Uiu2HAkykgaECHswi3YKJ5dMLbq2kPVCo89fcyTd38UcQD6ej5W"
+  let peerIdData = cast[seq[byte]](str)
   let peerId = PeerId(data: peerIdData)
-  let connection = await sw.dial(peerId, @[ma], @["defaultProto"])
+  # sw.dial(peerId, @[ma], @["/vac/waku/peer-exchange/2.0.0-alpha1"])
 
 proc main() {.async.} =
-  let addrs = "/ip4/139.99.173.27/tcp/30304"
+  let addrs = "/dns4/node-01.do-ams3.waku.test.status.im/tcp/30303/p2p"
   let switch = createSwitch()
   await switch.connectToPeer(addrs)
   let peerManager = PeerManager.new(switch)
   let wakuPx = WakuPeerExchange(peerManager: peerManager)
 
-  # Request peers and check if they are live
-  let res = await wakuPx.request(5)
-  if res.isOk:
-    let peers = res.get().peerInfos.mapIt(it.enr)
-    for peer in peers:
-      echo "Peer ENR: ", peer
-      # Add your logic to dial and check if the peer is live
-  else:
-    echo "Error requesting peers: ", res.error
-
 when isMainModule:
   asyncMain(main)
+
+# addrs :- /dns4/node-01.do-ams3.waku.test.status.im/tcp/30303/p2p/
+#          16Uiu2HAkykgaECHswi3YKJ5dMLbq2kPVCo89fcyTd38UcQD6ej5W
