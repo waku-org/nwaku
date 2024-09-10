1290ae8be57740c01316fef8b430acbee09d07b2
Ivan Folgueira Bande <ivansete@status.im>
2024-09-10 15:17:13 +0200

    adjusting lightpush test to the new response

--- tests/node/test_wakunode_lightpush.nim
+++ tests/node/test_wakunode_lightpush.nim
@@ -179,8 +179,5 @@ suite "RLN Proofs as a Lightpush Service":
 
       # Then the message is not relayed but not due to RLN
       assert publishResponse.isErr(), "We expect an error response"
-      assert (
-        publishResponse.error.contains(
-          "Lightpush request has not been published to any peers"
-        )
-      ), "incorrect error response"
+      assert (publishResponse.error == protocol_metrics.notPublishedAnyPeer),
+        "incorrect error response"
