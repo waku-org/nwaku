955dcca526667260733d1dec5ce77d37a949f7af
Gabriel mermelstein <gabrielmermelstein@gmail.com>
2024-11-26 15:01:17 +0200

    Revert "debug discv5 logs"
    This reverts commit f35ded4c6dceb7faa9e0681cfb3c898cc8f06651.

--- waku/discovery/waku_discv5.nim
+++ waku/discovery/waku_discv5.nim
@@ -215,7 +215,8 @@ proc findRandomPeers*(
 
   var discoveredRecords = discoveredNodes.mapIt(it.record)
 
-  logDiscv5FoundPeers(discoveredRecords)
+  when defined(debugDiscv5):
+    logDiscv5FoundPeers(discoveredRecords)
 
   # Filter out nodes that do not match the predicate
   if overridePred.isSome():
@@ -236,7 +237,7 @@ proc searchLoop(wd: WakuDiscoveryV5) {.async.} =
   info "Starting discovery v5 search"
 
   while wd.listening:
-    info "running discv5 discovery loop"
+    trace "running discv5 discovery loop"
     let discoveredRecords = await wd.findRandomPeers()
 
     var discoveredPeers: seq[RemotePeerInfo]
@@ -252,11 +253,11 @@ proc searchLoop(wd: WakuDiscoveryV5) {.async.} =
 
       discoveredPeers.add(peerInfo)
 
-    info "discv5 discovered peers",
+    trace "discv5 discovered peers",
       num_discovered_peers = discoveredPeers.len,
       peers = toSeq(discoveredPeers.mapIt(shortLog(it.peerId)))
 
-    info "discv5 discarded wrong records",
+    trace "discv5 discarded wrong records",
       wrong_records =
         wrongRecordsReasons.mapIt("(" & it.record & "," & it.errorDescription & ")")
 
