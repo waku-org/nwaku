c6ff44044c986c5dd7b02de19883c1231352de27
NagyZoltanPeter <113987313+NagyZoltanPeter@users.noreply.github.com>
2024-11-26 15:39:26 +0100

    Flush deployment supervisor output to log, set deployment test interval to 3h

--- apps/liteprotocoltester/lpt_supervisor.py
+++ apps/liteprotocoltester/lpt_supervisor.py
@@ -14,15 +14,14 @@ def load_env(file_path):
         predefined_test_env[key] = value
   return predefined_test_env
 
-def start_tester_node(predefined_test_env):
+def run_tester_node(predefined_test_env):
   role = sys.argv[1]
-  env = os.environ.copy()
   # override incoming environment variables with the ones from the file to prefer predefined testing environment.
   for key, value in predefined_test_env.items():
-      env[key] = value
-  script_cmd = ['sh', '/usr/bin/run_tester_node_at_infra.sh', '/usr/bin/liteprotocoltester', role]
-  process = Popen(script_cmd, env=env)
-  return process
+      os.environ[key] = value
+
+  script_cmd = "/usr/bin/run_tester_node_at_infra.sh /usr/bin/liteprotocoltester {role}".format(role=role)
+  return os.system(script_cmd)
 
 if __name__ == "__main__":
   if len(sys.argv) < 2 or sys.argv[1] not in ["RECEIVER", "SENDER"]:
@@ -33,18 +32,20 @@ if __name__ == "__main__":
   predefined_test_env = load_env(predefined_test_env_file)
 
   test_interval_minutes = int(predefined_test_env.get('TEST_INTERVAL_MINUTES', 60))  # Default to 60 minutes if not set
+  print(f"supervisor: Start testing loop. Interval is {test_interval_minutes} minutes")
+  counter = 0
 
   while True:
+    counter += 1
     start_time = time.time()
-    print(f"supervisor: Tester node started at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}")
+    print(f"supervisor: Run #{counter} started at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}")
     print(f"supervisor: with arguments: {predefined_test_env}")
 
-    process = start_tester_node(predefined_test_env)
-    process.wait()
+    exit_code = run_tester_node(predefined_test_env)
 
     end_time = time.time()
     run_time = end_time - start_time
-    sleep_time = max(5, (test_interval_minutes * 60) - run_time)
+    sleep_time = max(5 * 60, (test_interval_minutes * 60) - run_time)
 
     print(f"supervisor: Tester node finished at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(end_time))}")
     print(f"supervisor: Runtime was {run_time:.2f} seconds")
