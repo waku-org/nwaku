ac05fdab94ebb2351a433f3bca5ccd2aab50caba
Richard Ramos <info@richardramos.me>
2024-12-16 15:55:32 -0400

    feat(libwaku): add protected topic

--- library/waku_thread/inter_thread_communication/requests/protocols/relay_request.nim
+++ library/waku_thread/inter_thread_communication/requests/protocols/relay_request.nim
@@ -2,7 +2,7 @@ import std/net
 import chronicles, chronos, stew/byteutils, results
 import
   ../../../../../waku/waku_core/message/message,
-  ../../../../../waku/factory/waku,
+  ../../../../../waku/factory/[external_config, validator_signed, waku],
   ../../../../../waku/waku_core/message,
   ../../../../../waku/waku_core/time, # Timestamp
   ../../../../../waku/waku_core/topics/pubsub_topic,
@@ -17,6 +17,7 @@ type RelayMsgType* = enum
     ## to return the list of all connected peers to an specific pubsub topic
   LIST_MESH_PEERS
     ## to return the list of only the peers that conform the mesh for a particular pubsub topic
+  ADD_PROTECTED_SHARD ## Protects a shard with a public key
 
 type ThreadSafeWakuMessage* = object
   payload: SharedSeq[byte]
@@ -33,17 +34,26 @@ type RelayRequest* = object
   pubsubTopic: cstring
   relayEventCallback: WakuRelayHandler # not used in 'PUBLISH' requests
   message: ThreadSafeWakuMessage # only used in 'PUBLISH' requests
+  clusterId: cint # only used in 'ADD_PROTECTED_SHARD' requests
+  shardId: cint # only used in 'ADD_PROTECTED_SHARD' requests
+  publicKey: cstring # only used in 'ADD_PROTECTED_SHARD' requests
 
 proc createShared*(
     T: type RelayRequest,
     op: RelayMsgType,
-    pubsubTopic: PubsubTopic,
+    pubsubTopic: PubsubTopic = "",
     relayEventCallback: WakuRelayHandler = nil,
     m = WakuMessage(),
+    clusterId: cint = 0,
+    shardId: cint = 0,
+    publicKey: string = "",
 ): ptr type T =
   var ret = createShared(T)
   ret[].operation = op
   ret[].pubsubTopic = pubsubTopic.alloc()
+  ret[].clusterId = clusterId
+  ret[].shardId = shardId
+  ret[].publicKey = publicKey.alloc()
   ret[].relayEventCallback = relayEventCallback
   ret[].message = ThreadSafeWakuMessage(
     payload: allocSharedSeq(m.payload),
@@ -64,7 +74,8 @@ proc destroyShared(self: ptr RelayRequest) =
   deallocSharedSeq(self[].message.meta)
   when defined(rln):
     deallocSharedSeq(self[].message.proof)
-
+  deallocShared(self[].pubsubTopic)
+  deallocShared(self[].publicKey)
   deallocShared(self)
 
 proc toWakuMessage(m: ThreadSafeWakuMessage): WakuMessage =
@@ -120,5 +131,15 @@ proc process*(
       error "LIST_MESH_PEERS failed", error = error
       return err($error)
     return ok($numPeersInMesh)
-
+  of ADD_PROTECTED_SHARD:
+    try:
+      let relayShard =
+        RelayShard(clusterId: uint16(self.clusterId), shardId: uint16(self.shardId))
+      let protectedShard =
+        ProtectedShard.parseCmdArg($relayShard & ":" & $self.publicKey)
+      waku.node.wakuRelay.addSignedShardsValidator(
+        @[protectedShard], uint16(self.clusterId)
+      )
+    except ValueError:
+      return err("ADD_PROTECTED_SHARD exception: " & getCurrentExceptionMsg())
   return ok("")
