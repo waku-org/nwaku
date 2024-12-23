c540d9694d99cc4ea5c1ac502b9cab0a91043c19
Gabriel mermelstein <gabrielmermelstein@gmail.com>
2024-12-23 13:26:08 +0100

    add json connection change file

---
+++ library/events/json_connection_change_event.nim
@@ -0,0 +1,20 @@
+import system, std/json, libp2p/[connmanager, peerid]
+
+import ../../waku/common/base64, ./json_base_event
+
+type JsonConnectionChangeEvent* = ref object of JsonEvent
+  peerId*: PeerId
+  peerEvent*: PeerEventKind
+
+proc new*(
+    T: type JsonConnectionChangeEvent, peerId: PeerId, peerEvent: PeerEventKind
+): T =
+  # Returns a JsonConnectionChangeEvent event as indicated in
+  # https://rfc.vac.dev/spec/36/#jsonmessageevent-type
+
+  return JsonConnectionChangeEvent(
+    eventType: "relay_connection_change", peerId: peerId, peerEvent: peerEvent
+  )
+
+method `$`*(jsonConnectionChangeEvent: JsonConnectionChangeEvent): string =
+  $(%*jsonConnectionChangeEvent)
