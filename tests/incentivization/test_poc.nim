cb1c5e59f0612cad25d1b646f98fc762110ee64c
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2025-01-29 15:34:33 +0100

    add double-spend check for txid-based eligibility

--- tests/incentivization/test_poc.nim
+++ tests/incentivization/test_poc.nim
@@ -1,7 +1,7 @@
 {.used.}
 
 import
-  std/[options],
+  std/[options, sets],
   testutils/unittests,
   chronos,
   web3,
@@ -193,6 +193,22 @@ suite "Waku Incentivization PoC Eligibility Proofs":
     )
 
     assert isEligible.isOk(), isEligible.error
+
+  asyncTest "incentivization PoC: double-spend tx is not eligible":
+    ## Test that the same tx submitted twice is not eligible the second time
+
+    let eligibilityProof =
+      EligibilityProof(proofOfPayment: some(@(txHashRightReceiverRightAmount.bytes())))
+
+    let isEligibleOnce = await manager.isEligibleTxId(
+      eligibilityProof, receiverExpected, TxValueExpectedWei
+    )
+
+    let isEligibleTwice = await manager.isEligibleTxId(
+      eligibilityProof, receiverExpected, TxValueExpectedWei
+    )
+
+    assert (isEligibleOnce.isOk() and isEligibleTwice.isErr()), isEligibleTwice.error
 
   # Stop Anvil daemon
   stopAnvil(runAnvil)
