a3876f1ec45de1f4945eb7e02a5732f3ad9c67ab
gabrielmer <101006718+gabrielmer@users.noreply.github.com>
2025-02-17 18:37:43 +0200

    fix: libwaku's invalid waku message error handling (#3301)

--- library/libwaku.nim
+++ library/libwaku.nim
@@ -291,18 +291,17 @@ proc waku_relay_publish(
   checkLibwakuParams(ctx, callback, userData)
 
   let jwm = jsonWakuMessage.alloc()
+  defer:
+    deallocShared(jwm)
   var jsonMessage: JsonMessage
   try:
     let jsonContent = parseJson($jwm)
     jsonMessage = JsonMessage.fromJsonNode(jsonContent).valueOr:
       raise newException(JsonParsingError, $error)
   except JsonParsingError:
-    deallocShared(jwm)
     let msg = fmt"Error parsing json message: {getCurrentExceptionMsg()}"
     callback(RET_ERR, unsafeAddr msg[0], cast[csize_t](len(msg)), userData)
     return RET_ERR
-  finally:
-    deallocShared(jwm)
 
   let wakuMessage = jsonMessage.toWakuMessage().valueOr:
     let msg = "Problem building the WakuMessage: " & $error
