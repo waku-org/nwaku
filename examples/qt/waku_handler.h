c3567a723b7ed512be333fb768e670d0d101acae
Ivan Folgueira Bande <ivansete@status.im>
2025-02-27 22:01:04 +0100

    add simple Qt example that uses libwaku

---
+++ examples/qt/waku_handler.h
@@ -0,0 +1,44 @@
+#include <QObject>
+#include <QDebug>
+#include <QString>
+
+#include "../../library/libwaku.h"
+
+class WakuHandler : public QObject {
+    Q_OBJECT
+private:
+    static void event_handler(int callerRet, const char* msg, size_t len, void* userData) {
+        printf("Receiving message %s\n", msg);
+    }
+
+public:
+    WakuHandler() : QObject(), ctx(nullptr) {}
+
+    void initialize(const QString& jsonConfig, void* event_handler, void* userData) {
+        ctx = waku_new(jsonConfig.toUtf8().constData(), WakuCallBack(event_handler), userData);
+        qDebug() << "Waku context initialized, ready to start.";
+    }
+
+    Q_INVOKABLE void start() {
+        if (ctx) {
+            waku_start(ctx, event_handler, nullptr);
+            qDebug() << "Waku start called with event_handler and userData.";
+        } else {
+            qDebug() << "Context is not initialized in start.";
+        }
+    }
+
+    Q_INVOKABLE void stop() {
+        if (ctx) {
+            waku_stop(ctx, event_handler, nullptr);
+            qDebug() << "Waku stop called with event_handler and userData.";
+        } else {
+            qDebug() << "Context is not initialized in stop.";
+        }
+    }
+
+    virtual ~WakuHandler() {}
+
+private:
+    void* ctx;
+};
