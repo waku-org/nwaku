1610e78bc417b882f88ebbe38849378446f1ab5e
Ivan Folgueira Bande <ivansete@status.im>
2025-03-01 23:28:57 +0100

    nph fix

--- waku/common/databases/db_postgres/query_metrics.nim
+++ waku/common/databases/db_postgres/query_metrics.nim
@@ -11,18 +11,21 @@ const QueriesToMetricMap* = {
   "contentTopic IN": "content_topic",
   "SELECT version()": "select_version",
   "WITH min_timestamp": "messages_lookup",
-  "SELECT messageHash FROM messages WHERE pubsubTopic = ? AND timestamp >= ? AND timestamp <= ? ORDER BY timestamp DESC, messageHash DESC LIMIT ?": "msg_hash_no_ctopic",
+  "SELECT messageHash FROM messages WHERE pubsubTopic = ? AND timestamp >= ? AND timestamp <= ? ORDER BY timestamp DESC, messageHash DESC LIMIT ?":
+    "msg_hash_no_ctopic",
   "AS partition_name": "get_partitions_list",
   "SELECT COUNT(1) FROM messages": "count_msgs",
-  "SELECT messageHash FROM messages WHERE (timestamp, messageHash) < (?,?) AND pubsubTopic = ? AND timestamp >= ? AND timestamp <= ? ORDER BY timestamp DESC, messageHash DESC LIMIT ?": "msg_hash_with_cursor",
+  "SELECT messageHash FROM messages WHERE (timestamp, messageHash) < (?,?) AND pubsubTopic = ? AND timestamp >= ? AND timestamp <= ? ORDER BY timestamp DESC, messageHash DESC LIMIT ?":
+    "msg_hash_with_cursor",
   "SELECT pg_database_size(current_database())": "get_database_size",
   "DELETE FROM messages_lookup WHERE timestamp": "delete_from_msgs_lookup",
   "DROP TABLE messages_": "drop_partition_table",
   "ALTER TABLE messages DETACH PARTITION": "detach_partition",
   "SELECT pg_size_pretty(pg_total_relation_size(C.oid))": "get_partition_size",
   "pg_try_advisory_lock": "try_advisory_lock",
-  "SELECT messageHash FROM messages ORDER BY timestamp DESC, messageHash DESC LIMIT ?": "get_all_msg_hash",
+  "SELECT messageHash FROM messages ORDER BY timestamp DESC, messageHash DESC LIMIT ?":
+    "get_all_msg_hash",
   "SELECT pg_advisory_unlock": "advisory_unlock",
   "ANALYZE messages": "analyze_messages",
-  "SELECT EXISTS": "check_version_table_exists"
+  "SELECT EXISTS": "check_version_table_exists",
 }
