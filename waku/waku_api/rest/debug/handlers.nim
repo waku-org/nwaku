fe8327627ee15984bbecef4ca2864bcb7ab2a9b6
NagyZoltanPeter <113987313+NagyZoltanPeter@users.noreply.github.com>
2025-03-27 11:56:44 +0100

    Make /debug endpoints /version and /info available as root (#3333)

--- waku/waku_api/rest/debug/handlers.nim
+++ waku/waku_api/rest/debug/handlers.nim
@@ -8,10 +8,12 @@ export types
 logScope:
   topics = "waku node rest debug_api"
 
-const ROUTE_DEBUG_INFOV1* = "/debug/v1/info"
+const ROUTE_INFOV1* = "/info"
+# /debug route is deprecated, will be removed
+const ROUTE_DEBUG_INFOV1 = "/debug/v1/info"
 
 proc installDebugInfoV1Handler(router: var RestRouter, node: WakuNode) =
-  router.api(MethodGet, ROUTE_DEBUG_INFOV1) do() -> RestApiResponse:
+  let getInfo = proc(): RestApiResponse =
     let info = node.info().toDebugWakuInfo()
     let resp = RestApiResponse.jsonResponse(info, status = Http200)
     if resp.isErr():
@@ -20,10 +22,21 @@ proc installDebugInfoV1Handler(router: var RestRouter, node: WakuNode) =
 
     return resp.get()
 
-const ROUTE_DEBUG_VERSIONV1* = "/debug/v1/version"
+  # /debug route is deprecated, will be removed
+  router.api(MethodGet, ROUTE_DEBUG_INFOV1) do() -> RestApiResponse:
+    return getInfo()
+  router.api(MethodGet, ROUTE_INFOV1) do() -> RestApiResponse:
+    return getInfo()
+
+const ROUTE_VERSIONV1* = "/version"
+# /debug route is deprecated, will be removed
+const ROUTE_DEBUG_VERSIONV1 = "/debug/v1/version"
 
 proc installDebugVersionV1Handler(router: var RestRouter, node: WakuNode) =
+  # /debug route is deprecated, will be removed
   router.api(MethodGet, ROUTE_DEBUG_VERSIONV1) do() -> RestApiResponse:
+    return RestApiResponse.textResponse(git_version, status = Http200)
+  router.api(MethodGet, ROUTE_VERSIONV1) do() -> RestApiResponse:
     return RestApiResponse.textResponse(git_version, status = Http200)
 
 proc installDebugApiHandlers*(router: var RestRouter, node: WakuNode) =
