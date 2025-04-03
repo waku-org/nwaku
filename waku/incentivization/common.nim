b26606c6e2183cd32cd1971ab461b06416a761fd
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2025-04-03 11:48:51 +0200

    add EligibilityStatusCode type

--- waku/incentivization/common.nim
+++ waku/incentivization/common.nim
@@ -2,8 +2,12 @@ import std/options
 
 import waku/incentivization/[rpc, eligibility_manager]
 
+type EligibilityStatusCode* = enum
+  SUCCESS = uint32(200)
+  PAYMENT_REQUIRED = uint32(402)
+
 proc init*(T: type EligibilityStatus, isEligible: bool): T =
   if isEligible:
-    EligibilityStatus(statusCode: uint32(200), statusDesc: some("OK"))
+    EligibilityStatus(statusCode: uint32(EligibilityStatusCode.SUCCESS), statusDesc: some("OK"))
   else:
-    EligibilityStatus(statusCode: uint32(402), statusDesc: some("Payment Required"))
+    EligibilityStatus(statusCode: uint32(EligibilityStatusCode.PAYMENT_REQUIRED), statusDesc: some("Payment Required"))
