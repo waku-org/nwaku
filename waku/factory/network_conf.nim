a276eb62ecf660e15d1f5d0cdbdb59138f1627e8
Ivan FB <128452529+Ivansete-status@users.noreply.github.com>
2025-04-25 10:23:24 +0200

    fix: compilation issues and test_waku_netconfig (#3386)
    * adjust discv5 test to avoid port clash
    * rm network_conf_builder module as it is not used

--- waku/factory/network_conf.nim
+++ waku/factory/network_conf.nim
@@ -1,4 +1,5 @@
-import std[net, options, strutils]
+import std/[net, options, strutils]
+import libp2p/multiaddress
 
 type WebSocketSecureConf* {.requiresInit.} = object
   keyPath*: string
@@ -22,16 +23,12 @@ proc validateNoEmptyStrings(networkConf: NetworkConf): Result[void, string] =
       isEmptyOrWhiteSpace(networkConf.dns4DomainName.get().string):
     return err("dns4DomainName is an empty string, set it to none(string) instead")
 
-    if networkConf.secureConf.isSome() and networkConf.webSocketConf.isSome() and
-        networkConf.webSocketConf.secureConf.isSome():
-      let secureConf = networkConf.webSocketConf.secureConf.get()
+    if networkConf.webSocketConf.isSome() and
+        networkConf.webSocketConf.get().secureConf.isSome():
+      let secureConf = networkConf.webSocketConf.get().secureConf.get()
       if isEmptyOrWhiteSpace(secureConf.keyPath):
         return err("websocket.secureConf.keyPath is an empty string")
       if isEmptyOrWhiteSpace(secureConf.certPath):
         return err("websocket.secureConf.certPath is an empty string")
 
-  return ok()
-
-proc validate*(wakuConf: WakuConf): Result[void, string] =
-  ?wakuConf.validateNoEmptyStrings()
   return ok()
