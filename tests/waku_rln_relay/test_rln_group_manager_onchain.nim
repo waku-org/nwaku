47f86d52276acd592930b12a3f68b4ad1e816e7c
Ivan Folgueira Bande <ivansete@status.im>
2025-05-20 00:39:17 +0200

    minor test refactor

--- tests/waku_rln_relay/test_rln_group_manager_onchain.nim
+++ tests/waku_rln_relay/test_rln_group_manager_onchain.nim
@@ -64,12 +64,14 @@ suite "Onchain group manager":
 
   asyncTest "should error on initialization when loaded metadata does not match":
     (await manager.init()).isOkOr:
-      raiseAssert $error
+      assert false, $error
 
     let metadataSetRes = manager.setMetadata()
     assert metadataSetRes.isOk(), metadataSetRes.error
     let metadataOpt = manager.rlnInstance.getMetadata().valueOr:
-      raiseAssert $error
+      assert false, $error
+      return
+
     assert metadataOpt.isSome(), "metadata is not set"
     let metadata = metadataOpt.get()
 
@@ -84,17 +86,12 @@ suite "Onchain group manager":
       ethContractAddress: $differentContractAddress,
       rlnInstance: manager.rlnInstance,
       onFatalErrorAction: proc(errStr: string) =
-        raiseAssert errStr
+        assert false, errStr
       ,
     )
     let e = await manager2.init()
     (e).isErrOr:
-      raiseAssert "Expected error when contract address doesn't match"
-
-    echo "---"
-    discard "persisted data: contract address mismatch"
-    echo e.error
-    echo "---"
+      assert false, "Expected error when contract address doesn't match"
 
   asyncTest "should error if contract does not exist":
     manager.ethContractAddress = "0x0000000000000000000000000000000000000000"
