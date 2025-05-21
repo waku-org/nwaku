0d00fb88106a9e74d1508a818dee947b98ea89f7
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2025-05-21 14:48:58 +0200

    add JSON support for eligibility proof in Lightpush REST API

--- waku/incentivization/rpc.nim
+++ waku/incentivization/rpc.nim
@@ -1,4 +1,9 @@
-import std/options
+import  
+  std/[options, strutils],
+  stew/byteutils,  
+  json_serialization,  
+  json_serialization/std/options,  
+  ../waku_api/rest/serdes
 
 # Implementing the RFC:
 # https://github.com/vacp2p/rfc/tree/master/content/docs/rfcs/73
@@ -10,3 +15,26 @@ type
   EligibilityStatus* = object
     statusCode*: uint32
     statusDesc*: Option[string]
+
+proc writeValue*(
+    writer: var JsonWriter[RestJson], value: EligibilityProof
+) {.raises: [IOError].} =
+  if value.proofOfPayment.isSome():
+    writer.writeValue("0x" & value.proofOfPayment.get().toHex())
+  else:
+    writer.writeValue("")
+  
+proc readValue*(  
+    reader: var JsonReader[RestJson], value: var EligibilityProof
+) {.raises: [SerializationError, IOError].} =
+  let hexStr = reader.readValue(string)
+  if hexStr.len > 0:
+    let startIndex = if hexStr.len > 2 and hexStr[0..1] == "0x": 2 else: 0
+    try:
+      let bytes = hexToSeqByte(hexStr[startIndex..^1])
+      value = EligibilityProof(proofOfPayment: some(bytes))
+    except ValueError as e:
+      # Either handle the error or re-raise it
+      raise newException(SerializationError, "Invalid hex string: " & e.msg)
+  else:
+    value = EligibilityProof(proofOfPayment: none(seq[byte]))
