1ad55120a2414c46a3613d7f2155c5eb5c040976
Jakub Sokołowski <jakub@status.im>
2025-05-27 22:21:24 +0200

    nix: fix extracting data from koch.nim config
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- nix/nimble.nix
+++ nix/nimble.nix
@@ -6,7 +6,7 @@ let
 in pkgs.fetchFromGitHub {
   owner = "nim-lang";
   repo = "nimble";
-  rev = tools.findKeyValue "^ +NimbleStableCommit = \"([a-f0-9]+)\".+" sourceFile;
+  rev = tools.findKeyValue "^ +NimbleStableCommit = \"([a-f0-9]+)\".*$" sourceFile;
   # WARNING: Requires manual updates when Nim compiler version changes.
   hash = "sha256-MVHf19UbOWk8Zba2scj06PxdYYOJA6OXrVyDQ9Ku6Us=";
 }
