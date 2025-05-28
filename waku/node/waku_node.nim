deb7c593e2de0ca3480d454eee6cc8fdc90f2aa3
Gabriel mermelstein <gabrielmermelstein@gmail.com>
2025-05-28 13:04:50 +0200

    Revert "not returning error when attempting to unsubscribe to not subscribed topic"
    This reverts commit d0d34353bd98e8c8e4a002e074e6acdfd92e2b8d.

--- waku/node/waku_node.nim
+++ waku/node/waku_node.nim
@@ -364,8 +364,9 @@ proc unsubscribe*(
       return err("Unsupported subscription type in relay unsubscribe")
 
   if not node.wakuRelay.isSubscribed(pubsubTopic):
-    warn "Call to `unsubscribe` for when not subscribed to topic", pubsubTopic
-    return ok()
+    error "Invalid API call to `unsubscribe`. Was not subscribed", pubsubTopic
+    return
+      err("Invalid API call to `unsubscribe`. Was not subscribed to: " & $pubsubTopic)
 
   if contentTopicOp.isSome():
     # Remove this handler only
