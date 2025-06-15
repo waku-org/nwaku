8e93478fa6ffd09565be9f62e5f0b1bbdc222354
Ivan Folgueira Bande <ivansete@status.im>
2025-06-15 15:50:56 +0200

    enhance health status enum parsing

--- waku/node/health_monitor/health_status.nim
+++ waku/node/health_monitor/health_status.nim
@@ -1,4 +1,4 @@
-import results
+import results, std/strutils
 
 type HealthStatus* {.pure.} = enum
   INITIALIZING
@@ -9,18 +9,8 @@ type HealthStatus* {.pure.} = enum
   SHUTTING_DOWN
 
 proc init*(t: typedesc[HealthStatus], strRep: string): Result[HealthStatus, string] =
-  case strRep
-  of "Initializing":
-    return ok(HealthStatus.INITIALIZING)
-  of "Synchronizing":
-    return ok(HealthStatus.SYNCHRONIZING)
-  of "Ready":
-    return ok(HealthStatus.READY)
-  of "Not Ready":
-    return ok(HealthStatus.NOT_READY)
-  of "Not Mounted":
-    return ok(HealthStatus.NOT_MOUNTED)
-  of "Shutting Down":
-    return ok(HealthStatus.SHUTTING_DOWN)
-  else:
-    return err("Invalid HealthStatus string representation")
+  try:
+    let status = parseEnum[HealthStatus](strRep)
+    return ok(status)
+  except ValueError:
+    return err("Invalid HealthStatus string representation: " & strRep)
