2e40f2971f97ecf6bc63db101c8edf6a28737cdc
Ivan FB <128452529+Ivansete-status@users.noreply.github.com>
2025-06-16 18:44:21 +0200

    chore: refactor to unify online and health monitors (#3456)

--- apps/liteprotocoltester/liteprotocoltester.nim
+++ apps/liteprotocoltester/liteprotocoltester.nim
@@ -14,13 +14,11 @@ import
   waku/[
     common/enr,
     common/logging,
-    factory/waku,
+    factory/waku as waku_factory,
     factory/external_config,
     waku_node,
-    node/health_monitor,
     node/waku_metrics,
     node/peer_manager,
-    waku_api/rest/builder as rest_server_builder,
     waku_lightpush/common,
     waku_filter_v2,
     waku_peer_exchange/protocol,
@@ -49,7 +47,7 @@ when isMainModule:
   ## 5. Start monitoring tools and external interfaces
   ## 6. Setup graceful shutdown hooks
 
-  const versionString = "version / git commit hash: " & waku.git_version
+  const versionString = "version / git commit hash: " & waku_factory.git_version
 
   let confRes = LiteProtocolTesterConf.load(version = versionString)
   if confRes.isErr():
@@ -61,7 +59,7 @@ when isMainModule:
   ## Logging setup
   logging.setupLog(conf.logLevel, conf.logFormat)
 
-  info "Running Lite Protocol Tester node", version = waku.git_version
+  info "Running Lite Protocol Tester node", version = waku_factory.git_version
   logConfig(conf)
 
   ##Prepare Waku configuration
@@ -69,13 +67,13 @@ when isMainModule:
   ## - override according to tester functionality
   ##
 
-  var wConf: WakuNodeConf
+  var wakuNodeConf: WakuNodeConf
 
   if conf.configFile.isSome():
     try:
       var configFile {.threadvar.}: InputFile
       configFile = conf.configFile.get()
-      wConf = WakuNodeConf.load(
+      wakuNodeConf = WakuNodeConf.load(
         version = versionString,
         printUsage = false,
         secondarySources = proc(
@@ -88,101 +86,54 @@ when isMainModule:
       error "Loading Waku configuration failed", error = getCurrentExceptionMsg()
       quit(QuitFailure)
 
-  wConf.logLevel = conf.logLevel
-  wConf.logFormat = conf.logFormat
-  wConf.nat = conf.nat
-  wConf.maxConnections = 500
-  wConf.restAddress = conf.restAddress
-  wConf.restPort = conf.restPort
-  wConf.restAllowOrigin = conf.restAllowOrigin
+  wakuNodeConf.logLevel = conf.logLevel
+  wakuNodeConf.logFormat = conf.logFormat
+  wakuNodeConf.nat = conf.nat
+  wakuNodeConf.maxConnections = 500
+  wakuNodeConf.restAddress = conf.restAddress
+  wakuNodeConf.restPort = conf.restPort
+  wakuNodeConf.restAllowOrigin = conf.restAllowOrigin
 
-  wConf.dnsAddrsNameServers = @[parseIpAddress("8.8.8.8"), parseIpAddress("1.1.1.1")]
+  wakuNodeConf.dnsAddrsNameServers =
+    @[parseIpAddress("8.8.8.8"), parseIpAddress("1.1.1.1")]
 
-  wConf.shards = @[conf.shard]
-  wConf.contentTopics = conf.contentTopics
-  wConf.clusterId = conf.clusterId
+  wakuNodeConf.shards = @[conf.shard]
+  wakuNodeConf.contentTopics = conf.contentTopics
+  wakuNodeConf.clusterId = conf.clusterId
   ## TODO: Depending on the tester needs we might extend here with shards, clusterId, etc...
 
-  wConf.metricsServer = true
-  wConf.metricsServerAddress = parseIpAddress("0.0.0.0")
-  wConf.metricsServerPort = conf.metricsPort
+  wakuNodeConf.metricsServer = true
+  wakuNodeConf.metricsServerAddress = parseIpAddress("0.0.0.0")
+  wakuNodeConf.metricsServerPort = conf.metricsPort
 
   # If bootstrap option is chosen we expect our clients will not mounted
   # so we will mount PeerExchange manually to gather possible service peers,
   # if got some we will mount the client protocols afterward.
-  wConf.peerExchange = false
-  wConf.relay = false
-  wConf.filter = false
-  wConf.lightpush = false
-  wConf.store = false
+  wakuNodeConf.peerExchange = false
+  wakuNodeConf.relay = false
+  wakuNodeConf.filter = false
+  wakuNodeConf.lightpush = false
+  wakuNodeConf.store = false
 
-  wConf.rest = false
-  wConf.relayServiceRatio = "40:60"
+  wakuNodeConf.rest = false
+  wakuNodeConf.relayServiceRatio = "40:60"
 
-  # NOTE: {.threadvar.} is used to make the global variable GC safe for the closure uses it
-  # It will always be called from main thread anyway.
-  # Ref: https://nim-lang.org/docs/manual.html#threads-gc-safety
-  var nodeHealthMonitor {.threadvar.}: WakuNodeHealthMonitor
-  nodeHealthMonitor = WakuNodeHealthMonitor()
-  nodeHealthMonitor.setOverallHealth(HealthStatus.INITIALIZING)
-
-  let wakuConf = wConf.toWakuConf().valueOr:
-    error "Waku configuration failed", error = error
+  let wakuConf = wakuNodeConf.toWakuConf().valueOr:
+    error "Issue converting toWakuConf", error = $error
     quit(QuitFailure)
 
-  let restServer: WakuRestServerRef =
-    if wakuConf.restServerConf.isSome():
-      rest_server_builder.startRestServerEssentials(
-        nodeHealthMonitor, wakuConf.restServerConf.get(), wakuConf.portsShift
-      ).valueOr:
-        error "Starting essential REST server failed.", error = $error
-        quit(QuitFailure)
-    else:
-      nil
-
-  var wakuApp = Waku.new(wakuConf).valueOr:
+  var waku = Waku.new(wakuConf).valueOr:
     error "Waku initialization failed", error = error
     quit(QuitFailure)
 
-  wakuApp.restServer = restServer
-
-  nodeHealthMonitor.setNode(wakuApp.node)
-
-  (waitFor startWaku(addr wakuApp)).isOkOr:
+  (waitFor startWaku(addr waku)).isOkOr:
     error "Starting waku failed", error = error
     quit(QuitFailure)
 
-  if wakuConf.restServerConf.isSome():
-    rest_server_builder.startRestServerProtocolSupport(
-      restServer,
-      wakuApp.node,
-      wakuApp.wakuDiscv5,
-      wakuConf.restServerConf.get(),
-      wakuConf.relay,
-      wakuConf.lightPush,
-      wakuConf.clusterId,
-      wakuConf.shards,
-      wakuConf.contentTopics,
-    ).isOkOr:
-      error "Starting protocols support REST server failed.", error = $error
-      quit(QuitFailure)
-
-  if wakuConf.metricsServerConf.isSome():
-    wakuApp.metricsServer = waku_metrics.startMetricsServerAndLogging(
-      wakuConf.metricsServerConf.get(), wakuConf.portsShift
-    ).valueOr:
-      error "Starting monitoring and external interfaces failed", error = error
-      quit(QuitFailure)
-
-  nodeHealthMonitor.setOverallHealth(HealthStatus.READY)
-
   debug "Setting up shutdown hooks"
-  ## Setup shutdown hooks for this process.
-  ## Stop node gracefully on shutdown.
 
-  proc asyncStopper(wakuApp: Waku) {.async: (raises: [Exception]).} =
-    nodeHealthMonitor.setOverallHealth(HealthStatus.SHUTTING_DOWN)
-    await wakuApp.stop()
+  proc asyncStopper(waku: Waku) {.async: (raises: [Exception]).} =
+    await waku.stop()
     quit(QuitSuccess)
 
   # Handle Ctrl-C SIGINT
@@ -191,7 +142,7 @@ when isMainModule:
       # workaround for https://github.com/nim-lang/Nim/issues/4057
       setupForeignThreadGc()
     notice "Shutting down after receiving SIGINT"
-    asyncSpawn asyncStopper(wakuApp)
+    asyncSpawn asyncStopper(waku)
 
   setControlCHook(handleCtrlC)
 
@@ -199,7 +150,7 @@ when isMainModule:
   when defined(posix):
     proc handleSigterm(signal: cint) {.noconv.} =
       notice "Shutting down after receiving SIGTERM"
-      asyncSpawn asyncStopper(wakuApp)
+      asyncSpawn asyncStopper(waku)
 
     c_signal(ansi_c.SIGTERM, handleSigterm)
 
@@ -212,22 +163,26 @@ when isMainModule:
       # Not available in -d:release mode
       writeStackTrace()
 
-      waitFor wakuApp.stop()
+      waitFor waku.stop()
       quit(QuitFailure)
 
     c_signal(ansi_c.SIGSEGV, handleSigsegv)
 
   info "Node setup complete"
 
-  let codec = conf.getCodec()
+  var codec = WakuLightPushCodec
   # mounting relevant client, for PX filter client must be mounted ahead
+  if conf.testFunc == TesterFunctionality.SENDER:
+    codec = WakuLightPushCodec
+  else:
+    codec = WakuFilterSubscribeCodec
 
   var lookForServiceNode = false
   var serviceNodePeerInfo: RemotePeerInfo
   if conf.serviceNode.len == 0:
     if conf.bootstrapNode.len > 0:
       info "Bootstrapping with PeerExchange to gather random service node"
-      let futForServiceNode = pxLookupServiceNode(wakuApp.node, conf)
+      let futForServiceNode = pxLookupServiceNode(waku.node, conf)
       if not (waitFor futForServiceNode.withTimeout(20.minutes)):
         error "Service node not found in time via PX"
         quit(QuitFailure)
@@ -237,7 +192,7 @@ when isMainModule:
         quit(QuitFailure)
 
       serviceNodePeerInfo = selectRandomServicePeer(
-        wakuApp.node.peerManager, none(RemotePeerInfo), codec
+        waku.node.peerManager, none(RemotePeerInfo), codec
       ).valueOr:
         error "Service node selection failed"
         quit(QuitFailure)
@@ -252,11 +207,11 @@ when isMainModule:
 
   info "Service node to be used", serviceNode = $serviceNodePeerInfo
 
-  logSelfPeers(wakuApp.node.peerManager)
+  logSelfPeers(waku.node.peerManager)
 
   if conf.testFunc == TesterFunctionality.SENDER:
-    setupAndPublish(wakuApp.node, conf, serviceNodePeerInfo)
+    setupAndPublish(waku.node, conf, serviceNodePeerInfo)
   else:
-    setupAndListen(wakuApp.node, conf, serviceNodePeerInfo)
+    setupAndListen(waku.node, conf, serviceNodePeerInfo)
 
   runForever()
---
+++ waku/node/health_monitor/node_health_monitor.nim
@@ -0,0 +1,270 @@
+{.push raises: [].}
+
+import std/[options, sets, strformat], chronos, chronicles, libp2p/protocols/rendezvous
+
+import
+  ../waku_node,
+  ../../waku_rln_relay,
+  ../../waku_relay,
+  ../peer_manager,
+  ./online_monitor,
+  ./health_status,
+  ./protocol_health
+
+## This module is aimed to check the state of the "self" Waku Node
+
+type
+  HealthReport* = object
+    nodeHealth*: HealthStatus
+    protocolsHealth*: seq[ProtocolHealth]
+
+  NodeHealthMonitor* = ref object
+    nodeHealth: HealthStatus
+    node: WakuNode
+    onlineMonitor*: OnlineMonitor
+
+template checkWakuNodeNotNil(node: WakuNode, p: ProtocolHealth): untyped =
+  if node.isNil():
+    warn "WakuNode is not set, cannot check health", protocol_health_instance = $p
+    return p.notMounted()
+
+proc getRelayHealth(hm: NodeHealthMonitor): ProtocolHealth =
+  var p = ProtocolHealth.init("Relay")
+  checkWakuNodeNotNil(hm.node, p)
+
+  if hm.node.wakuRelay == nil:
+    return p.notMounted()
+
+  let relayPeers = hm.node.wakuRelay.getConnectedPubSubPeers(pubsubTopic = "").valueOr:
+    return p.notMounted()
+
+  if relayPeers.len() == 0:
+    return p.notReady("No connected peers")
+
+  return p.ready()
+
+proc getRlnRelayHealth(hm: NodeHealthMonitor): Future[ProtocolHealth] {.async.} =
+  var p = ProtocolHealth.init("Rln Relay")
+  if hm.node.isNil():
+    warn "WakuNode is not set, cannot check health", protocol_health_instance = $p
+    return p.notMounted()
+
+  if hm.node.wakuRlnRelay.isNil():
+    return p.notMounted()
+
+  const FutIsReadyTimout = 5.seconds
+
+  let isReadyStateFut = hm.node.wakuRlnRelay.isReady()
+  if not await isReadyStateFut.withTimeout(FutIsReadyTimout):
+    return p.notReady("Ready state check timed out")
+
+  try:
+    if not isReadyStateFut.completed():
+      return p.notReady("Ready state check timed out")
+    elif isReadyStateFut.read():
+      return p.ready()
+
+    return p.synchronizing()
+  except:
+    error "exception reading state: " & getCurrentExceptionMsg()
+    return p.notReady("State cannot be determined")
+
+proc getLightpushHealth(
+    hm: NodeHealthMonitor, relayHealth: HealthStatus
+): ProtocolHealth =
+  var p = ProtocolHealth.init("Lightpush")
+  checkWakuNodeNotNil(hm.node, p)
+
+  if hm.node.wakuLightPush == nil:
+    return p.notMounted()
+
+  if relayHealth == HealthStatus.READY:
+    return p.ready()
+
+  return p.notReady("Node has no relay peers to fullfill push requests")
+
+proc getLightpushClientHealth(
+    hm: NodeHealthMonitor, relayHealth: HealthStatus
+): ProtocolHealth =
+  var p = ProtocolHealth.init("Lightpush Client")
+  checkWakuNodeNotNil(hm.node, p)
+
+  if hm.node.wakuLightpushClient == nil:
+    return p.notMounted()
+
+  let selfServiceAvailable =
+    hm.node.wakuLightPush != nil and relayHealth == HealthStatus.READY
+  let servicePeerAvailable = hm.node.peerManager.selectPeer(WakuLightPushCodec).isSome()
+
+  if selfServiceAvailable or servicePeerAvailable:
+    return p.ready()
+
+  return p.notReady("No Lightpush service peer available yet")
+
+proc getLegacyLightpushHealth(
+    hm: NodeHealthMonitor, relayHealth: HealthStatus
+): ProtocolHealth =
+  var p = ProtocolHealth.init("Legacy Lightpush")
+  checkWakuNodeNotNil(hm.node, p)
+
+  if hm.node.wakuLegacyLightPush == nil:
+    return p.notMounted()
+
+  if relayHealth == HealthStatus.READY:
+    return p.ready()
+
+  return p.notReady("Node has no relay peers to fullfill push requests")
+
+proc getLegacyLightpushClientHealth(
+    hm: NodeHealthMonitor, relayHealth: HealthStatus
+): ProtocolHealth =
+  var p = ProtocolHealth.init("Legacy Lightpush Client")
+  checkWakuNodeNotNil(hm.node, p)
+
+  if hm.node.wakuLegacyLightpushClient == nil:
+    return p.notMounted()
+
+  if (hm.node.wakuLegacyLightPush != nil and relayHealth == HealthStatus.READY) or
+      hm.node.peerManager.selectPeer(WakuLegacyLightPushCodec).isSome():
+    return p.ready()
+
+  return p.notReady("No Lightpush service peer available yet")
+
+proc getFilterHealth(hm: NodeHealthMonitor, relayHealth: HealthStatus): ProtocolHealth =
+  var p = ProtocolHealth.init("Filter")
+  checkWakuNodeNotNil(hm.node, p)
+
+  if hm.node.wakuFilter == nil:
+    return p.notMounted()
+
+  if relayHealth == HealthStatus.READY:
+    return p.ready()
+
+  return p.notReady("Relay is not ready, filter will not be able to sort out messages")
+
+proc getFilterClientHealth(
+    hm: NodeHealthMonitor, relayHealth: HealthStatus
+): ProtocolHealth =
+  var p = ProtocolHealth.init("Filter Client")
+  checkWakuNodeNotNil(hm.node, p)
+
+  if hm.node.wakuFilterClient == nil:
+    return p.notMounted()
+
+  if hm.node.peerManager.selectPeer(WakuFilterSubscribeCodec).isSome():
+    return p.ready()
+
+  return p.notReady("No Filter service peer available yet")
+
+proc getStoreHealth(hm: NodeHealthMonitor): ProtocolHealth =
+  var p = ProtocolHealth.init("Store")
+  checkWakuNodeNotNil(hm.node, p)
+
+  if hm.node.wakuStore == nil:
+    return p.notMounted()
+
+  return p.ready()
+
+proc getStoreClientHealth(hm: NodeHealthMonitor): ProtocolHealth =
+  var p = ProtocolHealth.init("Store Client")
+  checkWakuNodeNotNil(hm.node, p)
+
+  if hm.node.wakuStoreClient == nil:
+    return p.notMounted()
+
+  if hm.node.peerManager.selectPeer(WakuStoreCodec).isSome() or hm.node.wakuStore != nil:
+    return p.ready()
+
+  return p.notReady(
+    "No Store service peer available yet, neither Store service set up for the node"
+  )
+
+proc getLegacyStoreHealth(hm: NodeHealthMonitor): ProtocolHealth =
+  var p = ProtocolHealth.init("Legacy Store")
+  checkWakuNodeNotNil(hm.node, p)
+
+  if hm.node.wakuLegacyStore == nil:
+    return p.notMounted()
+
+  return p.ready()
+
+proc getLegacyStoreClientHealth(hm: NodeHealthMonitor): ProtocolHealth =
+  var p = ProtocolHealth.init("Legacy Store Client")
+  checkWakuNodeNotNil(hm.node, p)
+
+  if hm.node.wakuLegacyStoreClient == nil:
+    return p.notMounted()
+
+  if hm.node.peerManager.selectPeer(WakuLegacyStoreCodec).isSome() or
+      hm.node.wakuLegacyStore != nil:
+    return p.ready()
+
+  return p.notReady(
+    "No Legacy Store service peers are available yet, neither Store service set up for the node"
+  )
+
+proc getPeerExchangeHealth(hm: NodeHealthMonitor): ProtocolHealth =
+  var p = ProtocolHealth.init("Peer Exchange")
+  checkWakuNodeNotNil(hm.node, p)
+
+  if hm.node.wakuPeerExchange == nil:
+    return p.notMounted()
+
+  return p.ready()
+
+proc getRendezvousHealth(hm: NodeHealthMonitor): ProtocolHealth =
+  var p = ProtocolHealth.init("Rendezvous")
+  checkWakuNodeNotNil(hm.node, p)
+
+  if hm.node.wakuRendezvous == nil:
+    return p.notMounted()
+
+  if hm.node.peerManager.switch.peerStore.peers(RendezVousCodec).len() == 0:
+    return p.notReady("No Rendezvous peers are available yet")
+
+  return p.ready()
+
+proc getNodeHealthReport*(hm: NodeHealthMonitor): Future[HealthReport] {.async.} =
+  var report: HealthReport
+  report.nodeHealth = hm.nodeHealth
+
+  if not hm.node.isNil():
+    let relayHealth = hm.getRelayHealth()
+    report.protocolsHealth.add(relayHealth)
+    report.protocolsHealth.add(await hm.getRlnRelayHealth())
+    report.protocolsHealth.add(hm.getLightpushHealth(relayHealth.health))
+    report.protocolsHealth.add(hm.getLegacyLightpushHealth(relayHealth.health))
+    report.protocolsHealth.add(hm.getFilterHealth(relayHealth.health))
+    report.protocolsHealth.add(hm.getStoreHealth())
+    report.protocolsHealth.add(hm.getLegacyStoreHealth())
+    report.protocolsHealth.add(hm.getPeerExchangeHealth())
+    report.protocolsHealth.add(hm.getRendezvousHealth())
+
+    report.protocolsHealth.add(hm.getLightpushClientHealth(relayHealth.health))
+    report.protocolsHealth.add(hm.getLegacyLightpushClientHealth(relayHealth.health))
+    report.protocolsHealth.add(hm.getStoreClientHealth())
+    report.protocolsHealth.add(hm.getLegacyStoreClientHealth())
+    report.protocolsHealth.add(hm.getFilterClientHealth(relayHealth.health))
+  return report
+
+proc setNodeToHealthMonitor*(hm: NodeHealthMonitor, node: WakuNode) =
+  hm.node = node
+
+proc setOverallHealth*(hm: NodeHealthMonitor, health: HealthStatus) =
+  hm.nodeHealth = health
+
+proc startHealthMonitor*(hm: NodeHealthMonitor) =
+  hm.onlineMonitor.startOnlineMonitor()
+
+proc stopHealthMonitor*(hm: NodeHealthMonitor) {.async.} =
+  await hm.onlineMonitor.stopOnlineMonitor()
+
+proc new*(
+    T: type NodeHealthMonitor,
+    dnsNameServers = @[parseIpAddress("1.1.1.1"), parseIpAddress("1.0.0.1")],
+): T =
+  T(
+    nodeHealth: INITIALIZING,
+    node: nil,
+    onlineMonitor: OnlineMonitor.init(dnsNameServers),
+  )
