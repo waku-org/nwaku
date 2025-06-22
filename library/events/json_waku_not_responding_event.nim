25095317473f1b06b892fe2840b2a6b711ce92ee
Ivan Folgueira Bande <ivansete@status.im>
2025-06-22 22:45:21 +0200

    simle rename

--- library/events/json_waku_not_responding_event.nim
+++ library/events/json_waku_not_responding_event.nim
@@ -5,5 +5,5 @@ type JsonWakuNotRespondingEvent* = ref object of JsonEvent
 proc new*(T: type JsonWakuNotRespondingEvent): T =
   return JsonWakuNotRespondingEvent(eventType: "waku_not_responding")
 
-method `$`*(jsonConnectionChangeEvent: JsonWakuNotRespondingEvent): string =
-  $(%*jsonConnectionChangeEvent)
+method `$`*(event: JsonWakuNotRespondingEvent): string =
+  $(%*event)
