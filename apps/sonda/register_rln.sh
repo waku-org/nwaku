135721eb3b60a9dcf42d072f2997d4da0d26c6f2
stubbsta <tanya.steyn22@gmail.com>
2025-07-30 13:10:15 +0200

    Update RLN contract address to new one with restricted token minting

--- apps/sonda/register_rln.sh
+++ apps/sonda/register_rln.sh
@@ -21,10 +21,10 @@ if test -n "${ETH_CLIENT_ADDRESS}"; then
   exit 1
 fi
 
-docker run -v $(pwd)/keystore:/keystore/:Z harbor.status.im/wakuorg/nwaku:v0.30.1 generateRlnKeystore \
+docker run -v $(pwd)/keystore:/keystore/:Z harbor.status.im/wakuorg/nwaku:v0.36.1 generateRlnKeystore \
 --rln-relay-eth-client-address=${RLN_RELAY_ETH_CLIENT_ADDRESS} \
 --rln-relay-eth-private-key=${ETH_TESTNET_KEY} \
---rln-relay-eth-contract-address=0xB9cd878C90E49F797B4431fBF4fb333108CB90e6 \
+--rln-relay-eth-contract-address=0x313a4a4dcf95da8e020b5375c0f8f732b7b4f15b \
 --rln-relay-cred-path=/keystore/keystore.json \
 --rln-relay-cred-password="${RLN_RELAY_CRED_PASSWORD}" \
 --rln-relay-user-message-limit=20 \
