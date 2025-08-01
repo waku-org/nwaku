23dd47dfbe45465e412ac87435b6c80626df8d5b
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2025-08-01 14:21:35 +0200

    add comment on INVALID_MESSAGE handling w.r.t reputation

--- waku/incentivization/reputation_manager.nim
+++ waku/incentivization/reputation_manager.nim
@@ -8,6 +8,10 @@ const BadLightPushErrorCodes* = [
   LightPushErrorCode.OUT_OF_RLN_PROOF,
   LightPushErrorCode.NO_PEERS_TO_RELAY
 ]
+# Note: if Bob's RLN proof is based on an outdated root, he will return INVALID_MESSAGE (code 420).
+# This is arguably incorrect, as the issue is on Bob's side, not Alice's.
+# See issue: https://github.com/waku-org/nwaku/issues/3531
+# We do not include INVALID_MESSAGE in BadLightPushErrorCodes, as it is a client-side error.
 
 type
   ResponseQuality* = enum
