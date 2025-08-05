5c1f86016fb3fcce3853b46c4d2277b2812813cc
darshankabariya <darshan@status.im>
2025-08-05 15:50:19 +0530

    fix: chore

--- tests/waku_rln_relay/test_all.nim
+++ tests/waku_rln_relay/test_all.nim
@@ -2,7 +2,7 @@
 
 import
   ./test_rln_group_manager_onchain,
-  ./test_rln_group_manager_static,
+  ./test_rln_group_manager_offchain,
   ./test_waku_rln_relay,
   ./test_wakunode_rln_relay,
   ./test_rln_nonce_manager,
