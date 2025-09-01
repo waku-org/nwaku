61754a175f8b03a3761cc897ec38cd87f61e0bb2
darshankabariya <darshan@status.im>
2025-09-01 15:08:59 +0530

    chore: update according to suggestion

--- waku/waku_rln_relay/group_manager/off_chain/group_manager.nim
+++ waku/waku_rln_relay/group_manager/off_chain/group_manager.nim
@@ -19,33 +19,43 @@ method init*(g: OffchainGroupManager): Future[GroupManagerResult[void]] {.async.
 method startGroupSync*(
     g: OffchainGroupManager
 ): Future[GroupManagerResult[void]] {.async.} =
-  raise newException(CatchableError, "OffchainGroupManager's startGroupSync is not implemented")
+  raise newException(
+    CatchableError, "OffchainGroupManager's startGroupSync is not implemented"
+  )
 
 method register*(
     g: OffchainGroupManager, rateCommitment: RateCommitment
 ): Future[void] {.async.} =
-  raise newException(CatchableError, "OffchainGroupManager's register is not implemented")
+  raise
+    newException(CatchableError, "OffchainGroupManager's register is not implemented")
 
 method registerBatch*(
     g: OffchainGroupManager, rateCommitments: seq[RawRateCommitment]
 ): Future[void] {.async.} =
-  raise newException(CatchableError, "OffchainGroupManager's registerBatch is not implemented")
+  raise newException(
+    CatchableError, "OffchainGroupManager's registerBatch is not implemented"
+  )
 
 method withdraw*(
     g: OffchainGroupManager, idSecretHash: IdentitySecretHash
 ): Future[void] {.async.} =
-  raise newException(CatchableError, "OffchainGroupManager's withdraw is not implemented")
+  raise
+    newException(CatchableError, "OffchainGroupManager's withdraw is not implemented")
 
 method withdrawBatch*(
     g: OffchainGroupManager, idSecretHashes: seq[IdentitySecretHash]
 ): Future[void] {.async.} =
-  raise newException(CatchableError, "OffchainGroupManager's withdrawBatch is not implemented")
+  raise newException(
+    CatchableError, "OffchainGroupManager's withdrawBatch is not implemented"
+  )
 
 method onRegister*(g: OffchainGroupManager, cb: OnRegisterCallback) {.gcsafe.} =
-  raise newException(CatchableError, "OffchainGroupManager's onRegister is not implemented")
+  raise
+    newException(CatchableError, "OffchainGroupManager's onRegister is not implemented")
 
 method onWithdraw*(g: OffchainGroupManager, cb: OnWithdrawCallback) {.gcsafe.} =
-  raise newException(CatchableError, "OffchainGroupManager's onWithdraw is not implemented")
+  raise
+    newException(CatchableError, "OffchainGroupManager's onWithdraw is not implemented")
 
 method stop*(g: OffchainGroupManager): Future[void] {.async.} =
   raise newException(CatchableError, "OffchainGroupManager's stop is not implemented")
