52c0b06104a2fe0e48f9619fb61b45f7fdb0c08d
Ivan Folgueira Bande <ivansete@status.im>
2025-09-26 16:11:01 +0200

    very first version of waku.nim
    That allows importing waku in another Nim projects

---
+++ waku.nim
@@ -0,0 +1,10 @@
+import
+  waku/[
+    common/logging,
+    factory/external_config,
+    factory/waku,
+    node/health_monitor,
+    waku_api/rest/builder as rest_server_builder,
+  ]
+
+export logging, external_config, waku, health_monitor, rest_server_builder
