37f34c2c431c37b718127f090484520b75f35e04
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2025-11-14 14:20:28 +0530

    use exit==destination approach for mix for better anonymity

--- apps/chat2mix/chat2mix.nim
+++ apps/chat2mix/chat2mix.nim
@@ -82,6 +82,8 @@ type
   PrivateKey* = crypto.PrivateKey
   Topic* = waku_core.PubsubTopic
 
+const MixMixNodePoolSize = 4
+
 #####################
 ## chat2 protobufs ##
 #####################
@@ -601,12 +603,12 @@ proc processInput(rfd: AsyncFD, rng: ref HmacDrbgContext) {.async.} =
   )
   echo "waiting for mix nodes to be discovered..."
   while true:
-    if node.getMixNodePoolSize() >= 3:
+    if node.getMixNodePoolSize() >= MixMixNodePoolSize:
       break
     discard await node.fetchPeerExchangePeers()
     await sleepAsync(1000)
 
-  while node.getMixNodePoolSize() < 3:
+  while node.getMixNodePoolSize() < MixMixNodePoolSize:
     info "waiting for mix nodes to be discovered",
       currentpoolSize = node.getMixNodePoolSize()
     await sleepAsync(1000)
